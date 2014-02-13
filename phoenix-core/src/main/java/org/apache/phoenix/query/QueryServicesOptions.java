eebeed4b85b4497e5b49a7d5502bf014277de8a2
maryannxue <maryannxue@apache.org>
2014-02-13 18:58:49 -0500

    Change DEFAULT_MAX_MEMORY_PERC to 20 (old value 50)

--- phoenix-core/src/main/java/org/apache/phoenix/query/QueryServicesOptions.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/QueryServicesOptions.java
@@ -74,7 +74,7 @@ public class QueryServicesOptions {
 	public static final int DEFAULT_QUEUE_SIZE = 500;
 	public static final int DEFAULT_THREAD_TIMEOUT_MS = 600000; // 10min
 	public static final int DEFAULT_SPOOL_THRESHOLD_BYTES = 1024 * 1024 * 20; // 20m
-	public static final int DEFAULT_MAX_MEMORY_PERC = 50; // 50% of heap
+	public static final int DEFAULT_MAX_MEMORY_PERC = 20; // 20% of heap
 	public static final int DEFAULT_MAX_MEMORY_WAIT_MS = 10000;
 	public static final int DEFAULT_MAX_TENANT_MEMORY_PERC = 100;
 	public static final long DEFAULT_MAX_SERVER_CACHE_SIZE = 1024*1024*100;  // 100 Mb
