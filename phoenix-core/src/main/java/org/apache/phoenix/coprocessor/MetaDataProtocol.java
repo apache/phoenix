5aac13a749f97d0bf99013348e482bf46782d477
Jeffrey Zhong <jzhong@JZhongs-MacBook-Pro.local>
2014-03-17 15:44:02 -0700

    PHOENIX-864: Bump up phoenix driver version from 3.0 to 4.0

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/MetaDataProtocol.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/MetaDataProtocol.java
@@ -52,7 +52,7 @@ import com.google.protobuf.HBaseZeroCopyByteString;
  * @since 0.1
  */
 public abstract class MetaDataProtocol extends MetaDataService {
-    public static final int PHOENIX_MAJOR_VERSION = 3;
+    public static final int PHOENIX_MAJOR_VERSION = 4;
     public static final int PHOENIX_MINOR_VERSION = 0;
     public static final int PHOENIX_PATCH_NUMBER = 0;
     public static final int PHOENIX_VERSION = 
