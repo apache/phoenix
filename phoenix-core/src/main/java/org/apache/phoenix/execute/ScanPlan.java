2c97b7b6483973d13860980be854fb9ad0cf774f
maryannxue <maryannxue@apache.org>
2014-04-28 11:53:40 -0400

    PHOENIX-951 Don't push LIMIT as PageFilter for joins

--- phoenix-core/src/main/java/org/apache/phoenix/execute/ScanPlan.java
+++ phoenix-core/src/main/java/org/apache/phoenix/execute/ScanPlan.java
@@ -57,11 +57,13 @@ import org.apache.phoenix.util.ScanUtil;
  */
 public class ScanPlan extends BasicQueryPlan {
     private List<KeyRange> splits;
+    private boolean allowPageFilter;
     
-    public ScanPlan(StatementContext context, FilterableStatement statement, TableRef table, RowProjector projector, Integer limit, OrderBy orderBy, ParallelIteratorFactory parallelIteratorFactory) {
+    public ScanPlan(StatementContext context, FilterableStatement statement, TableRef table, RowProjector projector, Integer limit, OrderBy orderBy, ParallelIteratorFactory parallelIteratorFactory, boolean allowPageFilter) {
         super(context, statement, table, projector, context.getBindManager().getParameterMetaData(), limit, orderBy, null, 
                 parallelIteratorFactory != null ? parallelIteratorFactory :
                     new SpoolingResultIterator.SpoolingResultIteratorFactory(context.getConnection().getQueryServices()));
+        this.allowPageFilter = allowPageFilter;
         if (!orderBy.getOrderByExpressions().isEmpty()) { // TopN
             int thresholdBytes = context.getConnection().getQueryServices().getProps().getInt(
                     QueryServices.SPOOL_THRESHOLD_BYTES_ATTRIB, QueryServicesOptions.DEFAULT_SPOOL_THRESHOLD_BYTES);
@@ -89,7 +91,7 @@ public class ScanPlan extends BasicQueryPlan {
          * limit is provided, run query serially.
          */
         boolean isOrdered = !orderBy.getOrderByExpressions().isEmpty();
-        ParallelIterators iterators = new ParallelIterators(context, tableRef, statement, projection, GroupBy.EMPTY_GROUP_BY, isOrdered ? null : limit, parallelIteratorFactory);
+        ParallelIterators iterators = new ParallelIterators(context, tableRef, statement, projection, GroupBy.EMPTY_GROUP_BY, !allowPageFilter || isOrdered ? null : limit, parallelIteratorFactory);
         splits = iterators.getSplits();
         if (isOrdered) {
             scanner = new MergeSortTopNResultIterator(iterators, limit, orderBy.getOrderByExpressions());
