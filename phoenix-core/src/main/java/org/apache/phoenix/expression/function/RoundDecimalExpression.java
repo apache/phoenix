29c610cfdec8414d0b35f2e66b7090f540c48eb7
James Taylor <jtaylor@salesforce.com>
2014-08-26 22:43:57 -0700

    PHOENIX-761 Implement getKeyPart for round/ceil/floor of a DECIMAL value (Kyle Buzsaki)

--- phoenix-core/src/main/java/org/apache/phoenix/expression/function/RoundDecimalExpression.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/function/RoundDecimalExpression.java
@@ -34,22 +34,28 @@ import org.apache.phoenix.schema.PDataType;
 import org.apache.phoenix.schema.tuple.Tuple;
 
 import com.google.common.collect.Lists;
+import java.util.Collections;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.phoenix.compile.KeyPart;
+import static org.apache.phoenix.expression.function.ScalarFunction.evaluateExpression;
+import org.apache.phoenix.query.KeyRange;
+import org.apache.phoenix.schema.PColumn;
+import static org.apache.phoenix.schema.PDataType.DECIMAL;
 
 /**
- * 
- * Class encapsulating the process for rounding off a column/literal of 
- * type {@link org.apache.phoenix.schema.PDataType#DECIMAL}
  *
- * 
+ * Class encapsulating the process for rounding off a column/literal of type
+ * {@link org.apache.phoenix.schema.PDataType#DECIMAL}
+ *
+ *
  * @since 3.0.0
  */
-
 public class RoundDecimalExpression extends ScalarFunction {
-    
+
     private int scale;
-    
+
     /**
-     * Creates a {@link RoundDecimalExpression} with rounding scale given by @param scale. 
+     * Creates a {@link RoundDecimalExpression} with rounding scale given by @param scale.
      *
      */
     public static Expression create(Expression expr, int scale) throws SQLException {
@@ -60,15 +66,15 @@ public class RoundDecimalExpression extends ScalarFunction {
         List<Expression> expressions = Lists.newArrayList(expr, scaleExpr);
         return new RoundDecimalExpression(expressions);
     }
-    
+
     /**
-     * Creates a {@link RoundDecimalExpression} with a default scale of 0 used for rounding. 
+     * Creates a {@link RoundDecimalExpression} with a default scale of 0 used for rounding.
      *
      */
     public static Expression create(Expression expr) throws SQLException {
         return create(expr, 0);
     }
-    
+
     public static Expression create(List<Expression> exprs) throws SQLException {
         Expression expr = exprs.get(0);
         if (expr.getDataType().isCoercibleTo(PDataType.LONG)) {
@@ -80,9 +86,9 @@ public class RoundDecimalExpression extends ScalarFunction {
         }
         return new RoundDecimalExpression(exprs);
     }
-    
+
     public RoundDecimalExpression() {}
-    
+
     protected RoundDecimalExpression(List<Expression> children) {
         super(children);
         LiteralExpression scaleChild = (LiteralExpression)children.get(1);
@@ -97,7 +103,7 @@ public class RoundDecimalExpression extends ScalarFunction {
                 }
             }
             throw new IllegalDataException("Invalid second argument for scale: " + scaleValue + ". The scale must be between 0 and " + PDataType.MAX_PRECISION + " inclusive.");
-        } 
+        }
     }
 
     @Override
@@ -116,9 +122,13 @@ public class RoundDecimalExpression extends ScalarFunction {
     public PDataType getDataType() {
         return PDataType.DECIMAL;
     }
-    
+
     protected RoundingMode getRoundingMode() {
         return RoundingMode.HALF_UP;
+    }
+
+    protected final int getRoundingScale() {
+        return scale;
     }
     
     @Override
@@ -137,10 +147,218 @@ public class RoundDecimalExpression extends ScalarFunction {
     public String getName() {
         return RoundFunction.NAME;
     }
-    
+
     @Override
     public OrderPreserving preservesOrder() {
         return OrderPreserving.YES;
+    }
+
+    @Override
+    public int getKeyFormationTraversalIndex() {
+        return 0;
+    }
+
+    @Override
+    public KeyPart newKeyPart(final KeyPart childPart) {
+        return new KeyPart() {
+            private final List<Expression> extractNodes = Collections.<Expression>singletonList(RoundDecimalExpression.this);
+
+            @Override
+            public PColumn getColumn() {
+                return childPart.getColumn();
+            }
+
+            @Override
+            public List<Expression> getExtractNodes() {
+                return extractNodes;
+            }
+
+            @Override
+            public KeyRange getKeyRange(CompareFilter.CompareOp op, Expression rhs) {
+                final BigDecimal rhsDecimal = (BigDecimal) DECIMAL.toObject(evaluateExpression(rhs));
+                
+                // equality requires an exact match. if rounding would cut off more precision
+                // than needed for a match, it's impossible for there to be any matches
+                if(op == CompareFilter.CompareOp.EQUAL && !hasEnoughPrecisionToProduce(rhsDecimal)) {
+                    return KeyRange.EMPTY_RANGE;
+                }
+                
+                // if the decimal needs to be rounded, round it such that the given 
+                // operator will still be valid
+                BigDecimal roundedDecimal = roundAndPreserveOperator(rhsDecimal, op);
+                
+                // the range of big decimals that could be rounded to produce the rounded result
+                // alternatively, the "rounding bucket" that this decimal falls into
+                final KeyRange equalityRange = getInputRangeProducing(roundedDecimal);
+                boolean lowerInclusive = equalityRange.isLowerInclusive();
+                boolean upperInclusive = equalityRange.isUpperInclusive();
+                byte[] lowerRange = KeyRange.UNBOUND;
+                byte[] upperRange = KeyRange.UNBOUND;
+                
+                switch(op) {
+                    case EQUAL:
+                        return equalityRange;
+                    case GREATER:
+                        // from the equality range and up, NOT including the equality range
+                        lowerRange = equalityRange.getUpperRange();
+                        lowerInclusive = !equalityRange.isUpperInclusive();
+                        break;
+                    case GREATER_OR_EQUAL:
+                        // from the equality range and up, including the equality range
+                        lowerRange = equalityRange.getLowerRange();
+                        break;
+                    case LESS:
+                        // from the equality range and down, NOT including the equality range
+                        upperRange = equalityRange.getLowerRange();
+                        upperInclusive = !equalityRange.isLowerInclusive();
+                        break;
+                    case LESS_OR_EQUAL:
+                        // from the equality range and down, including the equality range
+                        upperRange = equalityRange.getUpperRange();
+                        break;
+                    default:
+                        throw new AssertionError("Invalid CompareOp: " + op);
+                }
+
+                return KeyRange.getKeyRange(lowerRange, lowerInclusive, upperRange, upperInclusive);
+            }
+            
+            /**
+             * Produces a the given decimal rounded to this rounding expression's scale. If the 
+             * decimal requires more scale precision to produce than this expression has, as in
+             * ROUND(?, 2) &gt; 2.0098974, it ensures that the decimal is rounded such that the
+             * given operator will still produce correct results.
+             * @param decimal  the decimal to round with this expression's scale
+             * @param op  the operator to preserve comparison with in the event of lost precision
+             * @return  the rounded decimal
+             */
+            private BigDecimal roundAndPreserveOperator(BigDecimal decimal, CompareFilter.CompareOp op) {
+                final BigDecimal rounded = roundToScale(decimal);
+                
+                // if we lost information, make sure that the rounding didn't break the operator
+                if(!hasEnoughPrecisionToProduce(decimal)) {
+                    switch(op) {
+                        case GREATER_OR_EQUAL:
+                            // e.g. 'ROUND(dec, 2) >= 2.013' would be converted to 
+                            // 'ROUND(dec, 2) >= 2.01' but should be 'ROUND(dec, 2) >= 2.02'
+                            if(decimal.compareTo(rounded) > 0) {
+                                return stepNextInScale(rounded);
+                            }
+                            break;
+                        case GREATER:
+                            // e.g. 'ROUND(dec, 2) > 2.017' would be converted to 
+                            // 'ROUND(dec, 2) > 2.02' but should be 'ROUND(dec, 2) > 2.01'
+                            if(decimal.compareTo(rounded) < 0) {
+                                return stepPrevInScale(rounded);
+                            }
+                            break;
+                        case LESS_OR_EQUAL:
+                            // e.g. 'ROUND(dec, 2) < 2.017' would be converted to 
+                            // 'ROUND(dec, 2) < 2.02' but should be 'ROUND(dec, 2) < 2.01'
+                            if(decimal.compareTo(rounded) < 0) {
+                                return stepPrevInScale(rounded);
+                            }
+                            break;
+                        case LESS:
+                            // e.g. 'ROUND(dec, 2) <= 2.013' would be converted to 
+                            // 'ROUND(dec, 2) <= 2.01' but should be 'ROUND(dec, 2) <= 2.02'
+                            if(decimal.compareTo(rounded) > 0) {
+                                return stepNextInScale(rounded);
+                            }
+                            break;
+                    }
+                }
+                
+                // otherwise, rounding has not affected the operator, so return normally
+                return rounded;
+            }
+        };
+    }
+    
+    /**
+     * Finds the Decimal KeyRange that will produce the given result when fed into this 
+     * rounding expression. For example, a ROUND expression with scale 2 will produce the
+     * result "2.05" with any decimal in the range [2.045, 2.0545). 
+     * The result must be pre-rounded to within this rounding expression's scale. 
+     * @param result  the result to find an input range for. Must be producable.
+     * @return  a KeyRange of DECIMAL keys that can be rounded by this expression to produce result
+     * @throws IllegalArgumentException  if the result has more scale than this expression can produce
+     */
+    protected KeyRange getInputRangeProducing(BigDecimal result) {
+        if(!hasEnoughPrecisionToProduce(result)) {
+            throw new IllegalArgumentException("Cannot produce input range for decimal " + result 
+                + ", not enough precision with scale " + getRoundingScale());
+        }
+        byte[] lowerRange = DECIMAL.toBytes(halfStepPrevInScale(result));
+        byte[] upperRange = DECIMAL.toBytes(halfStepNextInScale(result));
+        // inclusiveness changes depending on sign
+        // e.g. -0.5 rounds "up" to -1 even though it is the lower boundary
+        boolean lowerInclusive = result.signum() > 0;
+        boolean upperInclusive = result.signum() < 0;
+        return KeyRange.getKeyRange(lowerRange, lowerInclusive, upperRange, upperInclusive);
+    }
+    
+    /**
+     * Determines whether this rounding expression's scale has enough precision to produce the
+     * minimum precision for the input decimal. In other words, determines whether the given
+     * decimal can be rounded to this scale without losing ordering information.
+     * For example, an expression with a scale of 2 has enough precision to produce "2.3", "2.71"
+     * and "2.100000", but does not have enough precision to produce "2.001"
+     * @param result  the decimal to round
+     * @return  true if the given decimal can be precisely matched by this rounding expression
+     */
+    protected final boolean hasEnoughPrecisionToProduce(BigDecimal result) {
+        // use compareTo so that 2.0 and 2.00 are treated as "equal"
+        return roundToScale(result).compareTo(result) == 0;
+    }
+    
+    /**
+     * Returns the given decimal rounded to this rounding expression's scale.
+     * For example, with scale 2 the decimal "2.453" would be rounded to either 2.45 or 
+     * 2.46 depending on the rounding mode, while "2.38" and "2.7" would be unchanged.  
+     * @param decimal  the decimal to round
+     * @return  the rounded result decimal
+     */
+    protected final BigDecimal roundToScale(BigDecimal decimal) {
+        return decimal.setScale(getRoundingScale(), getRoundingMode());
+    }
+    
+    /**
+     * Produces a value half of a "step" back in this expression's rounding scale.
+     * For example with a scale of 2, "2.5" would be stepped back to "2.495".
+     */
+    protected final BigDecimal halfStepPrevInScale(BigDecimal decimal) {
+        BigDecimal step = BigDecimal.ONE.scaleByPowerOfTen(-getRoundingScale());
+        BigDecimal halfStep = step.divide(BigDecimal.valueOf(2));
+        return decimal.subtract(halfStep);
+    }
+    
+    /**
+     * Produces a value half of a "step" forward in this expression's rounding scale.
+     * For example with a scale of 2, "2.5" would be stepped forward to "2.505".
+     */
+    protected final BigDecimal halfStepNextInScale(BigDecimal decimal) {
+        BigDecimal step = BigDecimal.ONE.scaleByPowerOfTen(-getRoundingScale());
+        BigDecimal halfStep = step.divide(BigDecimal.valueOf(2));
+        return decimal.add(halfStep);
+    }
+
+    /**
+     * Produces a value one "step" back in this expression's rounding scale.
+     * For example with a scale of 2, "2.5" would be stepped back to "2.49".
+     */
+    protected final BigDecimal stepPrevInScale(BigDecimal decimal) {
+        BigDecimal step = BigDecimal.ONE.scaleByPowerOfTen(-getRoundingScale());
+        return decimal.subtract(step);
+    }
+
+    /**
+     * Produces a value one "step" forward in this expression's rounding scale.
+     * For example with a scale of 2, "2.5" would be stepped forward to "2.51".
+     */
+    protected final BigDecimal stepNextInScale(BigDecimal decimal) {
+        BigDecimal step = BigDecimal.ONE.scaleByPowerOfTen(-getRoundingScale());
+        return decimal.add(step);
     }
 
 }
