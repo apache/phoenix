5aa8d405971911e393535fc85571fd1c6bd84650
James Taylor <jamestaylor@apache.org>
2014-09-16 13:30:04 -0700

    PHOENIX-1252 Potential lock leak for ungrouped aggregation (Jay Wong)

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/UngroupedAggregateRegionObserver.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/UngroupedAggregateRegionObserver.java
@@ -345,11 +345,11 @@ public class UngroupedAggregateRegionObserver extends BaseScannerRegionObserver 
                 }
             } while (hasMore);
         } finally {
-          try {
-            innerScanner.close();
-          } finally {
-            region.closeRegionOperation();
-          }
+            try {
+                innerScanner.close();
+            } finally {
+                region.closeRegionOperation();
+            }
         }
         
         if (logger.isInfoEnabled()) {
