cf41cc63594ac7d8f3d831511215519fe43afea0
Jeffrey Zhong <jeffreyz@apache.org>
2014-10-19 21:22:57 -0700

    PHOENIX-1355: sqlline.py or psql.py may hang if they're not invoked from bin folder

--- bin/sqlline.py
+++ bin/sqlline.py
@@ -51,7 +51,7 @@ colorSetting = "true"
 if os.name == 'nt':
     colorSetting = "false"
 
-java_cmd = 'java -cp ".' + os.pathsep + phoenix_utils.phoenix_client_jar + \
+java_cmd = 'java -cp "' + phoenix_utils.hbase_conf_path + os.pathsep + phoenix_utils.phoenix_client_jar + \
     '" -Dlog4j.configuration=file:' + \
     os.path.join(phoenix_utils.current_dir, "log4j.properties") + \
     " sqlline.SqlLine -d org.apache.phoenix.jdbc.PhoenixDriver \
