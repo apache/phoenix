cc594d77d77f87cb52cd50f8ff46fa26d5529c55
James Taylor <jtaylor@salesforce.com>
2014-10-21 23:37:03 -0700

    PHOENIX-1359 Backward compatibility fails with 4.1 client and current 4.0 branch on server

--- phoenix-core/src/main/java/org/apache/phoenix/expression/ExpressionType.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/ExpressionType.java
@@ -148,7 +148,6 @@ public enum ExpressionType {
     LowerFunction(LowerFunction.class),
     TrimFunction(TrimFunction.class),
     DistinctCountAggregateFunction(DistinctCountAggregateFunction.class),
-    DistinctValueAggregateFunction(DistinctValueAggregateFunction.class),
     PercentileContAggregateFunction(PercentileContAggregateFunction.class),
     PercentRankAggregateFunction(PercentRankAggregateFunction.class),
     StddevPopFunction(StddevPopFunction.class),
@@ -184,6 +183,7 @@ public enum ExpressionType {
     InlineArrayElemRefExpression(InlineArrayElemRefExpression.class),
     SQLIndexTypeFunction(SQLIndexTypeFunction.class),
     ModulusExpression(ModulusExpression.class),
+    DistinctValueAggregateFunction(DistinctValueAggregateFunction.class),
     RegexpSplitFunctiond(RegexpSplitFunction.class);
     ExpressionType(Class<? extends Expression> clazz) {
         this.clazz = clazz;
