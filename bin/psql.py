481d1bb878d08fc43fd1eb8f85c36244afdafe45
Gabriel Reid <gabrielr@ngdata.com>
2014-10-27 10:57:43 +0100

    PHOENIX-897 Quote parameters in psql.py
    Properly quote supplied command-line parameters in psql.py so that
    it's possible to supply any character (including ones that have
    special meanings in various shells) as parameters.

--- bin/psql.py
+++ bin/psql.py
@@ -26,11 +26,17 @@ import phoenix_utils
 
 phoenix_utils.setPath()
 
+if os.name == 'nt':
+    args = subprocess.list2cmdline(sys.argv[1:])
+else:
+    import pipes    # pipes module isn't available on Windows
+    args = " ".join([pipes.quote(v) for v in sys.argv[1:]])
+
 # HBase configuration folder path (where hbase-site.xml reside) for
 # HBase/Phoenix client side property override
 java_cmd = 'java -cp "' + phoenix_utils.hbase_conf_path + os.pathsep + phoenix_utils.phoenix_client_jar + \
     '" -Dlog4j.configuration=file:' + \
     os.path.join(phoenix_utils.current_dir, "log4j.properties") + \
-    " org.apache.phoenix.util.PhoenixRuntime " + ' '.join(sys.argv[1:])
+    " org.apache.phoenix.util.PhoenixRuntime " + args 
 
 subprocess.call(java_cmd, shell=True)
