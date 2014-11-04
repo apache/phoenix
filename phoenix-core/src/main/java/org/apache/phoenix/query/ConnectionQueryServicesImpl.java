dd659bb481437e985609348913d7e4cdd968e2b7
Gabriel Reid <gabrielr@ngdata.com>
2014-11-04 13:45:19 +0100

    PHOENIX-1403 Correct synchronization in ConnectionQueryServicesImpl.init

--- phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
@@ -1498,7 +1498,7 @@ public class ConnectionQueryServicesImpl extends DelegateQueryServices implement
                         }
                         return null;
                     }
-                    synchronized (this) {
+                    synchronized (ConnectionQueryServicesImpl.this) {
                         if (initialized) {
                             if (initializationException != null) {
                                 // Throw previous initialization exception, as we won't resuse this instance
