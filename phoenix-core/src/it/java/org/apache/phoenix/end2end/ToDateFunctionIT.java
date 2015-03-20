6a68955c95caa2ae1510f1b97321e249a572d7e1
dhacker1341 <dhacker1341@gmail.com>
2015-03-20 12:45:51 -0700

    PHOENIX-1744 Only unsigned long, not integer should be castable to timestamp.

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/ToDateFunctionIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/ToDateFunctionIT.java
@@ -18,16 +18,25 @@
 
 package org.apache.phoenix.end2end;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Properties;
+
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.schema.TypeMismatchException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.sql.*;
-import java.util.Properties;
-
-import static org.junit.Assert.*;
 
 
 public class ToDateFunctionIT extends BaseHBaseManagedTimeIT {
