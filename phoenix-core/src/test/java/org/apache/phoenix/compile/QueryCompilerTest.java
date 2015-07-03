8ee6a8a2c77e2cb985bbb1c6b5a465e4a2494dd6
Thomas D'Silva <tdsilva@salesforce.com>
2015-07-03 12:50:45 -0700

    PHOENIX-2065 Throw TableNotFoundException when select all columns of one column family from the table with schema, Add missing import

--- phoenix-core/src/test/java/org/apache/phoenix/compile/QueryCompilerTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/compile/QueryCompilerTest.java
@@ -32,6 +32,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Collections;
