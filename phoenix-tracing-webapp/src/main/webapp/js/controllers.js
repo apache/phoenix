2c1133075d81777943f766715390b4a2bb858e37
ayolajayamaha <raphaelanne2@gmail.com>
2015-07-28 23:06:45 +0530

    binding UI to the phoenix jdbc tracing data

--- phoenix-tracing-webapp/src/main/webapp/js/controllers.js
+++ phoenix-tracing-webapp/src/main/webapp/js/controllers.js
@@ -3,8 +3,22 @@
 var TraceCtrl = angular.module('TracingCtrl', ['nvd3', 'googlechart', 'ui.bootstrap']);
 
 //listing trace from json
-TraceCtrl.controller('TraceListCtrl', function($scope) {
-  $scope.traces = sampleTrace;
+TraceCtrl.controller('TraceListCtrl', function($scope, $http) {
+  //$scope.traces = sampleTrace;
+  $scope.traces = [];
+  $scope.tracesLimit =100;
+  
+    $scope.loadTrace = function() {
+        var httpRequest = $http({
+            method: 'GET',
+            url: '../trace?action=getall&limit='+$scope.tracesLimit
+
+        }).success(function(data, status) {
+            $scope.traces = data;
+        });
+
+    };
+    $scope.loadTrace();
 });
 
 //this will change with after db binding.
