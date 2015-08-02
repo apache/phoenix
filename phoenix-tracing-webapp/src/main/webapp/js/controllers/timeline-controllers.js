6d34275e3367af88fbf8afcdc8baed68fd2ac117
ayolajayamaha <raphaelanne2@gmail.com>
2015-08-02 21:07:50 +0530

    mapping BE for timeline

--- phoenix-tracing-webapp/src/main/webapp/js/controllers/timeline-controllers.js
+++ phoenix-tracing-webapp/src/main/webapp/js/controllers/timeline-controllers.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var TimeLineCtrl = angular.module('TimeLineCtrl', ['ui.bootstrap']);
-TimeLineCtrl.controller('TraceTimeLineCtrl', function($scope,$http) {
+TimeLineCtrl.controller('TraceTimeLineCtrl', function($scope, $http) {
   $scope.page = {
     title: 'Timeline for Trace'
   };
-
+  console.log(timeLine);
   $scope.chartObject = timeLine;
   $scope.clear = function() {
     var nextid = $scope.chartObject.data.rows.length;
@@ -15,37 +15,65 @@ TimeLineCtrl.controller('TraceTimeLineCtrl', function($scope,$http) {
   $scope.myItemID = "Trace 01";
   $scope.clearId = function() {
     var count = $scope.chartObject.data.rows.length;
-    for (var i = 0; i < count; i++) {
+    for(var i = 0; i < count; i++) {
       var obj = $scope.chartObject.data.rows[i];
-      if (obj === $scope.myItemID) {
+      if(obj === $scope.myItemID) {
         $scope.chartObject.data.rows.splice(i, 1);
       }
     }
   }
 
-$scope.trace = {};
-  $scope.addItemToTimeLine = function(item) {
-    $http.get('./js/api/trace.json').
-  success(function(data, status, headers, config) {
-    $scope.trace = data;
-    var nextid = $scope.chartObject.data.rows.length;
-    //adding to the time line
-    $scope.chartObject.data.rows[nextid] = {
-      "c": [{
-        "v": "Trace " + nextid
-      }, {
-        "v": $scope.trace.description
-      }, {
-        "v": new Date(parseFloat($scope.trace.start_time))
-      }, {
-        "v": new Date(parseFloat($scope.trace.end_time))
-      }]
-    }
-  }).
-  error(function(data, status, headers, config) {
-    console.log('error.....');
-  });
-
-
+  $scope.trace = {};
+  $scope.addItemToTimeLine = function() {
+    $http.get('../trace?action=getall&limit=10').
+    success(function(data, status, headers, config) {
+      $scope.trace = data[0];
+      var nextid = $scope.chartObject.data.rows.length;
+      console.log(data[0]);
+      //adding to the time line
+      $scope.chartObject.data.rows[nextid] = {
+        "c": [{
+          "v": "Trace " + nextid
+        }, {
+          "v": $scope.trace.description
+        }, {
+          "v": new Date(parseFloat($scope.trace.start_time))
+        }, {
+          "v": new Date(parseFloat($scope.trace.end_time))
+        }]
+      }
+    }).
+    error(function(data, status, headers, config) {
+      console.log('error in loading time line item');
+    });
   };
+
+  $scope.loadTimeLine = function() {
+    $http.get('../trace?action=getall&limit=5').
+    success(function(data, status, headers, config) {
+
+      var nextid = $scope.chartObject.data.rows.length;me
+      //adding list of items for time line
+      for(var i = 0; i < data.length; i++) {
+        var nextid = $scope.chartObject.data.rows.length;
+        var datax = data[i];
+        console.log(datax);
+        $scope.chartObject.data.rows[nextid] = {
+          "c": [{
+            "v": "Trace " + nextid
+          }, {
+            "v": datax.description
+          }, {
+            "v": new Date(parseFloat(datax.start_time))
+          }, {
+            "v": new Date(parseFloat(datax.end_time))
+          }]
+        }
+      }
+    }).
+    error(function(data, status, headers, config) {
+      console.log('error of loading timeline in start');
+    });
+  };
+  $scope.loadTimeLine();
 });
