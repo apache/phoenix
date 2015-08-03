373502b609d0a79cff4390681e587f4e00c9166f
ayolajayamaha <raphaelanne2@gmail.com>
2015-08-03 13:08:42 +0530

    adding test spec and mocks

---
+++ phoenix-tracing-webapp/src/test/webapp/js/specs/test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+//beforeEach(module('TracingAppCtrl'));
+describe('The Tracing Web App ', function() {
+
+  describe("Testing TestSpecs", function() {
+    it("contains spec with an expectation", function() {
+      expect(true).toBe(true);
+    });
+
+    it("checking the variables and `undefined`", function() {
+      var trace = {
+        tabel: 'SYSTEM.TRACING_STATS'
+      };
+      expect(trace.tabel).toBeDefined();
+      expect(trace.system).not.toBeDefined();
+    });
+
+  });
+
+  describe('Controller: TracingAppCtrl', function() {
+
+    // load the controller's module
+    beforeEach(module('TracingAppCtrl'));
+
+    //TODO - Write tets for controller in TracingApp
+    var TraceListCtrl, scope;
+    it('Chart Controller should to be defined', function() {
+      expect(TraceListCtrl).toBeUndefined();
+    });
+  });
+
+
+});
