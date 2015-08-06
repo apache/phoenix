3718da18195dc4b5d35cd8e9b0f1b8a6b488faec
Jan <jfernando@salesforce.com>
2015-08-06 11:12:52 -0700

    PHOENIX-1791. Adding ability for Pherf Write Workloads to write to a multi-tenant view.

--- phoenix-pherf/src/main/java/org/apache/phoenix/pherf/workload/WriteWorkload.java
+++ phoenix-pherf/src/main/java/org/apache/phoenix/pherf/workload/WriteWorkload.java
@@ -18,6 +18,21 @@
 
 package org.apache.phoenix.pherf.workload;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import org.apache.phoenix.pherf.PherfConstants;
 import org.apache.phoenix.pherf.configuration.Column;
 import org.apache.phoenix.pherf.configuration.Scenario;
@@ -33,17 +48,6 @@ import org.apache.phoenix.pherf.util.PhoenixUtil;
 import org.apache.phoenix.pherf.util.RowCalculator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.math.BigDecimal;
-import java.sql.*;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class WriteWorkload implements Workload {
     private static final Logger logger = LoggerFactory.getLogger(WriteWorkload.class);
@@ -158,7 +162,7 @@ public class WriteWorkload implements Workload {
         waitForBatches(dataLoadTimeSummary, scenario, start, writeBatches);
 
         // always update stats for Phoenix base tables
-        updatePhoenixStats(scenario.getTableName());
+        updatePhoenixStats(scenario.getTableName(), scenario);
     }
 
     private List<Future> getBatches(DataLoadThreadTime dataLoadThreadTime, Scenario scenario)
@@ -172,7 +176,7 @@ public class WriteWorkload implements Workload {
             List<Column>
                     phxMetaCols =
                     pUtil.getColumnsFromPhoenix(scenario.getSchemaName(),
-                            scenario.getTableNameWithoutSchemaName(), pUtil.getConnection());
+                            scenario.getTableNameWithoutSchemaName(), pUtil.getConnection(scenario.getTenantId()));
             int threadRowCount = rowCalculator.getNext();
             logger.info(
                     "Kick off thread (#" + i + ")for upsert with (" + threadRowCount + ") rows.");
@@ -215,9 +219,9 @@ public class WriteWorkload implements Workload {
      * @param tableName
      * @throws Exception
      */
-    public void updatePhoenixStats(String tableName) throws Exception {
+    public void updatePhoenixStats(String tableName, Scenario scenario) throws Exception {
         logger.info("Updating stats for " + tableName);
-        pUtil.executeStatement("UPDATE STATISTICS " + tableName);
+        pUtil.executeStatement("UPDATE STATISTICS " + tableName, scenario);
     }
 
     public Future<Info> upsertData(final Scenario scenario, final List<Column> columns,
@@ -230,7 +234,7 @@ public class WriteWorkload implements Workload {
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
                 Connection connection = null;
                 try {
-                    connection = pUtil.getConnection();
+                    connection = pUtil.getConnection(scenario.getTenantId());
                     long logStartTime = System.currentTimeMillis();
                     long
                             maxDuration =
