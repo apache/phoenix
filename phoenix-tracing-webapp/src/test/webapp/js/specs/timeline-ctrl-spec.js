7f78b5508f782196a97c4f876987e6cdaf573477
ayolajayamaha <raphaelanne2@gmail.com>
2015-08-20 20:47:34 +0530

    adding front-end of trace web app

---
+++ phoenix-tracing-webapp/src/test/webapp/js/specs/timeline-ctrl-spec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+describe('Controller : TimeLineCtrl', function() {
+/*
+  beforeEach(module('TimeLineCtrl'));
+
+  beforeEach(inject(function($rootScope, $controller) {
+    scope = $rootScope.$new();
+    traceTimeLineCtrl = $controller('TraceTimeLineCtrl', {
+      $scope: scope
+    });
+  }));
+
+
+
+  it('should to be defined', function() {
+    expect(scope).toBeDefined();
+  });
+
+  it('scope level variable should to be defined', function() {
+    expect(scope.chartObject).toBeDefined();
+    expect(scope.loadTimeLine).toBeDefined();
+    expect(scope.clear).toBeDefined();
+  });
+*/
+});
