2248c5d11d60152666d529edb618a9347b948b96
Thomas D'Silva <tdsilva@salesforce.com>
2015-10-15 17:12:42 -0700

    PHOENIX-2284 Remove call to returnAllSequences when connection count drop to zero

--- phoenix-core/src/main/java/org/apache/phoenix/query/QueryServices.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/QueryServices.java
@@ -169,6 +169,7 @@ public interface QueryServices extends SQLCloseable {
     public static final String ALLOW_USER_DEFINED_FUNCTIONS_ATTRIB = "phoenix.functions.allowUserDefinedFunctions";
     public static final String COLLECT_REQUEST_LEVEL_METRICS = "phoenix.query.request.metrics.enabled";
     public static final String ALLOW_VIEWS_ADD_NEW_CF_BASE_TABLE = "phoenix.view.allowNewColumnFamily";
+    public static final String RETURN_SEQUENCE_VALUES_ATTRIB = "phoenix.sequence.returnValues";
     
 
     /**
