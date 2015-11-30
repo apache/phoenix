b1418760625ee18a9dc90b107322e3f0fad529fb
James Taylor <jtaylor@salesforce.com>
2015-11-30 11:58:21 -0800

    PHOENIX-2461 Reverting Tweak shutdown of ConnectionQueryService during testing

--- phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
@@ -225,7 +225,6 @@ public class ConnectionQueryServicesImpl extends DelegateQueryServices implement
 
     private HConnection connection;
     private TransactionServiceClient txServiceClient;
-    protected volatile ZKClientService zkClientService;
     private volatile boolean initialized;
     private volatile int nSequenceSaltBuckets;
 
@@ -311,7 +310,7 @@ public class ConnectionQueryServicesImpl extends DelegateQueryServices implement
     
     private void initTxServiceClient() {
         String zkQuorumServersString = connectionInfo.getZookeeperQuorum()+":"+connectionInfo.getPort();
-        zkClientService = ZKClientServices.delegate(
+        ZKClientService zkClientService = ZKClientServices.delegate(
                   ZKClients.reWatchOnExpire(
                     ZKClients.retryOnFailure(
                       ZKClientService.Builder.of(zkQuorumServersString)
