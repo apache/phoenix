3d9d3c22c2fed192693ded7393baec4a16fa794a
Josh Elser <elserj@apache.org>
2015-12-08 10:59:38 -0500

    PHOENIX-2496 Attempt to gracefully close in ShutdownHook, but still exit promptly.
    The JVM will not exit until all ShutdownHooks have completed.
    Blocking until a query thread completes might be on the order
    of minutes which is undesirable. Try to clean up, but exit quickly.
    
    Introduces configuration property 'phoenix.shutdown.timeoutMs'
    with a default value of 5000 (5 seconds).

--- phoenix-core/src/main/java/org/apache/phoenix/query/QueryServices.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/QueryServices.java
@@ -52,6 +52,7 @@ public interface QueryServices extends SQLCloseable {
     public static final String CONSISTENCY_ATTRIB = "phoenix.connection.consistency";
     // joni byte regex engine setting
     public static final String USE_BYTE_BASED_REGEX_ATTRIB = "phoenix.regex.byteBased";
+    public static final String DRIVER_SHUTDOWN_TIMEOUT_MS = "phoenix.shutdown.timeoutMs";
 
     /**
 	 * max size to spool the the result into
