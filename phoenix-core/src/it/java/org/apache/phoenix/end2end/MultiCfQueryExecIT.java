b35cb98caece0183ce4dc29fcae4bfc80dbdc504
ramkrishna <ramkrishna.s.vasudevan@gmail.com>
2015-12-21 13:35:40 +0530

    PHOENIX-1312 Do not always project the empty column family (Ram)

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/MultiCfQueryExecIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/MultiCfQueryExecIT.java
@@ -172,17 +172,80 @@ public class MultiCfQueryExecIT extends BaseOwnClusterClientManagedTimeIT {
             assertFalse(rs.next());
             // Use E column family. Since the column family with the empty key value (the first one, A)
             // is always added to the scan, we never really use other guideposts (but this may change).
-            List<KeyRange> splits = getAllSplits(conn, "MULTI_CF", "e.cpu_utilization IS NOT NULL");
+            List<KeyRange> splits = getAllSplits(conn, "MULTI_CF", "e.cpu_utilization IS NOT NULL", "COUNT(*)");
             // Since the E column family is not populated, it won't have as many splits
             assertEquals(3, splits.size());
             // Same as above for G column family.
-            splits = getAllSplits(conn, "MULTI_CF", "g.response_time IS NOT NULL");
+            splits = getAllSplits(conn, "MULTI_CF", "g.response_time IS NOT NULL", "COUNT(*)");
             assertEquals(3, splits.size());
         } finally {
             conn.close();
         }
     }
-    
+
+    @Test
+    public void testGuidePostsRetrievedForMultiCF() throws Exception {
+      Connection conn;
+      PreparedStatement stmt;
+      ResultSet rs;
+
+      long ts = nextTimestamp();
+      Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+      props.setProperty(PhoenixRuntime.CURRENT_SCN_ATTRIB, Long.toString(ts + 10));
+      conn = DriverManager.getConnection(getUrl(), props);
+      conn.createStatement()
+              .execute(
+                      "CREATE TABLE T (  k INTEGER PRIMARY KEY, A.V1 VARCHAR, B.V2 VARCHAR, C.V3 VARCHAR)");
+      conn.close();
+
+      props.setProperty(PhoenixRuntime.CURRENT_SCN_ATTRIB, Long.toString(ts + 30));
+      conn = DriverManager.getConnection(getUrl(), props);
+      stmt = conn.prepareStatement("UPSERT INTO T VALUES(?,?,?,?)");
+      stmt.setInt(1, 1);
+      stmt.setString(2, "A");
+      stmt.setString(3, "B");
+      stmt.setString(4, "C");
+      stmt.execute();
+      conn.commit();
+      
+      stmt = conn.prepareStatement("UPSERT INTO T VALUES(?,?,?,?)");
+      stmt.setInt(1, 2);
+      stmt.setString(2, "D");
+      stmt.setString(3, "E");
+      stmt.setString(4, "F");
+      stmt.execute();
+      conn.commit();
+      
+      stmt = conn.prepareStatement("UPSERT INTO T(k, A.V1, C.V3) VALUES(?,?,?)");
+      stmt.setInt(1, 3);
+      stmt.setString(2, "E");
+      stmt.setString(3, "X");
+      stmt.execute();
+      conn.commit();
+      
+      stmt = conn.prepareStatement("UPSERT INTO T(k, A.V1, C.V3) VALUES(?,?,?)");
+      stmt.setInt(1, 4);
+      stmt.setString(2, "F");
+      stmt.setString(3, "F");
+      stmt.execute();
+      conn.commit();
+ 
+      conn.close();
+ 
+      analyzeTable(getUrl(), ts + 50, "T");
+
+      props.setProperty(PhoenixRuntime.CURRENT_SCN_ATTRIB, Long.toString(ts + 60));
+      conn = DriverManager.getConnection(getUrl(), props);
+      rs = conn.createStatement().executeQuery("SELECT B.V2 FROM T WHERE B.V2 = 'B'");
+      assertTrue(rs.next());
+      assertEquals("B",rs.getString(1));
+      List<KeyRange> splits = getAllSplits(conn, "T", "C.V3 = 'X'", "A.V1");
+      assertEquals(5, splits.size());
+      splits = getAllSplits(conn, "T", "B.V2 = 'B'", "B.V2");
+      assertEquals(3, splits.size());
+      conn.close();
+    }
+
     @Test
     public void testCFToDisambiguate2() throws Exception {
         long ts = nextTimestamp();
