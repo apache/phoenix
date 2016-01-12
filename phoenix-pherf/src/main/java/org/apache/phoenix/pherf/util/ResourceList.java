938bf9fcc9660cac16ac355340a00de94e1caae2
Mujtaba <mujtaba@apache.org>
2016-01-11 16:31:13 -0800

    PHOENIX-2563 Pherf's ResourceList should fail gracefully if a Jar file doesn't exist (elserj)

--- phoenix-pherf/src/main/java/org/apache/phoenix/pherf/util/ResourceList.java
+++ phoenix-pherf/src/main/java/org/apache/phoenix/pherf/util/ResourceList.java
@@ -24,6 +24,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
@@ -121,13 +122,17 @@ public class ResourceList {
         return retVal;
     }
 
-    private Collection<String> getResourcesFromJarFile(
+    // Visible for testing
+    Collection<String> getResourcesFromJarFile(
             final File file,
             final Pattern pattern) {
         final List<String> retVal = new ArrayList<>();
         ZipFile zf;
         try {
             zf = new ZipFile(file);
+        } catch (FileNotFoundException e) {
+            // Gracefully handle a jar listed on the classpath that doesn't actually exist.
+            return Collections.emptyList();
         } catch (final ZipException e) {
             throw new Error(e);
         } catch (final IOException e) {
