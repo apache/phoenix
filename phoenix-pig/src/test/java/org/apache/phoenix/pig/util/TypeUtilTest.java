ea65e5998c2e7ae242b2e09f0350b3219e7680a0
ravimagham <ravi.magham@bazaarvoice.com>
2016-01-21 18:56:13 -0800

    PHOENIX-2584 - Support Array datatype in phoenix-pig module

--- phoenix-pig/src/test/java/org/apache/phoenix/pig/util/TypeUtilTest.java
+++ phoenix-pig/src/test/java/org/apache/phoenix/pig/util/TypeUtilTest.java
@@ -24,44 +24,57 @@ import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.List;
+import java.util.Map;
 
-import org.apache.phoenix.pig.writable.PhoenixPigDBWritable;
+import org.apache.phoenix.mapreduce.PhoenixRecordWritable;
+import org.apache.phoenix.schema.types.PArrayDataType;
+import org.apache.phoenix.schema.types.PDouble;
+import org.apache.phoenix.schema.types.PhoenixArray;
 import org.apache.pig.ResourceSchema.ResourceFieldSchema;
 import org.apache.pig.data.DataType;
 import org.apache.pig.data.Tuple;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public class TypeUtilTest {
 
     @Test
     public void testTransformToTuple() throws Exception {
-        PhoenixPigDBWritable record = mock(PhoenixPigDBWritable.class);
-        List<Object> values = Lists.newArrayList();
-        values.add("213123");
-        values.add(1231123);
-        values.add(31231231232131L);
-        values.add("bytearray".getBytes());
-        when(record.getValues()).thenReturn(values);
+        PhoenixRecordWritable record = mock(PhoenixRecordWritable.class);
+        Double[] doubleArr =  new Double[2];
+        doubleArr[0] = 64.87;
+        doubleArr[1] = 89.96;
+        PhoenixArray arr = PArrayDataType.instantiatePhoenixArray(PDouble.INSTANCE, doubleArr);
+        Map<String,Object> values = Maps.newLinkedHashMap();
+        values.put("first", "213123");
+        values.put("second", 1231123);
+        values.put("third", 31231231232131L);
+        values.put("four", "bytearray".getBytes());
+        values.put("five", arr);
+        when(record.getResultMap()).thenReturn(values);
 
         ResourceFieldSchema field = new ResourceFieldSchema().setType(DataType.CHARARRAY);
         ResourceFieldSchema field1 = new ResourceFieldSchema().setType(DataType.INTEGER);
         ResourceFieldSchema field2 = new ResourceFieldSchema().setType(DataType.LONG);
         ResourceFieldSchema field3 = new ResourceFieldSchema().setType(DataType.BYTEARRAY);
-        ResourceFieldSchema[] projectedColumns = { field, field1, field2, field3 };
+        ResourceFieldSchema field4 = new ResourceFieldSchema().setType(DataType.TUPLE);
+        ResourceFieldSchema[] projectedColumns = { field, field1, field2, field3 , field4 }; 
 
         Tuple t = TypeUtil.transformToTuple(record, projectedColumns);
 
         assertEquals(DataType.LONG, DataType.findType(t.get(2)));
+        assertEquals(DataType.TUPLE, DataType.findType(t.get(4)));
+        Tuple doubleArrayTuple = (Tuple)t.get(4);
+        assertEquals(2,doubleArrayTuple.size());
 
         field = new ResourceFieldSchema().setType(DataType.BIGDECIMAL);
         field1 = new ResourceFieldSchema().setType(DataType.BIGINTEGER);
         values.clear();
-        values.add(new BigDecimal(123123123.123213));
-        values.add(new BigInteger("1312313231312"));
+        values.put("first", new BigDecimal(123123123.123213));
+        values.put("second", new BigInteger("1312313231312"));
         ResourceFieldSchema[] columns = { field, field1 };
+        
         t = TypeUtil.transformToTuple(record, columns);
 
         assertEquals(DataType.BIGDECIMAL, DataType.findType(t.get(0)));
