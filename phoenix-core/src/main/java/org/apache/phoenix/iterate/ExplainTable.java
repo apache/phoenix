bf6702d2d144597c90799ed5b7d6ab2c68e2b0dd
Samarth <samarth.jain@salesforce.com>
2016-06-17 12:41:46 -0700

    PHOENIX-2276 remove commented code

--- phoenix-core/src/main/java/org/apache/phoenix/iterate/ExplainTable.java
+++ phoenix-core/src/main/java/org/apache/phoenix/iterate/ExplainTable.java
@@ -286,8 +286,6 @@ public abstract class ExplainTable {
                 }
             }
             if (isLocalIndex && i == 0) {
-                    //&& ((context.getConnection().getTenantId() != null && i == 0) || (context
-                    //        .getConnection().getTenantId() == null && i == 1))) {
                 appendPKColumnValue(buf, b, isNull, i, true);
             } else {
                 appendPKColumnValue(buf, b, isNull, i, false);
