f9f792558dbba60de0e767cb2145a199d225c959
Samarth <samarth.jain@salesforce.com>
2016-06-28 09:58:43 -0700

    PHOENIX-3028 StatisticsWriter should update the stats collection timestamp asynchronously

--- phoenix-core/src/main/java/org/apache/phoenix/schema/stats/StatisticsWriter.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/stats/StatisticsWriter.java
@@ -78,10 +78,6 @@ public class StatisticsWriter implements Closeable {
         HTableInterface statsReaderTable = ServerUtil.getHTableForCoprocessorScan(env, statsWriterTable);
         StatisticsWriter statsTable = new StatisticsWriter(statsReaderTable, statsWriterTable, tableName,
                 clientTimeStamp);
-        if (clientTimeStamp != DefaultStatisticsCollector.NO_TIMESTAMP) { // Otherwise we do this later as we don't know the ts
-                                                                   // yet
-            statsTable.commitLastStatsUpdatedTime();
-        }
         return statsTable;
     }
 
@@ -193,7 +189,8 @@ public class StatisticsWriter implements Closeable {
         }
     }
 
-    public void commitStats(List<Mutation> mutations) throws IOException {
+    public void commitStats(List<Mutation> mutations, StatisticsCollector statsCollector) throws IOException {
+        commitLastStatsUpdatedTime(statsCollector);
         if (mutations.size() > 0) {
             byte[] row = mutations.get(0).getRow();
             MutateRowsRequest.Builder mrmBuilder = MutateRowsRequest.newBuilder();
@@ -220,10 +217,9 @@ public class StatisticsWriter implements Closeable {
         return put;
     }
 
-    private void commitLastStatsUpdatedTime() throws IOException {
-        // Always use wallclock time for this, as it's a mechanism to prevent
-        // stats from being collected too often.
-        Put put = getLastStatsUpdatedTimePut(clientTimeStamp);
+    private void commitLastStatsUpdatedTime(StatisticsCollector statsCollector) throws IOException {
+        long timeStamp = clientTimeStamp == StatisticsCollector.NO_TIMESTAMP ? statsCollector.getMaxTimeStamp() : clientTimeStamp;
+        Put put = getLastStatsUpdatedTimePut(timeStamp);
         statsWriterTable.put(put);
     }
 
