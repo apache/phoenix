bc9c421114f0c55d1556ffabda4a1837e8839e07
Samarth <samarth.jain@salesforce.com>
2016-09-01 10:36:43 -0700

    PHOENIX-2793 Test case for date-time functions on null dates return no data (Kevin Liew)

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/DateTimeIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/DateTimeIT.java
@@ -776,4 +776,15 @@ public class DateTimeIT extends BaseHBaseManagedTimeIT {
         assertEquals(false, rs.getBoolean(1));
         assertFalse(rs.next());
     }
+    
+    @Test
+    public void testFunctionOnNullDate() throws Exception {
+        ResultSet rs = conn.createStatement().executeQuery("SELECT YEAR(a_date), entity_id from " + ATABLE_NAME + " WHERE entity_id = '" + ROW10 + "'");
+        assertNotNull(rs);
+        assertTrue(rs.next());
+        assertEquals(ROW10, rs.getString(2));
+        assertNull(rs.getDate(1));
+        assertNull(rs.getDate(1, GregorianCalendar.getInstance()));
+        assertFalse(rs.next());
+    }
 }
