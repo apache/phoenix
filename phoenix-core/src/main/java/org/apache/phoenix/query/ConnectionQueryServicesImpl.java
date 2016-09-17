21ead442eb2f28c83ffca3ed30774228e63da170
James Taylor <jamestaylor@apache.org>
2016-09-16 21:25:00 -0700

    PHOENIX-3291 Do not throw return value of Throwables#propagate call

--- phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/ConnectionQueryServicesImpl.java
@@ -2625,7 +2625,7 @@ public class ConnectionQueryServicesImpl extends DelegateQueryServices implement
             });
         } catch (Exception e) {
             Throwables.propagateIfInstanceOf(e, SQLException.class);
-            throw Throwables.propagate(e);
+            Throwables.propagate(e);
         }
     }
 
