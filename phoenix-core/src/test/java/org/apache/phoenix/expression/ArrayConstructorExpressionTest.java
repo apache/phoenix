67027d618f3817eea9ce215dc3e2999397995b83
Thomas D'Silva <tdsilva@salesforce.com>
2016-10-05 20:58:03 -0700

    PHOENIX-3296 ArrayConstructor expression does not serialize arrays with leading nulls correctly

---
+++ phoenix-core/src/test/java/org/apache/phoenix/expression/ArrayConstructorExpressionTest.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.phoenix.expression;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.List;
+
+import org.apache.phoenix.expression.function.ArrayElemRefExpression;
+import org.apache.phoenix.hbase.index.util.ImmutableBytesPtr;
+import org.apache.phoenix.schema.types.PVarbinary;
+import org.apache.phoenix.util.ByteUtil;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+public class ArrayConstructorExpressionTest {
+    
+    private static final byte[] BYTE_ARRAY1 = new byte[]{1,2,3,4,5};
+    private static final byte[] BYTE_ARRAY2 = new byte[]{6,7,8};
+
+    @Test
+    public void testArraysWithLeadingNulls() throws Exception {
+        List<Expression> children = Lists.newArrayListWithExpectedSize(4);
+        LiteralExpression nullExpression = LiteralExpression.newConstant(null);
+        children.add(nullExpression);
+        children.add(nullExpression);
+        children.add(LiteralExpression.newConstant(BYTE_ARRAY1, PVarbinary.INSTANCE));
+        children.add(LiteralExpression.newConstant(BYTE_ARRAY2, PVarbinary.INSTANCE));
+        ArrayConstructorExpression arrayConstructorExpression = new ArrayConstructorExpression(children, PVarbinary.INSTANCE, false);
+        ImmutableBytesPtr ptr = new ImmutableBytesPtr();
+        
+        ArrayElemRefExpression arrayElemRefExpression = new ArrayElemRefExpression(Lists.<Expression>newArrayList(arrayConstructorExpression));
+        
+        arrayElemRefExpression.setIndex(1);
+        arrayElemRefExpression.evaluate(null, ptr);
+        assertArrayEquals(ByteUtil.EMPTY_BYTE_ARRAY, ptr.copyBytesIfNecessary());
+        arrayElemRefExpression.setIndex(2);
+        arrayElemRefExpression.evaluate(null, ptr);
+        assertArrayEquals(ByteUtil.EMPTY_BYTE_ARRAY, ptr.copyBytesIfNecessary());
+        arrayElemRefExpression.setIndex(3);
+        arrayElemRefExpression.evaluate(null, ptr);
+        assertArrayEquals(BYTE_ARRAY1, ptr.copyBytesIfNecessary());
+        arrayElemRefExpression.setIndex(4);
+        arrayElemRefExpression.evaluate(null, ptr);
+        assertArrayEquals(BYTE_ARRAY2, ptr.copyBytesIfNecessary());
+    }
+}
