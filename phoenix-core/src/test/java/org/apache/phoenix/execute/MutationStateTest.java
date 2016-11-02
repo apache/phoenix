184194775aa1aa79a94cc93610667bd6124f059c
James Taylor <jamestaylor@apache.org>
2016-11-02 09:59:06 -0700

    PHOENIX-3208 MutationState.toMutations method would throw a exception if multiple tables are upserted (chenglei)

--- phoenix-core/src/test/java/org/apache/phoenix/execute/MutationStateTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/execute/MutationStateTest.java
@@ -20,7 +20,20 @@ package org.apache.phoenix.execute;
 import static org.apache.phoenix.execute.MutationState.joinSortedIntArrays;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Pair;
+import org.apache.phoenix.schema.types.PUnsignedInt;
+import org.apache.phoenix.schema.types.PVarchar;
+import org.apache.phoenix.util.PhoenixRuntime;
 import org.junit.Test;
 
 public class MutationStateTest {
@@ -58,5 +71,67 @@ public class MutationStateTest {
         
         assertEquals(4, result.length);
         assertArrayEquals(new int[] {1,2,3,4}, result);
+    }
+
+    private static String getUrl() {
+        return PhoenixRuntime.JDBC_PROTOCOL + PhoenixRuntime.JDBC_PROTOCOL_SEPARATOR + PhoenixRuntime.CONNECTIONLESS;
+    }
+
+    @Test
+    public void testToMutationsOverMultipleTables() throws Exception {
+        Connection conn = null;
+        try {
+            conn=DriverManager.getConnection(getUrl());
+            conn.createStatement().execute(
+                    "create table MUTATION_TEST1"+
+                            "( id1 UNSIGNED_INT not null primary key,"+
+                    "appId1 VARCHAR)");
+            conn.createStatement().execute(
+                    "create table MUTATION_TEST2"+
+                            "( id2 UNSIGNED_INT not null primary key,"+
+                    "appId2 VARCHAR)");
+
+            conn.createStatement().execute("upsert into MUTATION_TEST1(id1,appId1) values(111,'app1')");
+            conn.createStatement().execute("upsert into MUTATION_TEST2(id2,appId2) values(222,'app2')");
+
+
+            Iterator<Pair<byte[],List<KeyValue>>> dataTableNameAndMutationKeyValuesIter =
+                    PhoenixRuntime.getUncommittedDataIterator(conn);
+
+
+            assertTrue(dataTableNameAndMutationKeyValuesIter.hasNext());
+            Pair<byte[],List<KeyValue>> pair=dataTableNameAndMutationKeyValuesIter.next();
+            String tableName1=Bytes.toString(pair.getFirst());
+            List<KeyValue> keyValues1=pair.getSecond();
+
+            assertTrue(dataTableNameAndMutationKeyValuesIter.hasNext());
+            pair=dataTableNameAndMutationKeyValuesIter.next();
+            String tableName2=Bytes.toString(pair.getFirst());
+            List<KeyValue> keyValues2=pair.getSecond();
+
+            if("MUTATION_TEST1".equals(tableName1)) {
+                assertTable(tableName1, keyValues1, tableName2, keyValues2);
+            }
+            else {
+                assertTable(tableName2, keyValues2, tableName1, keyValues1);
+            }
+            assertTrue(!dataTableNameAndMutationKeyValuesIter.hasNext());
+        }
+        finally {
+            if(conn!=null) {
+                conn.close();
+            }
+        }
+    }
+
+    private void assertTable(String tableName1,List<KeyValue> keyValues1,String tableName2,List<KeyValue> keyValues2) {
+        assertTrue("MUTATION_TEST1".equals(tableName1));
+        assertTrue(Bytes.equals(PUnsignedInt.INSTANCE.toBytes(111),CellUtil.cloneRow(keyValues1.get(0))));
+        assertTrue("app1".equals(PVarchar.INSTANCE.toObject(CellUtil.cloneValue(keyValues1.get(0)))));
+
+        assertTrue("MUTATION_TEST2".equals(tableName2));
+        assertTrue(Bytes.equals(PUnsignedInt.INSTANCE.toBytes(222),CellUtil.cloneRow(keyValues2.get(0))));
+        assertTrue("app2".equals(PVarchar.INSTANCE.toObject(CellUtil.cloneValue(keyValues2.get(0)))));
+
     }
 }
