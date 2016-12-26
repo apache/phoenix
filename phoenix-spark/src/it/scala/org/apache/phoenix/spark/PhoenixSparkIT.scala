95759f4ee340cc8d99a00a95ec18fbbde381dabb
Ankit Singhal <ankitsinghal59@gmail.com>
2016-12-26 17:02:42 +0530

    PHOENIX-3544 Fix UNSIGNED_DATE and UNSIGNED_TIME type in phoenix-spark

--- phoenix-spark/src/it/scala/org/apache/phoenix/spark/PhoenixSparkIT.scala
+++ phoenix-spark/src/it/scala/org/apache/phoenix/spark/PhoenixSparkIT.scala
@@ -631,5 +631,13 @@ class PhoenixSparkIT extends AbstractPhoenixSparkIT {
     val epoch = new Date().getTime
     assert(Math.abs(epoch - time) < 86400000)
   }
+  test("can read all Phoenix data types") {
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.load("org.apache.phoenix.spark", Map("table" -> "GIGANTIC_TABLE",
+      "zkUrl" -> quorumAddress))
+    df.save("org.apache.phoenix.spark",SaveMode.Overwrite, Map("table" -> "OUTPUT_GIGANTIC_TABLE",
+     "zkUrl" -> quorumAddress))
+    df.count() shouldEqual 1
+  }
 
 }
