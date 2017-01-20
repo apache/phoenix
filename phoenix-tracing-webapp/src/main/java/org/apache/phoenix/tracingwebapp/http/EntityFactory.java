9d64bc3ee29cf18fda7cea548e0d36dbc2f12119
Rajeshbabu Chintaguntla <rajeshbabu@apache.org>
2017-01-20 23:15:25 +0530

    PHOENIX-3613 Avoid possible SQL Injection with proper input validations(Rajeshbabu)

--- phoenix-tracing-webapp/src/main/java/org/apache/phoenix/tracingwebapp/http/EntityFactory.java
+++ phoenix-tracing-webapp/src/main/java/org/apache/phoenix/tracingwebapp/http/EntityFactory.java
@@ -39,29 +39,12 @@ public class EntityFactory {
     this.connection = connection;
   }
 
-  public Map<String, Object> findSingle(Object[] params) throws SQLException {
-    List<Map<String, Object>> objects = this.findMultiple(params);
-
-    if (objects.size() != 1) {
-      throw new SQLException("Query did not produce one object it produced: "
-          + objects.size() + " objects.");
-    }
-
-    Map<String, Object> object = objects.get(0); // get first record;
-
-    return object;
-  }
-
-  public List<Map<String, Object>> findMultiple(Object[] params)
+  public List<Map<String, Object>> findMultiple()
       throws SQLException {
     ResultSet rs = null;
     PreparedStatement ps = null;
     try {
       ps = this.connection.prepareStatement(this.queryString);
-      for (int i = 0; i < params.length; ++i) {
-        ps.setObject(1, params[i]);
-      }
-
       rs = ps.executeQuery();
       return getEntitiesFromResultSet(rs);
     } catch (SQLException e) {
