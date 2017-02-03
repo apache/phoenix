e437f22fa263e5cfe4589ae02938020701480bbf
Josh Elser <elserj@apache.org>
2017-02-03 18:45:16 -0500

    PHOENIX-1754 Try to handle keytab paths on Windows filesystems

--- phoenix-core/src/main/java/org/apache/phoenix/jdbc/PhoenixEmbeddedDriver.java
+++ phoenix-core/src/main/java/org/apache/phoenix/jdbc/PhoenixEmbeddedDriver.java
@@ -19,6 +19,7 @@ package org.apache.phoenix.jdbc;
 
 import static org.apache.phoenix.util.PhoenixRuntime.PHOENIX_TEST_DRIVER_URL_PARAM;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Driver;
@@ -36,6 +37,7 @@ import javax.annotation.concurrent.Immutable;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.security.UserGroupInformation;
@@ -199,6 +201,7 @@ public abstract class PhoenixEmbeddedDriver implements Driver, SQLCloseable {
     public static class ConnectionInfo {
         private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ConnectionInfo.class);
         private static final Object KERBEROS_LOGIN_LOCK = new Object();
+        private static final char WINDOWS_SEPARATOR_CHAR = '\\';
         private static SQLException getMalFormedUrlException(String url) {
             return new SQLExceptionInfo.Builder(SQLExceptionCode.MALFORMED_CONNECTION_URL)
             .setMessage(url).build().buildException();
@@ -249,8 +252,18 @@ public abstract class PhoenixEmbeddedDriver implements Driver, SQLCloseable {
                 }
                 tokens[nTokens++] = token;
             }
+            // Look-forward to see if the last token is actually the C:\\ path
             if (tokenizer.hasMoreTokens() && !TERMINATOR.equals(token)) {
-                throw getMalFormedUrlException(url);
+                String extraToken = tokenizer.nextToken();
+                if (WINDOWS_SEPARATOR_CHAR == extraToken.charAt(0)) {
+                  String prevToken = tokens[nTokens - 1];
+                  tokens[nTokens - 1] = prevToken + ":" + extraToken;
+                  if (tokenizer.hasMoreTokens() && !(token=tokenizer.nextToken()).equals(TERMINATOR)) {
+                      throw getMalFormedUrlException(url);
+                  }
+                } else {
+                    throw getMalFormedUrlException(url);
+                }
             }
             String quorum = null;
             Integer port = null;
@@ -280,6 +293,15 @@ public abstract class PhoenixEmbeddedDriver implements Driver, SQLCloseable {
                             principal = tokens[tokenIndex++]; // Found principal
                             if (nTokens > tokenIndex) {
                                 keytabFile = tokens[tokenIndex++]; // Found keytabFile
+                                // There's still more after, try to see if it's a windows file path
+                                if (tokenIndex < tokens.length) {
+                                    String nextToken = tokens[tokenIndex++];
+                                    // The next token starts with the directory separator, assume
+                                    // it's still the keytab path.
+                                    if (null != nextToken && WINDOWS_SEPARATOR_CHAR == nextToken.charAt(0)) {
+                                        keytabFile = keytabFile + ":" + nextToken;
+                                    }
+                                }
                             }
                         }
                     }
