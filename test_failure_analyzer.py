cecbdb3e6d01e72342f731fe1412ffc22855a46d
Rajeshbabu Chintaguntla <rajeshbabu@apache.org>
2017-04-13 15:50:35 +0530

    Tests in csv are sorted by error message.

--- test_failure_analyzer.py
+++ test_failure_analyzer.py
@@ -4,14 +4,17 @@ import xml.etree.ElementTree as ET
 import sys
 import os
 import csv
+import operator
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 fieldnames = ['classname', 'testcase', 'error_message','type','info'];
 singlelinecsvfile = open("single_line_calcite_failed_tests.csv", "w");
+failed_tests = []
 with open('calcite_failed_tests.csv', 'w') as csvfile:
-  writer = csv.DictWriter(csvfile, fieldnames,dialect='excel',delimiter=',');  
-  writer.writeheader();
+  writer = csv.writer(csvfile);
+  writer.writerow(fieldnames);
   target_dir = os.path.join(os.getcwd(),'phoenix-core/target');
   list_of_dirs = os.listdir(target_dir);
   for dir in list_of_dirs:
@@ -24,17 +27,25 @@ with open('calcite_failed_tests.csv', 'w') as csvfile:
           root = tree.getroot()
           for elem in root.iter("testcase"):
             for child in elem.getchildren():
+	      test_case_details = []
 	      if child.tag == "failure" or child.tag == "error":
 	        classname = elem.get('classname');
                 testcase = elem.get('name');
                 error_message = child.get('message');
                 type = child.get('type');
                 info = child.text;
+		test_case_details.append(classname);
+		test_case_details.append(testcase);
+		test_case_details.append(error_message);
+		test_case_details.append(type);
+		test_case_details.append(info);
+		failed_tests.append(test_case_details);
 		singlelinecsvfile.write(classname+',');
 		singlelinecsvfile.write(testcase+',');
 		singlelinecsvfile.write(type.replace('\n',"\r")+',');
 		singlelinecsvfile.write(info.replace('\n',"\r"));
 		singlelinecsvfile.write("\b")
-	        writer.writerow({'classname':classname,'testcase':testcase,'error_message':error_message,'type':type,'info':info});
+  sortedlist = sorted(failed_tests, key=operator.itemgetter(2))
+  writer.writerows(sortedlist);
 csvfile.close();
 singlelinecsvfile.close();
