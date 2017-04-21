301e961ffeb92a6ef382784b5657be51b9063b5c
Ankit Singhal <ankitsinghal59@gmail.com>
2017-04-21 11:57:58 +0530

    PHOENIX-3759 Dropping a local index causes NPE

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/index/LocalIndexIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/index/LocalIndexIT.java
@@ -599,7 +599,6 @@ public class LocalIndexIT extends BaseLocalIndexIT {
             admin.disableTable(tableName);
             copyLocalIndexHFiles(config, tableRegions.get(0), tableRegions.get(1), false);
             copyLocalIndexHFiles(config, tableRegions.get(3), tableRegions.get(0), false);
-
             admin.enableTable(tableName);
 
             int count=getCount(conn, tableName, "L#0");
@@ -607,13 +606,23 @@ public class LocalIndexIT extends BaseLocalIndexIT {
             admin.majorCompact(tableName);
             int tryCount = 5;// need to wait for rebuilding of corrupted local index region
             while (tryCount-- > 0 && count != 14) {
-                Thread.sleep(30000);
+                Thread.sleep(15000);
                 count = getCount(conn, tableName, "L#0");
             }
             assertEquals(14, count);
             rs = statement.executeQuery("SELECT COUNT(*) FROM " + indexName1);
             assertTrue(rs.next());
             assertEquals(7, rs.getLong(1));
+            statement.execute("DROP INDEX " + indexName1 + " ON " + tableName);
+            admin.majorCompact(tableName);
+            statement.execute("DROP INDEX " + indexName + " ON " + tableName);
+            admin.majorCompact(tableName);
+            Thread.sleep(15000);
+            admin.majorCompact(tableName);
+            Thread.sleep(15000);
+            rs = statement.executeQuery("SELECT COUNT(*) FROM " + tableName);
+            assertTrue(rs.next());
+            
         }
     }
 
