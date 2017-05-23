f57a97a081d9620dc4b366a489d5f35e6f45d121
gjacoby <gjacoby@salesforce.com>
2017-05-23 10:02:54 -0700

    PHOENIX-3877 - Connection throttling doesn't always decrement on connection close

--- phoenix-core/src/it/java/org/apache/phoenix/monitoring/PhoenixMetricsIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/monitoring/PhoenixMetricsIT.java
@@ -36,6 +36,7 @@ import static org.apache.phoenix.util.PhoenixRuntime.TENANT_ID_ATTRIB;
 import static org.apache.phoenix.util.PhoenixRuntime.UPSERT_BATCH_SIZE_ATTRIB;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -856,14 +857,15 @@ public class PhoenixMetricsIT extends BaseUniqueNamesOwnClusterIT {
 
     @Test
     public void testGetConnectionsThrottledForSameUrl() throws Exception {
-        int expectedPhoenixConnections = 11;
+        int attemptedPhoenixConnections = 11;
+        int maxConnections = attemptedPhoenixConnections -1;
         List<Connection> connections = Lists.newArrayList();
         String zkQuorum = "localhost:" + getUtility().getZkCluster().getClientPort();
         String url = PhoenixRuntime.JDBC_PROTOCOL + PhoenixRuntime.JDBC_PROTOCOL_SEPARATOR + zkQuorum +
         ':' +  CUSTOM_URL_STRING + '=' + "throttletest";
 
         Properties props = new Properties();
-        props.setProperty(QueryServices.CLIENT_CONNECTION_MAX_ALLOWED_CONNECTIONS, "10");
+        props.setProperty(QueryServices.CLIENT_CONNECTION_MAX_ALLOWED_CONNECTIONS, Integer.toString(maxConnections));
 
         GLOBAL_HCONNECTIONS_COUNTER.getMetric().reset();
         GLOBAL_QUERY_SERVICES_COUNTER.getMetric().reset();
@@ -871,7 +873,7 @@ public class PhoenixMetricsIT extends BaseUniqueNamesOwnClusterIT {
         GLOBAL_PHOENIX_CONNECTIONS_THROTTLED_COUNTER.getMetric().reset();
         boolean wasThrottled = false;
         try {
-            for (int k = 0; k < expectedPhoenixConnections; k++) {
+            for (int k = 0; k < attemptedPhoenixConnections; k++) {
                 connections.add(DriverManager.getConnection(url, props));
             }
         } catch (SQLException se) {
@@ -885,7 +887,25 @@ public class PhoenixMetricsIT extends BaseUniqueNamesOwnClusterIT {
         assertEquals(1, GLOBAL_QUERY_SERVICES_COUNTER.getMetric().getValue());
         assertTrue("No connection was throttled!", wasThrottled);
         assertEquals(1, GLOBAL_PHOENIX_CONNECTIONS_THROTTLED_COUNTER.getMetric().getValue());
-        assertEquals(expectedPhoenixConnections, GLOBAL_PHOENIX_CONNECTIONS_ATTEMPTED_COUNTER.getMetric().getValue());
+        assertEquals(maxConnections, connections.size());
+        assertTrue("Not all connections were attempted!",
+            attemptedPhoenixConnections <= GLOBAL_PHOENIX_CONNECTIONS_ATTEMPTED_COUNTER.getMetric().getValue());
+        connections.clear();
+        //now check that we decremented the counter for the connections we just released
+        try {
+            for (int k = 0; k < maxConnections; k++){
+                connections.add(DriverManager.getConnection(url, props));
+            }
+        } catch(SQLException se) {
+            if (se.getErrorCode() == (SQLExceptionCode.NEW_CONNECTION_THROTTLED).getErrorCode()){
+                fail("Connection was throttled when it shouldn't be!");
+            }
+        } finally {
+            for (Connection c : connections) {
+                c.close();
+            }
+        }
+        assertEquals(maxConnections, connections.size());
     }
 
     @Test
