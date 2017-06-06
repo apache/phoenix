58b2604455f887af6b6cbebda70501e053045bac
Ankit Singhal <ankitsinghal59@gmail.com>
2017-06-06 13:24:58 -0700

    PHOENIX-3797 Local Index - Compaction fails on table with local index due to non-increasing bloom keys

--- phoenix-core/src/main/java/org/apache/hadoop/hbase/regionserver/DataTableLocalIndexRegionScanner.java
+++ phoenix-core/src/main/java/org/apache/hadoop/hbase/regionserver/DataTableLocalIndexRegionScanner.java
@@ -17,20 +17,36 @@
  */
 package org.apache.hadoop.hbase.regionserver;
 
+import static org.apache.phoenix.query.QueryServices.MUTATE_BATCH_SIZE_ATTRIB;
+import static org.apache.phoenix.query.QueryServices.MUTATE_BATCH_SIZE_BYTES_ATTRIB;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Mutation;
+import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.phoenix.coprocessor.DelegateRegionScanner;
+import org.apache.phoenix.coprocessor.UngroupedAggregateRegionObserver;
+import org.apache.phoenix.coprocessor.UngroupedAggregateRegionObserver.MutationList;
 import org.apache.phoenix.hbase.index.ValueGetter;
 import org.apache.phoenix.hbase.index.util.GenericKeyValueBuilder;
 import org.apache.phoenix.hbase.index.util.ImmutableBytesPtr;
 import org.apache.phoenix.hbase.index.util.KeyValueBuilder;
 import org.apache.phoenix.index.IndexMaintainer;
+import org.apache.phoenix.query.QueryServicesOptions;
 import org.apache.phoenix.schema.tuple.MultiKeyValueTuple;
-
+import org.apache.phoenix.util.ServerUtil;
+/*
+ * Scanner to read data store and regenerate the local index data
+ */
 public class DataTableLocalIndexRegionScanner extends DelegateRegionScanner {
     MultiKeyValueTuple result = new MultiKeyValueTuple();
     ImmutableBytesWritable ptr = new ImmutableBytesWritable();
@@ -39,22 +55,43 @@ public class DataTableLocalIndexRegionScanner extends DelegateRegionScanner {
     private byte[] startKey;
     private byte[] endKey;
     private byte[] localIndexFamily;
+    private HRegion region;
+    long maxBatchSizeBytes;
+    int maxBatchSize;
+    private MutationList mutationList;
 
+    /**
+     * @param scanner Scanner for data table stores
+     * @param region
+     * @param indexMaintainers Maintainer of local Indexes which needs to built
+     * @param localIndexFamily LocalIndex family needs to be built.
+     * @param conf
+     * @throws IOException
+     */    
     public DataTableLocalIndexRegionScanner(RegionScanner scanner, HRegion region,
-            List<IndexMaintainer> indexMaintainers, byte[] localIndexFamily) throws IOException {
+            List<IndexMaintainer> indexMaintainers, byte[] localIndexFamily,Configuration conf) throws IOException {
         super(scanner);
         this.indexMaintainers = indexMaintainers;
         this.startKey = region.getRegionInfo().getStartKey();
         this.endKey = region.getRegionInfo().getEndKey();
         this.localIndexFamily = localIndexFamily;
+        this.region=region;
+        maxBatchSize = conf.getInt(MUTATE_BATCH_SIZE_ATTRIB, QueryServicesOptions.DEFAULT_MUTATE_BATCH_SIZE);
+        maxBatchSizeBytes = conf.getLong(MUTATE_BATCH_SIZE_BYTES_ATTRIB,
+            QueryServicesOptions.DEFAULT_MUTATE_BATCH_SIZE_BYTES);
+        mutationList=new UngroupedAggregateRegionObserver.MutationList(maxBatchSize);
     }
-   
-
+    
     @Override
-    public boolean next(List<Cell> outResult) throws IOException {
+    public boolean next(List<Cell> results) throws IOException {
         List<Cell> dataTableResults = new ArrayList<Cell>();
         boolean next = super.next(dataTableResults);
-        getLocalIndexCellsFromDataTable(dataTableResults, outResult);
+        addMutations(dataTableResults);
+        if (ServerUtil.readyToCommit(mutationList.size(), mutationList.byteSize(), maxBatchSize, maxBatchSizeBytes)||!next) {
+            region.batchMutate(mutationList.toArray(new Mutation[mutationList.size()]), HConstants.NO_NONCE,
+                    HConstants.NO_NONCE);
+            mutationList.clear();
+        }
         return next;
     }
 
@@ -62,11 +99,16 @@ public class DataTableLocalIndexRegionScanner extends DelegateRegionScanner {
     public boolean next(List<Cell> results, int limit) throws IOException {
         List<Cell> dataTableResults = new ArrayList<Cell>();
         boolean next = super.next(dataTableResults, limit);
-        getLocalIndexCellsFromDataTable(dataTableResults, results);
+        addMutations(dataTableResults);
+        if (ServerUtil.readyToCommit(mutationList.size(), mutationList.byteSize(), maxBatchSize, maxBatchSizeBytes)||!next) {
+            region.batchMutate(mutationList.toArray(new Mutation[mutationList.size()]), HConstants.NO_NONCE,
+                    HConstants.NO_NONCE);
+            mutationList.clear();
+        }
         return next;
     }
 
-    private void getLocalIndexCellsFromDataTable(List<Cell> dataTableResults, List<Cell> localIndexResults)
+    private void addMutations(List<Cell> dataTableResults)
             throws IOException {
         if (!dataTableResults.isEmpty()) {
             result.setKeyValues(dataTableResults);
@@ -77,11 +119,26 @@ public class DataTableLocalIndexRegionScanner extends DelegateRegionScanner {
                 List<Cell> list = maintainer.buildUpdateMutation(kvBuilder, valueGetter, ptr,
                         dataTableResults.get(0).getTimestamp(), startKey, endKey).getFamilyCellMap()
                         .get(localIndexFamily);
-                if (list != null) {
-                    localIndexResults.addAll(list);
+                Put put = null;
+                Delete del = null;
+                for (Cell cell : list) {
+                    if (KeyValue.Type.codeToType(cell.getTypeByte()) == KeyValue.Type.Put) {
+                        if (put == null) {
+                            put = new Put(CellUtil.cloneRow(cell));
+                            mutationList.add(put);
+                        }
+                        put.add(cell);
+                    } else {
+                        if (del == null) {
+                            del = new Delete(CellUtil.cloneRow(cell));
+                            mutationList.add(del);
+                        }
+                        del.addDeleteMarker(cell);
+                    }
                 }
             }
         }
     }
+    
 
 }
