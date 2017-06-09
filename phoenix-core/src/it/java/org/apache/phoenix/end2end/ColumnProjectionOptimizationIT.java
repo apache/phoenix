edad3503df2665ce2fbc54a3e3215a20b751f2a3
James Taylor <jamestaylor@apache.org>
2017-06-09 12:04:51 -0700

    Revert "PHOENIX-3930 Move BaseQueryIT to ParallelStatsDisabledIT"
    This reverts commit 95f7a3ec879e9ee59e7a83eb4aab72d15fb416b7.

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/ColumnProjectionOptimizationIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/ColumnProjectionOptimizationIT.java
@@ -63,6 +63,7 @@ import org.junit.Test;
 
 public class ColumnProjectionOptimizationIT extends BaseClientManagedTimeIT {
 
+    private String tableName; 
     @Test
     public void testSelect() throws Exception {
         long ts = nextTimestamp();
