e37387d653146b3a1c89d024c840f0a184bbbd2f
Rajeshbabu Chintaguntla <rajeshbabu@apache.org>
2017-06-29 10:42:16 +0530

    PHOENIX-3984 PDataType to Calcite type conversion should consider whether precision or scale allowed for type in Calcite-addendum

--- phoenix-core/src/main/java/org/apache/phoenix/calcite/CalciteUtils.java
+++ phoenix-core/src/main/java/org/apache/phoenix/calcite/CalciteUtils.java
@@ -228,14 +228,22 @@ public class CalciteUtils {
         final PDataType normalizedBaseType = PDataType.fromTypeId(sqlTypeId);
         final SqlTypeName sqlTypeName = SqlTypeName.valueOf(normalizedBaseType.getSqlTypeName());
         RelDataType type;
-        if (sqlTypeName.allowsNoPrecNoScale()
-                || (sqlTypeName.allowsPrecNoScale() && maxLength == null)
-                || (sqlTypeName.allowsScale() && maxLength == null && scale == null)) {
-            type = typeFactory.createSqlType(sqlTypeName);
+        if(sqlTypeName.allowsScale()) {
+            if(maxLength != null && scale != null) {
+                type = typeFactory.createSqlType(sqlTypeName, maxLength, scale);
+            } else if(maxLength != null) {
+                type = typeFactory.createSqlType(sqlTypeName, maxLength);
+            } else {
+                type = typeFactory.createSqlType(sqlTypeName);
+            }
         } else if(sqlTypeName.allowsPrecNoScale()) {
-            type = typeFactory.createSqlType(sqlTypeName, maxLength);
+            if(maxLength != null) {
+                type = typeFactory.createSqlType(sqlTypeName, maxLength);
+            } else {
+                type = typeFactory.createSqlType(sqlTypeName);
+            }
         } else {
-            type = typeFactory.createSqlType(sqlTypeName, maxLength, scale);
+            type = typeFactory.createSqlType(sqlTypeName);
         }
         if (isArrayType) {
             type = typeFactory.createArrayType(type, arraySize == null ? -1 : arraySize);
