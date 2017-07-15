2987741d3a09e4fd3af21ce23edb0e9dee666277
Vincent <vincentpoon@gmail.com>
2017-07-14 17:16:40 -0700

    PHOENIX-3948 Enable shorter time outs for server-side index writes
    Signed-off-by: gjacoby <gjacoby@apache.org>

--- phoenix-core/src/main/java/org/apache/phoenix/hbase/index/write/IndexWriterUtils.java
+++ phoenix-core/src/main/java/org/apache/phoenix/hbase/index/write/IndexWriterUtils.java
@@ -68,7 +68,18 @@ public class IndexWriterUtils {
   public static final String HTABLE_THREAD_KEY = "hbase.htable.threads.max";
    public static final String INDEX_WRITES_THREAD_MAX_PER_REGIONSERVER_KEY = "phoenix.index.writes.threads.max";
    public static final String HTABLE_KEEP_ALIVE_KEY = "hbase.htable.threads.keepalivetime";
-   
+
+   public static final String INDEX_WRITER_RPC_RETRIES_NUMBER = "phoenix.index.writes.rpc.retries.number";
+   /**
+    * Based on the logic in HBase's AsyncProcess, a default of 11 retries with a pause of 100ms
+    * approximates 48 sec total retry time (factoring in backoffs).  The total time should be less
+    * than HBase's rpc timeout (default of 60 sec) or else the client will retry before receiving
+    * the response
+    */
+   public static final int DEFAULT_INDEX_WRITER_RPC_RETRIES_NUMBER = 11;
+   public static final String INDEX_WRITER_RPC_PAUSE = "phoenix.index.writes.rpc.pause";
+   public static final int DEFAULT_INDEX_WRITER_RPC_PAUSE = 100;
+
   private IndexWriterUtils() {
     // private ctor for utilites
   }
