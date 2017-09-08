cc36a9a476e4d7bcd23ddda84ae1e5adbf47ddf1
aertoria <castives@gmail.com>
2017-09-08 13:25:28 -0700

    PHOENIX-4150 nit: adding license and reformating comments
    Signed-off-by: Thomas D'Silva <tdsilva@apache.org>

--- phoenix-core/src/test/java/org/apache/phoenix/query/TestPropertyPolicy.java
+++ phoenix-core/src/test/java/org/apache/phoenix/query/TestPropertyPolicy.java
@@ -1,7 +1,19 @@
 /*
- * Copyright, 1999-2013, SALESFORCE.com
- * All Rights Reserved
- * Company Confidential
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.phoenix.query;
 
@@ -19,16 +31,17 @@ import static java.util.Arrays.asList;
  * configuration settings.
  */
 public class TestPropertyPolicy implements PropertyPolicy {
-  final static Set<String> propertiesKeyDisAllowed = Collections.unmodifiableSet(
-      new HashSet<>(asList("DisallowedProperty")));
+  final static Set<String> propertiesKeyDisAllowed =
+      Collections.unmodifiableSet(new HashSet<>(asList("DisallowedProperty")));
 
   @Override public void evaluate(Properties properties) throws PropertyNotAllowedException {
     final Properties offendingProperties = new Properties();
 
-    for(Object k:properties.keySet()){
-      if (propertiesKeyDisAllowed.contains(k)) offendingProperties.put((String)k,properties.getProperty((String)k));
+    for (Object k : properties.keySet()) {
+      if (propertiesKeyDisAllowed.contains(k))
+        offendingProperties.put((String) k, properties.getProperty((String) k));
     }
 
-    if (offendingProperties.size()>0) throw new PropertyNotAllowedException(offendingProperties);
+    if (offendingProperties.size() > 0) throw new PropertyNotAllowedException(offendingProperties);
   }
 }
