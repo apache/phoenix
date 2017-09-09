580b035f09fd492325719ca8b2647e99c41010d6
Josh Elser <elserj@apache.org>
2017-09-09 00:00:57 -0400

    PHOENIX-4189 Introduce a class that wraps the Map of primary key data

--- phoenix-hive/src/main/java/org/apache/phoenix/hive/PhoenixRowKey.java
+++ phoenix-hive/src/main/java/org/apache/phoenix/hive/PhoenixRowKey.java
@@ -17,15 +17,12 @@
  */
 package org.apache.phoenix.hive;
 
-import com.google.common.collect.Maps;
 import org.apache.hadoop.hive.ql.io.RecordIdentifier;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
@@ -35,33 +32,29 @@ import java.util.Map;
 
 public class PhoenixRowKey extends RecordIdentifier {
 
-    private Map<String, Object> rowKeyMap = Maps.newHashMap();
+    private PrimaryKeyData rowKeyMap = PrimaryKeyData.EMPTY;
 
     public PhoenixRowKey() {
 
     }
 
     public void setRowKeyMap(Map<String, Object> rowKeyMap) {
-        this.rowKeyMap = rowKeyMap;
+        this.rowKeyMap = new PrimaryKeyData(rowKeyMap);
     }
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         super.write(dataOutput);
 
-        try (ObjectOutputStream oos = new ObjectOutputStream((OutputStream) dataOutput)) {
-            oos.writeObject(rowKeyMap);
-            oos.flush();
-        }
+        rowKeyMap.serialize((OutputStream) dataOutput);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public void readFields(DataInput dataInput) throws IOException {
         super.readFields(dataInput);
 
-        try (ObjectInputStream ois = new ObjectInputStream((InputStream) dataInput)) {
-            rowKeyMap = (Map<String, Object>) ois.readObject();
+        try {
+            rowKeyMap = PrimaryKeyData.deserialize((InputStream) dataInput);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
