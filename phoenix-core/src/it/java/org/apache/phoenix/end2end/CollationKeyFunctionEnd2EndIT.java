8cc2b5c668a1386ddce9d740cb93d3e9d68d34df
Shehzaad <snakhoda@salesforce.com>
2017-10-17 11:42:21 -0700

    (1) add end2end test for ordering by COLLKEY and (2) use hex representation bytes to check collation key output to make byte array easier to read (as opposed to numbers which can be signed)

---
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/CollationKeyFunctionEnd2EndIT.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.phoenix.end2end;
+
+import static org.apache.phoenix.util.TestUtil.closeStmtAndConn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CollationKeyFunctionEnd2EndIT extends ParallelStatsDisabledIT {
+
+	private String tableName;
+	private String[] dataArray = new String[] { "\u963f", "\u55c4", "\u963e", "\u554a", "\u4ec8", "\u3d9a", "\u9f51" };
+
+	@Before
+	public void initAndPopulateTable() throws Exception {
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		tableName = generateUniqueName();
+		try {
+			conn = DriverManager.getConnection(getUrl());
+			String ddl = "CREATE TABLE " + tableName + " (id INTEGER PRIMARY KEY, data VARCHAR)";
+			conn.createStatement().execute(ddl);
+
+			// insert dataArray into the table, with the index into the array as
+			// the id
+			for (int i = 0; i < dataArray.length; i++) {
+				PreparedStatement ps = conn.prepareStatement("upsert into " + tableName + " values(?, ?)");
+				ps.setInt(1, i);
+				ps.setString(2, dataArray[i]);
+				ps.executeUpdate();
+			}
+			conn.commit();
+		} finally {
+			closeStmtAndConn(stmt, conn);
+		}
+	}
+
+	@Test
+	public void testZhSort() throws Exception {
+		queryDataColumnWithCollKeyOrdering("zh", new Integer[] { 3, 0, 1, 6, 5, 4, 2 });
+	}
+
+	@Test
+	public void testZhTwSort() throws Exception {
+		queryDataColumnWithCollKeyOrdering("zh_TW", new Integer[] { 0, 3, 4, 1, 5, 2, 6 });
+	}
+
+	@Test
+	public void testZhTwStrokeSort() throws Exception {
+		queryDataColumnWithCollKeyOrdering("zh_TW_STROKE", new Integer[] { 4, 2, 0, 3, 1, 6, 5 });
+	}
+
+	@Test
+	public void testZhStrokeSort() throws Exception {
+		queryDataColumnWithCollKeyOrdering("zh__STROKE", new Integer[] { 0, 1, 3, 4, 6, 2, 5 });
+	}
+
+	@Test
+	public void testZhPinyinSort() throws Exception {
+		queryDataColumnWithCollKeyOrdering("zh__PINYIN", new Integer[] { 0, 1, 3, 4, 6, 2, 5 });
+	}
+
+	/**
+	 * Issue a query ordered by the collation key of the data column according to the provided localeString,
+	 * and compare the ID and data columns to the expected order.
+	 * 
+	 * @param expectedIndexOrder
+	 *            an array of indexes into the dataArray in the order we expect.
+	 *            This is the same as the ID column
+	 * @throws SQLException
+	 */
+	private void queryDataColumnWithCollKeyOrdering(String localeString, Integer[] expectedIndexOrder)
+			throws SQLException {
+		String query = String.format("SELECT id, data FROM %s ORDER BY COLLKEY(data, '%s')", tableName, localeString);
+
+		Connection conn = DriverManager.getConnection(getUrl());
+		PreparedStatement ps = conn.prepareStatement(query);
+		ResultSet rs = ps.executeQuery();
+		int i = 0;
+		while (rs.next()) {
+			int expectedId = expectedIndexOrder[i];
+			assertEquals("For row " + i + "The ID did not match the expected index", expectedId, rs.getInt(1));
+			assertEquals("For row " + i + "The data did not match the expected entry from the data array",
+					dataArray[expectedId], rs.getString(2));
+			i++;
+		}
+		assertEquals("The result set returned a different number of rows from the data array", dataArray.length, i);
+	}
+}
--- phoenix-core/src/test/java/org/apache/phoenix/expression/function/CollationKeyFunctionTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/expression/function/CollationKeyFunctionTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.fail;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Hex;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.phoenix.expression.function.CollationKeyFunction;
 import org.apache.phoenix.schema.SortOrder;
 import org.apache.phoenix.schema.types.PBoolean;
 import org.apache.phoenix.schema.types.PInteger;
 import org.apache.phoenix.schema.types.PVarchar;
-import org.apache.phoenix.schema.types.PhoenixArray;
 
 import org.apache.phoenix.expression.Expression;
 import org.apache.phoenix.expression.LiteralExpression;
@@ -33,61 +33,61 @@ public class CollationKeyFunctionTest {
 	public void testChineseCollationKeyBytes() throws Exception {
 		
 		// Chinese (China)
-		test("\u963f", "zh", new Integer[] { 2, 235, 0, 0, 0, 1, 0, 0 });
-		test("\u55c4", "zh", new Integer[] { 20, 173, 0, 0, 0, 1, 0, 0 });
-		test("\u963e", "zh", new Integer[] { 128, 0, 150, 63, 0, 0, 0, 1, 0, 1, 0, 0 });
-		test("\u554a", "zh", new Integer[] { 2, 234, 0, 0, 0, 1, 0, 0 });
-		test("\u4ec8", "zh", new Integer[] { 128, 0, 78, 201, 0, 0, 0, 1, 0, 1, 0, 0 });
-		test("\u3d9a", "zh", new Integer[] { 128, 0, 61, 155, 0, 0, 0, 1, 0, 1, 0, 0 });
-		test("\u9f51", "zh", new Integer[] { 25, 5, 0, 0, 0, 1, 0, 0 });
+		test("\u963f", "zh", "02eb000000010000");
+		test("\u55c4", "zh", "14ad000000010000");
+		test("\u963e", "zh", "8000963f0000000100010000");
+		test("\u554a", "zh", "02ea000000010000");
+		test("\u4ec8", "zh", "80004ec90000000100010000");
+		test("\u3d9a", "zh", "80003d9b0000000100010000");
+		test("\u9f51", "zh", "1905000000010000");
 		
 		// Chinese (Taiwan)
-		test("\u963f", "zh_TW", new Integer[] { 6, 61, 0, 0, 0, 1, 0, 0 });
-		test("\u55c4", "zh_TW", new Integer[] { 36, 30, 0, 0, 0, 1, 0, 0 });
-		test("\u963e", "zh_TW", new Integer[] { 128, 0, 150, 63, 0, 0, 0, 1, 0, 1, 0, 0 });
-		test("\u554a", "zh_TW", new Integer[] { 9, 201, 0, 0, 0, 1, 0, 0 });
-		test("\u4ec8", "zh_TW", new Integer[] { 24, 27, 0, 0, 0, 1, 0, 0 });
-		test("\u3d9a", "zh_TW", new Integer[] { 128, 0, 61, 155, 0, 0, 0, 1, 0, 1, 0, 0 });
-		test("\u9f51", "zh_TW", new Integer[] { 128, 0, 159, 82, 0, 0, 0, 1, 0, 1, 0, 0 });
+		test("\u963f", "zh_TW", "063d000000010000");
+		test("\u55c4", "zh_TW", "241e000000010000");
+		test("\u963e", "zh_TW", "8000963f0000000100010000");
+		test("\u554a", "zh_TW", "09c9000000010000");
+		test("\u4ec8", "zh_TW", "181b000000010000");
+		test("\u3d9a", "zh_TW", "80003d9b0000000100010000");
+		test("\u9f51", "zh_TW", "80009f520000000100010000");
 		
 		// Chinese (Taiwan, Stroke)
-		test("\u963f", "zh_TW_STROKE", new Integer[] { 84, 80, 1, 5, 0 });
-		test("\u55c4", "zh_TW_STROKE", new Integer[] { 115, 52, 1, 5, 0 });
-		test("\u963e", "zh_TW_STROKE", new Integer[] { 84, 79, 1, 5, 0 });
-		test("\u554a", "zh_TW_STROKE", new Integer[] { 98, 222, 1, 5, 0 });
-		test("\u4ec8", "zh_TW_STROKE", new Integer[] { 70, 190, 1, 5, 0 });
-		test("\u3d9a", "zh_TW_STROKE", new Integer[] { 165, 3, 146, 1, 5, 0 });
-		test("\u9f51", "zh_TW_STROKE", new Integer[] { 137, 21, 1, 5, 0 });
+		test("\u963f", "zh_TW_STROKE", "5450010500");
+		test("\u55c4", "zh_TW_STROKE", "7334010500");
+		test("\u963e", "zh_TW_STROKE", "544f010500");
+		test("\u554a", "zh_TW_STROKE", "62de010500");
+		test("\u4ec8", "zh_TW_STROKE", "46be010500");
+		test("\u3d9a", "zh_TW_STROKE", "a50392010500");
+		test("\u9f51", "zh_TW_STROKE", "8915010500");
 		
 		// Chinese (China, Stroke)
-		test("\u963f", "zh__STROKE", new Integer[] { 40, 1, 5, 0 });
-		test("\u55c4", "zh__STROKE", new Integer[] { 42, 1, 5, 0 });
-		test("\u963e", "zh__STROKE", new Integer[] { 117, 117, 1, 5, 0 });
-		test("\u554a", "zh__STROKE", new Integer[] { 43, 1, 5, 0 });
-		test("\u4ec8", "zh__STROKE", new Integer[] { 81, 161, 1, 5, 0 });
-		test("\u3d9a", "zh__STROKE", new Integer[] { 165, 3, 146, 1, 5, 0 });
-		test("\u9f51", "zh__STROKE", new Integer[] { 105, 53, 1, 5, 0 });
+		test("\u963f", "zh__STROKE", "28010500");
+		test("\u55c4", "zh__STROKE", "2a010500");
+		test("\u963e", "zh__STROKE", "7575010500");
+		test("\u554a", "zh__STROKE", "2b010500");
+		test("\u4ec8", "zh__STROKE", "51a1010500");
+		test("\u3d9a", "zh__STROKE", "a50392010500");
+		test("\u9f51", "zh__STROKE", "6935010500");
 		
 		// Chinese (China, Pinyin)
-		test("\u963f", "zh__PINYIN", new Integer[] { 40, 1, 5, 0 });
-		test("\u55c4", "zh__PINYIN", new Integer[] { 42, 1, 5, 0 });
-		test("\u963e", "zh__PINYIN", new Integer[] { 117, 117, 1, 5, 0 });
-		test("\u554a", "zh__PINYIN", new Integer[] { 43, 1, 5, 0 });
-		test("\u4ec8", "zh__PINYIN", new Integer[] { 81, 161, 1, 5, 0 });
-		test("\u3d9a", "zh__PINYIN", new Integer[] { 165, 3, 146, 1, 5, 0 });
-		test("\u9f51", "zh__PINYIN", new Integer[] { 105, 53, 1, 5, 0 });
+		test("\u963f", "zh__PINYIN", "28010500");
+		test("\u55c4", "zh__PINYIN", "2a010500");
+		test("\u963e", "zh__PINYIN", "7575010500");
+		test("\u554a", "zh__PINYIN", "2b010500");
+		test("\u4ec8", "zh__PINYIN", "51a1010500");
+		test("\u3d9a", "zh__PINYIN", "a50392010500");
+		test("\u9f51", "zh__PINYIN", "6935010500");
 		
 	}
 
-	private static void test(String inputStr, String localeIsoCode, Integer[] expectedCollationKeyBytes)
-			throws SQLException {
-		boolean ret1 = testExpression(inputStr, localeIsoCode, SortOrder.ASC, expectedCollationKeyBytes);
-		boolean ret2 = testExpression(inputStr, localeIsoCode, SortOrder.DESC, expectedCollationKeyBytes);
+	private static void test(String inputStr, String localeIsoCode, String expectedCollationKeyBytesHex)
+			throws Exception {
+		boolean ret1 = testExpression(inputStr, localeIsoCode, SortOrder.ASC, expectedCollationKeyBytesHex);
+		boolean ret2 = testExpression(inputStr, localeIsoCode, SortOrder.DESC, expectedCollationKeyBytesHex);
 		assertEquals(ret1, ret2);
 	}
 
 	private static boolean testExpression(String inputStr, String localeIsoCode, SortOrder sortOrder,
-			Integer[] expectedCollationKeyBytes) throws SQLException {
+			String expectedCollationKeyBytesHex) throws Exception {
 		LiteralExpression inputStrLiteral, localeIsoCodeLiteral, upperCaseBooleanLiteral, strengthLiteral,
 				decompositionLiteral;
 		inputStrLiteral = LiteralExpression.newConstant(inputStr, PVarchar.INSTANCE, sortOrder);
@@ -96,13 +96,13 @@ public class CollationKeyFunctionTest {
 		strengthLiteral = LiteralExpression.newConstant(null, PInteger.INSTANCE, sortOrder);
 		decompositionLiteral = LiteralExpression.newConstant(null, PInteger.INSTANCE, sortOrder);
 		boolean ret = testExpression(inputStrLiteral, localeIsoCodeLiteral, upperCaseBooleanLiteral, strengthLiteral,
-				decompositionLiteral, new PhoenixArray(PInteger.INSTANCE, expectedCollationKeyBytes));
+				decompositionLiteral, expectedCollationKeyBytesHex);
 		return ret;
 	}
 
 	private static boolean testExpression(LiteralExpression inputStrLiteral, LiteralExpression localeIsoCodeLiteral,
 			LiteralExpression upperCaseBooleanLiteral, LiteralExpression strengthLiteral,
-			LiteralExpression decompositionLiteral, PhoenixArray expectedCollationKeyByteArray) throws SQLException {
+			LiteralExpression decompositionLiteral, String expectedCollationKeyBytesHex) throws Exception {
 		List<Expression> expressions = Lists.newArrayList((Expression) inputStrLiteral,
 				(Expression) localeIsoCodeLiteral, (Expression) upperCaseBooleanLiteral, (Expression) strengthLiteral,
 				(Expression) decompositionLiteral);
@@ -110,19 +110,21 @@ public class CollationKeyFunctionTest {
 		ImmutableBytesWritable ptr = new ImmutableBytesWritable();
 		boolean ret = collationKeyFunction.evaluate(null, ptr);
 		if (ret) {
-			PhoenixArray result = (PhoenixArray) collationKeyFunction.getDataType().toObject(ptr,
+			byte[] result = (byte[]) collationKeyFunction.getDataType().toObject(ptr,
 					collationKeyFunction.getSortOrder());
 
+			byte[] expectedCollationKeyByteArray = Hex.decodeHex(expectedCollationKeyBytesHex.toCharArray());
+			
 			assertEquals(
 					String.format("Size of collation key byte arrays was not as expected for [%s], [%s]",
 							inputStrLiteral.getValue(), localeIsoCodeLiteral.getValue()),
-					expectedCollationKeyByteArray.getDimensions(), result.getDimensions());
-			for (int i = 0; i < expectedCollationKeyByteArray.getDimensions(); i++) {
+					expectedCollationKeyByteArray.length, result.length);
+			for (int i = 0; i < expectedCollationKeyByteArray.length; i++) {
 				assertEquals(
 						String.format(
 								"Element " + i + " of collation key byte array was not as expected for [%s], [%s]",
 								inputStrLiteral.getValue(), localeIsoCodeLiteral.getValue()),
-						expectedCollationKeyByteArray.getElement(i), result.getElement(i));
+						expectedCollationKeyByteArray[i], result[i]);
 			}
 
 		} else {
