c594038a1bee96b92edd9c788969e3e2dbd6db5a
James Taylor <jtaylor@salesforce.com>
2017-10-18 09:29:44 -0700

    PHOENIX-4295 Fix argument order for StatsCollectorIT derived classes

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/NonColumnEncodedImmutableNonTxStatsCollectorIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/NonColumnEncodedImmutableNonTxStatsCollectorIT.java
@@ -29,10 +29,9 @@ public class NonColumnEncodedImmutableNonTxStatsCollectorIT extends StatsCollect
         super(mutable, transactional, userTableNamespaceMapped, columnEncoded);
     }
 
-    @Parameters(
-            name = "columnEncoded = {0}, mutable = {1}, transactional = {2}, isUserTableNamespaceMapped = {3}")
+    @Parameters(name = "mutable = {0}, transactional = {1}, isUserTableNamespaceMapped = {2}, columnEncoded = {3}")
     public static Collection<Boolean[]> data() {
         return Arrays.asList(
-            new Boolean[][] { { false, false, false, false }, { false, false, false, true } });
+            new Boolean[][] { { false, false, false, false }, { false, false, true, false } });
     }
 }
--- phoenix-core/src/it/java/org/apache/phoenix/end2end/SysTableNamespaceMappedStatsCollectorIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/SysTableNamespaceMappedStatsCollectorIT.java
@@ -35,8 +35,7 @@ public class SysTableNamespaceMappedStatsCollectorIT extends StatsCollectorIT {
         super(mutable, transactional, userTableNamespaceMapped, columnEncoded);
     }
 
-    @Parameters(
-            name = "columnEncoded = {0}, mutable = {1}, transactional = {2}, isUserTableNamespaceMapped = {3}")
+    @Parameters(name = "mutable = {0}, transactional = {1}, isUserTableNamespaceMapped = {2}, columnEncoded = {3}")
     public static Collection<Boolean[]> data() {
         return Arrays.asList(
             new Boolean[][] { { true, true, false, false }, { true, true, false, true }, });
