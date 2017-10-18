9c7dbab8573f47dec217c41df532083828b3f663
James Taylor <jtaylor@salesforce.com>
2017-10-18 09:29:44 -0700

    PHOENIX-4295 Fix argument order for StatsCollectorIT derived classes

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/ColumnEncodedImmutableNonTxStatsCollectorIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/ColumnEncodedImmutableNonTxStatsCollectorIT.java
@@ -29,10 +29,10 @@ public class ColumnEncodedImmutableNonTxStatsCollectorIT extends StatsCollectorI
         super(mutable, transactional, userTableNamespaceMapped, columnEncoded);
     }
 
-    @Parameters(name="columnEncoded = {0}, mutable = {1}, transactional = {2}, isUserTableNamespaceMapped = {3}")
+    @Parameters(name = "mutable = {0}, transactional = {1}, isUserTableNamespaceMapped = {2}, columnEncoded = {3}")
     public static Collection<Boolean[]> data() {
         return Arrays.asList(new Boolean[][] {     
-                { true, false, false, false }, { true, false, false, true }
+                { false, false, false, true }, { false, false, true, true }
         });
     }
 }
