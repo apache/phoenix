7c21a83df97878f421464147d29a9dfd2d636870
James Taylor <jtaylor@salesforce.com>
2017-10-29 15:19:23 -0700

    PHOENIX-4277 Treat delete markers consistently with puts for point-in-time scans

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/PointInTimeQueryIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/PointInTimeQueryIT.java
@@ -63,7 +63,7 @@ public class PointInTimeQueryIT extends BaseQueryIT {
     public PointInTimeQueryIT(String idxDdl, boolean columnEncoded)
             throws Exception {
         // These queries fail without KEEP_DELETED_CELLS=true
-        super(idxDdl, columnEncoded, true);
+        super(idxDdl, columnEncoded, false);
     }
 
     @Test
