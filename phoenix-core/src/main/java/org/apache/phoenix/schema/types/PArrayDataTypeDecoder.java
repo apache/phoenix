b69f1875e5021849d93a8673e2223a449312a8a4
Vincent Poon <vincentpoon@apache.org>
2017-12-27 15:52:39 -0800

    PHOENIX-4382 Immutable table SINGLE_CELL_ARRAY_WITH_OFFSETS values starting with separator byte return null in query results

--- phoenix-core/src/main/java/org/apache/phoenix/schema/types/PArrayDataTypeDecoder.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/types/PArrayDataTypeDecoder.java
@@ -22,6 +22,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.phoenix.expression.Expression;
 import org.apache.phoenix.query.QueryConstants;
 import org.apache.phoenix.schema.ColumnValueDecoder;
+import org.apache.phoenix.schema.SortOrder;
 import org.apache.phoenix.schema.tuple.Tuple;
 import org.apache.phoenix.util.ByteUtil;
 
@@ -78,13 +79,45 @@ public class PArrayDataTypeDecoder implements ColumnValueDecoder {
             }
             int elementLength = 0;
             if (arrayIndex == (noOfElements - 1)) {
-                int separatorBytes =  serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION ? 3 : 0;
-                elementLength = (bytes[currOffset + initPos] == QueryConstants.SEPARATOR_BYTE || bytes[currOffset + initPos] == QueryConstants.DESC_SEPARATOR_BYTE) ? 0 : indexOffset
-                        - (currOffset + initPos) - separatorBytes;
+                // in the original IMMUTABLE_SERIALIZATION_VERSION (v1), for nulls we store
+                // (separatorByte, #_of_nulls) in the data. Because of the separatorByte, we can't
+                // distinguish between nulls and actual data values that start with the separator
+                // byte. We do a hack here to limit the damage by checking offsets - if the prior
+                // offset had a length of 0, then we know we're storing 2 or more nulls. However, we
+                // still can't fix the case distinguishing a single null from a short value. There
+                // are two kinds of separatorByte, so the results will be potentially incorrect for
+                // 2 short values that correspond to (separatorByte, 1)
+                if (serializationVersion == PArrayDataType.IMMUTABLE_SERIALIZATION_VERSION) {
+                    elementLength = indexOffset - (currOffset + initPos);
+                    if (isNullValue(arrayIndex, bytes, initPos, serializationVersion, useShort, indexOffset, currOffset, elementLength)) {
+                        elementLength = 0;
+                    }
+                } else {
+                    int separatorBytes =  serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION ? 3 : 0;
+                    elementLength = isSeparatorByte(bytes, initPos, currOffset) && serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION ? 0 : indexOffset
+                            - (currOffset + initPos) - separatorBytes;
+                }
             } else {
-                int separatorByte =  serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION ? 1 : 0;
-                elementLength = (bytes[currOffset + initPos] == QueryConstants.SEPARATOR_BYTE || bytes[currOffset + initPos] == QueryConstants.DESC_SEPARATOR_BYTE) ? 0 : PArrayDataType.getOffset(bytes,
-                        arrayIndex + 1, useShort, indexOffset, serializationVersion) - currOffset - separatorByte;
+                if (serializationVersion == PArrayDataType.IMMUTABLE_SERIALIZATION_VERSION) {
+                    elementLength = PArrayDataType.getOffset(bytes, arrayIndex + 1,
+                        useShort, indexOffset, serializationVersion)
+                            - currOffset;
+                    if (isNullValue(arrayIndex, bytes, initPos, serializationVersion, useShort, indexOffset, currOffset, elementLength)) {
+                        elementLength = 0;
+                    }
+                } else {
+                    int separatorByte =
+                            serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION
+                                    ? 1
+                                    : 0;
+                    elementLength =
+                            isSeparatorByte(bytes, initPos, currOffset)
+                                    && serializationVersion == PArrayDataType.SORTABLE_SERIALIZATION_VERSION
+                                            ? 0
+                                            : PArrayDataType.getOffset(bytes, arrayIndex + 1,
+                                                useShort, indexOffset, serializationVersion)
+                                                    - currOffset - separatorByte;
+                }
             }
             ptr.set(bytes, currOffset + initPos, elementLength);
         } else {
@@ -97,6 +130,40 @@ public class PArrayDataTypeDecoder implements ColumnValueDecoder {
             }
         }
         return true;
+    }
+
+    // returns true if the prior element in the array is a null
+    private static boolean isNullValue(int arrayIndex, byte[] bytes, int initPos,
+            byte serializationVersion, boolean useShort, int indexOffset, int currOffset,
+            int elementLength) {
+        if (isSeparatorByte(bytes, initPos, currOffset)) {
+            if (isPriorValueZeroLength(arrayIndex, bytes,
+                serializationVersion, useShort, indexOffset, currOffset)) {
+                return true;
+            } else {
+                // if there's no prior null, there can be at most 1 null
+                if (elementLength == 2) {
+                    // nullByte calculation comes from the encoding of one null
+                    // see PArrayDataType#serializeNulls
+                    byte nullByte = SortOrder.invert((byte)(0));
+                    if (bytes[initPos+currOffset+1] == nullByte) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // checks prior value length by subtracting offset of the previous item from the current offset
+    private static boolean isPriorValueZeroLength(int arrayIndex, byte[] bytes, byte serializationVersion,
+            boolean useShort, int indexOffset, int currOffset) {
+        return arrayIndex > 0 && currOffset - PArrayDataType.getOffset(bytes, arrayIndex - 1,
+            useShort, indexOffset, serializationVersion) == 0;
+    }
+
+    private static boolean isSeparatorByte(byte[] bytes, int initPos, int currOffset) {
+        return bytes[currOffset + initPos] == QueryConstants.SEPARATOR_BYTE || bytes[currOffset + initPos] == QueryConstants.DESC_SEPARATOR_BYTE;
     }
 
 }
