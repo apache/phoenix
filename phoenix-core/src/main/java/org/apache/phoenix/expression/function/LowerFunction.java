f1154aa4341d2dddb8ede41f76964d372b42dcbf
Shehzaad <snakhoda@salesforce.com>
2018-01-31 02:21:21 -0800

    PHOENIX-4418 - add locale-awareness to UPPER() and LOWER() functions

--- phoenix-core/src/main/java/org/apache/phoenix/expression/function/LowerFunction.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/function/LowerFunction.java
@@ -18,8 +18,11 @@
 
 package org.apache.phoenix.expression.function;
 
+import java.io.DataInput;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Locale;
 
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.phoenix.expression.Expression;
@@ -28,16 +31,35 @@ import org.apache.phoenix.schema.tuple.Tuple;
 import org.apache.phoenix.schema.types.PDataType;
 import org.apache.phoenix.schema.types.PVarchar;
 
+import com.force.i18n.LocaleUtils;
+
 @FunctionParseNode.BuiltInFunction(name=LowerFunction.NAME,  args={
-        @FunctionParseNode.Argument(allowedTypes={PVarchar.class})} )
+        @FunctionParseNode.Argument(allowedTypes={PVarchar.class}),
+        @FunctionParseNode.Argument(allowedTypes={PVarchar.class}, defaultValue="null", isConstant=true)} )
 public class LowerFunction extends ScalarFunction {
     public static final String NAME = "LOWER";
+
+    private Locale locale = null;
 
     public LowerFunction() {
     }
 
     public LowerFunction(List<Expression> children) throws SQLException {
         super(children);
+        initialize();
+    }
+
+    private void initialize() {
+        if(children.size() > 1) {
+            String localeISOCode = getLiteralValue(1, String.class);
+            locale = LocaleUtils.get().getLocaleByIsoCode(localeISOCode);
+        }
+    }
+
+    @Override
+    public void readFields(DataInput input) throws IOException {
+        super.readFields(input);
+        initialize();
     }
 
     @Override
@@ -55,7 +77,9 @@ public class LowerFunction extends ScalarFunction {
             return true;
         }
 
-        ptr.set(PVarchar.INSTANCE.toBytes(sourceStr.toLowerCase()));
+        String resultStr = locale == null ? sourceStr.toLowerCase() : sourceStr.toLowerCase(locale);
+
+        ptr.set(PVarchar.INSTANCE.toBytes(resultStr));
         return true;
     }
 
