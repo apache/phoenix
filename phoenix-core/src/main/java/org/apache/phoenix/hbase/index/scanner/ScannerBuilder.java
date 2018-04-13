7b2c7e135916d5a6087ec3e6c4b6c261779795ac
James Taylor <jtaylor@salesforce.com>
2018-04-13 10:10:07 -0700

    Revert "PHOENIX-4496 Fix RowValueConstructorIT and IndexMetadataIT"
    This reverts commit 70c9c6bb2d2073bb40640a0a794032b166c4b63b.

--- phoenix-core/src/main/java/org/apache/phoenix/hbase/index/scanner/ScannerBuilder.java
+++ phoenix-core/src/main/java/org/apache/phoenix/hbase/index/scanner/ScannerBuilder.java
@@ -31,7 +31,6 @@ import org.apache.hadoop.hbase.filter.BinaryComparator;
 import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
 import org.apache.hadoop.hbase.filter.FamilyFilter;
 import org.apache.hadoop.hbase.filter.Filter;
-import org.apache.hadoop.hbase.filter.FilterBase;
 import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.QualifierFilter;
 import org.apache.hadoop.hbase.util.Bytes;
@@ -98,15 +97,6 @@ public class ScannerBuilder {
                 ref.getQualifier())));
       }
       columnFilters.addFilter(columnFilter);
-    }
-    
-    if(columns.isEmpty()){
-        columnFilters.addFilter(new FilterBase(){
-            @Override
-            public boolean filterAllRemaining() throws IOException {
-                return true;
-            }
-        });
     }
     return columnFilters;
   }
