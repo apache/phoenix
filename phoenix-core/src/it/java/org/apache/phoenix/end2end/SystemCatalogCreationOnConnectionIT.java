07125f25e2f1ea7d2a69cda105c68a75fac02ec8
James Taylor <jtaylor@salesforce.com>
2018-04-13 23:32:28 -0700

    Revert PHOENIX-4579 Remove SYSTEM.LOG from list of Phoenix system tables since that JIRA hasn't been backported

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/SystemCatalogCreationOnConnectionIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/SystemCatalogCreationOnConnectionIT.java
@@ -72,11 +72,11 @@ public class SystemCatalogCreationOnConnectionIT {
 
     private static final Set<String> PHOENIX_SYSTEM_TABLES = new HashSet<>(Arrays.asList(
       "SYSTEM.CATALOG", "SYSTEM.SEQUENCE", "SYSTEM.STATS", "SYSTEM.FUNCTION",
-      "SYSTEM.MUTEX"));
+      "SYSTEM.MUTEX", "SYSTEM.LOG"));
 
     private static final Set<String> PHOENIX_NAMESPACE_MAPPED_SYSTEM_TABLES = new HashSet<>(
       Arrays.asList("SYSTEM:CATALOG", "SYSTEM:SEQUENCE", "SYSTEM:STATS", "SYSTEM:FUNCTION",
-        "SYSTEM:MUTEX"));
+        "SYSTEM:MUTEX", "SYSTEM.LOG"));
 
     private static class PhoenixSysCatCreationServices extends ConnectionQueryServicesImpl {
 
