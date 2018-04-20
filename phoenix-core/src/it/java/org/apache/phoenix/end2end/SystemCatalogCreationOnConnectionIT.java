751043d8f6aa2f9aa54035b58255462456cfb196
Ankit Singhal <ankitsinghal59@gmail.com>
2018-04-20 10:57:05 +0530

    PHOENIX-2715 Query Log (addendum)

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/SystemCatalogCreationOnConnectionIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/SystemCatalogCreationOnConnectionIT.java
@@ -76,7 +76,7 @@ public class SystemCatalogCreationOnConnectionIT {
 
     private static final Set<String> PHOENIX_NAMESPACE_MAPPED_SYSTEM_TABLES = new HashSet<>(
       Arrays.asList("SYSTEM:CATALOG", "SYSTEM:SEQUENCE", "SYSTEM:STATS", "SYSTEM:FUNCTION",
-        "SYSTEM:MUTEX", "SYSTEM.LOG"));
+        "SYSTEM:MUTEX", "SYSTEM:LOG"));
 
     private static class PhoenixSysCatCreationServices extends ConnectionQueryServicesImpl {
 
