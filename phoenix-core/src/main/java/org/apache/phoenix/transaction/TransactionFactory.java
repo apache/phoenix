54edd2fab19635b79d44458b0274da6ff0296563
James Taylor <jamestaylor@apache.org>
2018-06-17 23:56:06 +0200

    Run tests with only Tephra enabled to verify there are no regressions

--- phoenix-core/src/main/java/org/apache/phoenix/transaction/TransactionFactory.java
+++ phoenix-core/src/main/java/org/apache/phoenix/transaction/TransactionFactory.java
@@ -25,8 +25,8 @@ import org.apache.phoenix.coprocessor.MetaDataProtocol;
 
 public class TransactionFactory {
     public enum Provider {
-        TEPHRA((byte)1, TephraTransactionProvider.getInstance(), false),
-        OMID((byte)2, OmidTransactionProvider.getInstance(), true);
+        TEPHRA((byte)1, TephraTransactionProvider.getInstance(), true),
+        OMID((byte)2, OmidTransactionProvider.getInstance(), false);
         
         private final byte code;
         private final PhoenixTransactionProvider provider;
