19c87f24d99c7807df6dbc8288609a3ac083fec5
Gerald Sangudi <gsangudi@23andme.com>
2018-08-22 16:59:12 -0700

    PHOENIX-4791 Propagate projected array elements through hash join

--- phoenix-core/src/main/java/org/apache/phoenix/iterate/RegionScannerFactory.java
+++ phoenix-core/src/main/java/org/apache/phoenix/iterate/RegionScannerFactory.java
@@ -287,7 +287,7 @@ public abstract class RegionScannerFactory {
             QueryConstants.ARRAY_VALUE_COLUMN_QUALIFIER, 0,
             QueryConstants.ARRAY_VALUE_COLUMN_QUALIFIER.length, HConstants.LATEST_TIMESTAMP,
             KeyValue.Type.codeToType(rowKv.getTypeByte()), value, 0, value.length));
-        return result.size() - 1;
+        return getArrayCellPosition(result);
       }
 
       @Override
@@ -301,4 +301,8 @@ public abstract class RegionScannerFactory {
       }
     };
   }
+
+    public static int getArrayCellPosition(List<Cell> result) {
+        return result.size() - 1;
+    }
 }
