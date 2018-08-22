58569220706b8c1d49ae649757abf8dca5818862
Gerald Sangudi <gsangudi@23andme.com>
2018-08-22 16:59:12 -0700

    PHOENIX-4791 Array elements are nullified with joins

--- phoenix-core/src/main/java/org/apache/phoenix/iterate/RegionScannerFactory.java
+++ phoenix-core/src/main/java/org/apache/phoenix/iterate/RegionScannerFactory.java
@@ -286,7 +286,7 @@ public abstract class RegionScannerFactory {
             QueryConstants.ARRAY_VALUE_COLUMN_QUALIFIER, 0,
             QueryConstants.ARRAY_VALUE_COLUMN_QUALIFIER.length, HConstants.LATEST_TIMESTAMP,
             KeyValue.Type.codeToType(rowKv.getTypeByte()), value, 0, value.length));
-        return result.size() - 1;
+        return getArrayCellPosition(result);
       }
 
       @Override
@@ -300,4 +300,9 @@ public abstract class RegionScannerFactory {
       }
     };
   }
+
+    // PHOENIX-4791 Share position of array element cell
+    public static int getArrayCellPosition(List<Cell> result) {
+        return result.size() - 1;
+    }
 }
