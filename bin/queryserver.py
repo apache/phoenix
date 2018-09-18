e5379cd1dc6b0ca40ed8a0524332acf75430b15f
Vitaliy <vit34-95@mail.ru>
2018-09-18 15:05:55 -0400

    PHOENIX-4892 Unable to start load balancer with queryserver
    Added phoenix_loadbalancer_jar path to phoenix_utils.py,
    added phoenix_loadbalancer_jar to classpath in queryserver.py,
    added service for registry in meta-inf
    
    Closes #343
    
    Signed-off-by: Josh Elser <elserj@apache.org>

--- bin/queryserver.py
+++ bin/queryserver.py
@@ -120,8 +120,8 @@ out_file_path = os.path.join(log_dir, phoenix_out_file)
 
 # The command is run through subprocess so environment variables are automatically inherited
 java_cmd = '%(java)s -cp ' + hbase_config_path + os.pathsep + hadoop_config_path + os.pathsep + \
-    phoenix_utils.phoenix_client_jar + os.pathsep + phoenix_utils.phoenix_queryserver_jar + \
-    os.pathsep + hadoop_classpath + \
+    phoenix_utils.phoenix_client_jar + os.pathsep + phoenix_utils.phoenix_loadbalancer_jar + \
+    os.pathsep + phoenix_utils.phoenix_queryserver_jar + os.pathsep + hadoop_classpath + \
     " -Dproc_phoenixserver" + \
     " -Dlog4j.configuration=file:" + os.path.join(phoenix_utils.current_dir, "log4j.properties") + \
     " -Dpsql.root.logger=%(root_logger)s" + \
