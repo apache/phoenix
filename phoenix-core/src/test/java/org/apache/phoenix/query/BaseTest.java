c2d33ed384467fea4655e6f49ce43834f3886409
Karan Mehta <karanmehta93@gmail.com>
2018-11-01 17:15:26 -0700

    PHOENIX-4997 Phoenix MR on snapshots can produce duplicate rows

--- phoenix-core/src/test/java/org/apache/phoenix/query/BaseTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/query/BaseTest.java
@@ -1781,15 +1781,15 @@ public abstract class BaseTest {
     
 
     /**
-     *  Split SYSTEM.CATALOG at the given split point 
+     *  Synchronously split table at the given split point
      */
-    protected static void splitRegion(byte[] splitPoint) throws SQLException, IOException, InterruptedException {
+    protected static void splitRegion(TableName fullTableName, byte[] splitPoint) throws SQLException, IOException, InterruptedException {
         Admin admin =
                 driver.getConnectionQueryServices(getUrl(), TestUtil.TEST_PROPERTIES).getAdmin();
-        admin.split(PhoenixDatabaseMetaData.SYSTEM_CATALOG_HBASE_TABLE_NAME, splitPoint);
+        admin.split(fullTableName, splitPoint);
         // make sure the split finishes (there's no synchronous splitting before HBase 2.x)
-        admin.disableTable(PhoenixDatabaseMetaData.SYSTEM_CATALOG_HBASE_TABLE_NAME);
-        admin.enableTable(PhoenixDatabaseMetaData.SYSTEM_CATALOG_HBASE_TABLE_NAME);
+        admin.disableTable(fullTableName);
+        admin.enableTable(fullTableName);
     }
     
     /**
@@ -1818,7 +1818,7 @@ public abstract class BaseTest {
         AssignmentManager am = master.getAssignmentManager();
         // No need to split on the first splitPoint since the end key of region boundaries are exclusive
         for (int i=1; i<splitPoints.size(); ++i) {
-            splitRegion(splitPoints.get(i));
+            splitRegion(fullTableName, splitPoints.get(i));
         }
         HashMap<ServerName, List<HRegionInfo>> serverToRegionsList = Maps.newHashMapWithExpectedSize(NUM_SLAVES_BASE);
         Deque<ServerName> availableRegionServers = new ArrayDeque<ServerName>(NUM_SLAVES_BASE);
