0b823f4546c665bd8c0e5a61a1c14a2c6ab11dd0
Ohad Shacham <ohads@yahoo-inc.com>
2018-12-16 22:47:43 -0500

    [PHOENIX-3623] Integrate Omid with Phoenix.
    This patch add's support for Omid transaction processor.
    More information exists at the Jira and at [OMID-82], which is the corresponding jira and commit to Omid.

--- phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
+++ phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
@@ -18,89 +18,279 @@
 package org.apache.phoenix.transaction;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.hadoop.hbase.client.Table;
+import org.apache.omid.proto.TSOProto;
+import org.apache.omid.transaction.AbstractTransaction.VisibilityLevel;
+import org.apache.omid.transaction.HBaseCellId;
+import org.apache.omid.transaction.HBaseTransaction;
+import org.apache.omid.transaction.HBaseTransactionManager;
+import org.apache.omid.transaction.RollbackException;
+import org.apache.omid.transaction.Transaction;
+//import org.apache.omid.tso.TSOMockModule;
+import org.apache.omid.transaction.Transaction.Status;
+import org.apache.omid.transaction.TransactionException;
+import org.apache.phoenix.exception.SQLExceptionCode;
+import org.apache.phoenix.exception.SQLExceptionInfo;
 import org.apache.phoenix.jdbc.PhoenixConnection;
 import org.apache.phoenix.schema.PTable;
 import org.apache.phoenix.transaction.TransactionFactory.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+//import org.apache.omid.tso.TSOMockModule;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 //import org.apache.omid.tso.TSOMockModule;
 
 public class OmidTransactionContext implements PhoenixTransactionContext {
 
+    private static final Logger logger = LoggerFactory.getLogger(OmidTransactionContext.class);
+
+    private HBaseTransactionManager tm;
+    private HBaseTransaction tx;
+
     public OmidTransactionContext() {
+        this.tx = null;
+        this.tm = null;
     }
 
     public OmidTransactionContext(PhoenixConnection connection) throws SQLException {
+        PhoenixTransactionClient client = connection.getQueryServices().initTransactionClient(getProvider());
+        assert (client instanceof OmidTransactionProvider.OmidTransactionClient);
+        this.tm = ((OmidTransactionProvider.OmidTransactionClient)client).getTransactionClient();
+        this.tx = null;
     }
 
     public OmidTransactionContext(byte[] txnBytes) throws InvalidProtocolBufferException {
+        this();
+        if (txnBytes != null && txnBytes.length > 0) {
+            TSOProto.Transaction transaction = TSOProto.Transaction.parseFrom(txnBytes);
+            tx = new HBaseTransaction(transaction.getTimestamp(), transaction.getEpoch(), new HashSet<HBaseCellId>(),
+                    new HashSet<HBaseCellId>(), null, tm.isLowLatency());
+        } else {
+            tx = null;
+        }
     }
 
     public OmidTransactionContext(PhoenixTransactionContext ctx, boolean subTask) {
+        assert (ctx instanceof OmidTransactionContext);
+        OmidTransactionContext omidTransactionContext = (OmidTransactionContext) ctx;
+
+        this.tm = omidTransactionContext.tm;
+        
+        if (subTask) {
+            if (omidTransactionContext.isTransactionRunning()) {
+                Transaction transaction = omidTransactionContext.getTransaction();
+                this.tx = new HBaseTransaction(transaction.getTransactionId(), transaction.getEpoch(),
+                        new HashSet<HBaseCellId>(), new HashSet<HBaseCellId>(), this.tm,
+                        transaction.getReadTimestamp(), transaction.getWriteTimestamp(), tm.isLowLatency());
+            } else {
+                this.tx = null;
+            }
+
+            this.tm = null;
+        } else {
+            this.tx = omidTransactionContext.getTransaction();
+        }
     }
 
     @Override
     public void begin() throws SQLException {
+        if (tm == null) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.NULL_TRANSACTION_CONTEXT).build()
+                    .buildException();
+        }
+
+
+        try {
+            tx = (HBaseTransaction) tm.begin();
+        } catch (TransactionException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TRANSACTION_FAILED)
+                    .setMessage(e.getMessage()).setRootCause(e).build()
+                    .buildException();
+        }
     }
 
     @Override
     public void commit() throws SQLException {
+        if (tx == null || tm == null)
+            return;
+
+        try {
+            tm.commit(tx);
+        } catch (TransactionException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TRANSACTION_FAILED)
+                    .setMessage(e.getMessage()).setRootCause(e).build()
+                    .buildException();
+        } catch (RollbackException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TRANSACTION_CONFLICT_EXCEPTION)
+                    .setMessage(e.getMessage()).setRootCause(e).build()
+                    .buildException();
+        }
     }
 
     @Override
     public void abort() throws SQLException {
+        if (tx == null || tm == null || tx.getStatus() != Status.RUNNING) {
+            return;
+        }
+
+        try {
+            tm.rollback(tx);
+        } catch (TransactionException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TRANSACTION_FAILED)
+                    .setMessage(e.getMessage()).setRootCause(e).build()
+                    .buildException();
+        }
     }
 
     @Override
     public void checkpoint(boolean hasUncommittedData) throws SQLException {
+        if (hasUncommittedData) {
+            try {
+                tx.checkpoint();
+            } catch (TransactionException e) {
+                throw new SQLException(e);
+            }
+        }
+        tx.setVisibilityLevel(VisibilityLevel.SNAPSHOT_EXCLUDE_CURRENT);
     }
 
     @Override
     public void commitDDLFence(PTable dataTable) throws SQLException {
+
+        try {
+            tx = (HBaseTransaction) tm.fence(dataTable.getName().getBytes());
+            if (logger.isInfoEnabled()) {
+                logger.info("Added write fence at ~"
+                        + tx.getReadTimestamp());
+            }
+        } catch (TransactionException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TX_UNABLE_TO_GET_WRITE_FENCE)
+            .setSchemaName(dataTable.getSchemaName().getString())
+            .setTableName(dataTable.getTableName().getString()).build()
+            .buildException();
+        }
     }
 
     @Override
     public void join(PhoenixTransactionContext ctx) {
+
+        if (ctx == PhoenixTransactionContext.NULL_CONTEXT) {
+            return;
+        }
+
+        assert (ctx instanceof OmidTransactionContext);
+        OmidTransactionContext omidContext = (OmidTransactionContext) ctx;
+        
+        HBaseTransaction transaction = omidContext.getTransaction();
+        if (transaction == null || tx == null) return;
+
+        Set<HBaseCellId> writeSet = transaction.getWriteSet();
+
+        for (HBaseCellId cell : writeSet) {
+            tx.addWriteSetElement(cell);
+        }
     }
 
     @Override
     public boolean isTransactionRunning() {
-        return false;
+        return (tx != null);
     }
 
     @Override
     public void reset() {
+        tx = null;
     }
 
     @Override
     public long getTransactionId() {
-        return 0;
+        return tx.getTransactionId();
     }
 
     @Override
     public long getReadPointer() {
-        return 0;
+        return tx.getReadTimestamp();
     }
 
     @Override
     public long getWritePointer() {
-        return 0;
+        return tx.getWriteTimestamp();
     }
 
     @Override
     public PhoenixVisibilityLevel getVisibilityLevel() {
-        return null;
+        VisibilityLevel visibilityLevel = null;
+
+        assert(tx != null);
+        visibilityLevel = tx.getVisibilityLevel();
+
+        PhoenixVisibilityLevel phoenixVisibilityLevel;
+        switch (visibilityLevel) {
+        case SNAPSHOT:
+            phoenixVisibilityLevel = PhoenixVisibilityLevel.SNAPSHOT;
+            break;
+        case SNAPSHOT_EXCLUDE_CURRENT:
+            phoenixVisibilityLevel = PhoenixVisibilityLevel.SNAPSHOT_EXCLUDE_CURRENT;
+            break;
+        case SNAPSHOT_ALL:
+            phoenixVisibilityLevel = PhoenixVisibilityLevel.SNAPSHOT_ALL;
+        default:
+            phoenixVisibilityLevel = null;
+        }
+
+        return phoenixVisibilityLevel;
     }
 
     @Override
     public void setVisibilityLevel(PhoenixVisibilityLevel visibilityLevel) {
+
+        VisibilityLevel omidVisibilityLevel = null;
+
+        switch (visibilityLevel) {
+        case SNAPSHOT:
+            omidVisibilityLevel = VisibilityLevel.SNAPSHOT;
+            break;
+        case SNAPSHOT_EXCLUDE_CURRENT:
+            omidVisibilityLevel = VisibilityLevel.SNAPSHOT_EXCLUDE_CURRENT;
+            break;
+        case SNAPSHOT_ALL:
+            omidVisibilityLevel = VisibilityLevel.SNAPSHOT_ALL;
+            break;
+        default:
+            assert (false);
+        }
+
+        assert(tx != null);
+        tx.setVisibilityLevel(omidVisibilityLevel);
+
     }
 
     @Override
     public byte[] encodeTransaction() throws SQLException {
-        return null;
+        assert(tx != null);
+
+        TSOProto.Transaction.Builder transactionBuilder = TSOProto.Transaction.newBuilder();
+
+        transactionBuilder.setTimestamp(tx.getTransactionId());
+        transactionBuilder.setEpoch(tx.getEpoch());
+
+        byte[] encodedTxBytes = transactionBuilder.build().toByteArray();
+        // Add code of TransactionProvider at end of byte array
+        encodedTxBytes = Arrays.copyOf(encodedTxBytes, encodedTxBytes.length + 1);
+        encodedTxBytes[encodedTxBytes.length - 1] = getProvider().getCode();
+        return encodedTxBytes;
     }
 
     @Override
@@ -110,12 +300,21 @@ public class OmidTransactionContext implements PhoenixTransactionContext {
 
     @Override
     public PhoenixTransactionContext newTransactionContext(PhoenixTransactionContext context, boolean subTask) {
-        return null;
+        return new OmidTransactionContext(context, subTask);
     }
 
     @Override
     public void markDMLFence(PTable dataTable) {
     }
+
+    /**
+    *  OmidTransactionContext specific functions
+    */
+
+    public HBaseTransaction getTransaction() {
+        return tx;
+    }
+
 
     @Override
     public Table getTransactionalTable(Table htable, boolean isConflictFree) throws SQLException {
@@ -124,6 +323,9 @@ public class OmidTransactionContext implements PhoenixTransactionContext {
 
     @Override
     public Table getTransactionalTableWriter(PhoenixConnection connection, PTable table, Table htable, boolean isIndex) throws SQLException {
-        return new OmidTransactionTable(this, htable, table.isImmutableRows() || isIndex);
+        // When we're getting a table for writing, if the table being written to is an index,
+        // write the shadow cells immediately since the only time we write to an index is
+        // when we initially populate it synchronously.
+        return new OmidTransactionTable(this, htable, table.isImmutableRows() || isIndex, isIndex);
     }
 }
