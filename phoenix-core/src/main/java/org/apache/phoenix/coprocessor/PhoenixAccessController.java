85ab5d616bfefee9c23e70a29b6733861694af9f
Monani Mihir <monani.mihir@gmail.com>
2018-12-19 22:46:39 +0530

    PHOENIX-5070 NPE when upgrading Phoenix 4.13.0 to Phoenix 4.14.1 with hbase-1.x branch in secure setup

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/PhoenixAccessController.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/PhoenixAccessController.java
@@ -419,7 +419,7 @@ public class PhoenixAccessController extends BaseMetaDataEndpointObserver {
                 final List<UserPermission> userPermissions = new ArrayList<UserPermission>();
                 try (Connection connection = ConnectionFactory.createConnection(env.getConfiguration())) {
                     // Merge permissions from all accessController coprocessors loaded in memory
-                    for (MasterObserver service : accessControllers) {
+                    for (MasterObserver service : getAccessControllers()) {
                         // Use AccessControlClient API's if the accessController is an instance of org.apache.hadoop.hbase.security.access.AccessController
                         if (service.getClass().getName().equals(org.apache.hadoop.hbase.security.access.AccessController.class.getName())) {
                             userPermissions.addAll(AccessControlClient.getUserPermissions(connection, tableName.getNameAsString()));
