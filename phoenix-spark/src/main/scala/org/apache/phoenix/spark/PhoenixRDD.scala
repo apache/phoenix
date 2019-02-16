6b1e2136631b36e4d43cdf5b52d21ce867bd812a
Thomas D'Silva <tdsilva@apache.org>
2019-02-15 16:44:02 -0800

    PHOENIX-5141 Use HBaseFactoryProvider.getConfigurationFactory to get the config in PhoenixRDD

--- phoenix-spark/src/main/scala/org/apache/phoenix/spark/PhoenixRDD.scala
+++ phoenix-spark/src/main/scala/org/apache/phoenix/spark/PhoenixRDD.scala
@@ -21,6 +21,7 @@ import org.apache.hadoop.io.NullWritable
 import org.apache.phoenix.jdbc.PhoenixDriver
 import org.apache.phoenix.mapreduce.PhoenixInputFormat
 import org.apache.phoenix.mapreduce.util.PhoenixConfigurationUtil
+import org.apache.phoenix.query.HBaseFactoryProvider
 import org.apache.spark._
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.rdd.RDD
@@ -76,9 +77,7 @@ class PhoenixRDD(sc: SparkContext, table: String, columns: Seq[String],
 
   def getPhoenixConfiguration: Configuration = {
 
-    // This is just simply not serializable, so don't try, but clone it because
-    // PhoenixConfigurationUtil mutates it.
-    val config = HBaseConfiguration.create(conf)
+    val config = HBaseFactoryProvider.getConfigurationFactory.getConfiguration(conf);
 
     PhoenixConfigurationUtil.setInputClass(config, classOf[PhoenixRecordWritable])
     PhoenixConfigurationUtil.setInputTableName(config, table)
