8cedf240d4fcf3188bc43ef19511d58aaca42119
Gokcen Iskender <giskender@salesforce.com>
2019-03-06 17:58:21 +0000

    PHOENIX-5190 Implement TaskRegionObserver for Index rebuild
    Signed-off-by: Geoffrey Jacoby <gjacoby@apache.org>

---
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/tasks/DropChildViewsTask.java
@@ -0,0 +1,81 @@
+package org.apache.phoenix.coprocessor.tasks;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.phoenix.coprocessor.MetaDataEndpointImpl;
+import org.apache.phoenix.coprocessor.MetaDataProtocol;
+import org.apache.phoenix.coprocessor.TaskRegionObserver;
+import org.apache.phoenix.jdbc.PhoenixConnection;
+import org.apache.phoenix.schema.MetaDataClient;
+import org.apache.phoenix.schema.task.Task;
+import org.apache.phoenix.util.PhoenixRuntime;
+import org.apache.phoenix.util.QueryUtil;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+/**
+ * Task runs periodically to clean up task of child views whose parent is dropped
+ *
+ */
+public class DropChildViewsTask extends BaseTask {
+    public static final Log LOG = LogFactory.getLog(DropChildViewsTask.class);
+
+    public TaskRegionObserver.TaskResult run(Task.TaskRecord taskRecord) {
+        PhoenixConnection pconn = null;
+        Timestamp timestamp = taskRecord.getTimeStamp();
+        try {
+            String tenantId = taskRecord.getTenantId();
+            if (tenantId != null) {
+                Properties tenantProps = new Properties();
+                tenantProps.setProperty(PhoenixRuntime.TENANT_ID_ATTRIB, tenantId);
+                pconn = QueryUtil.getConnectionOnServer(tenantProps, env.getConfiguration()).unwrap(PhoenixConnection.class);
+            }
+            else {
+                pconn = QueryUtil.getConnectionOnServer(env.getConfiguration()).unwrap(PhoenixConnection.class);
+            }
+
+            MetaDataProtocol.MetaDataMutationResult result = new MetaDataClient(pconn).updateCache(pconn.getTenantId(),
+                    taskRecord.getSchemaName(), taskRecord.getTableName(), true);
+            if (result.getMutationCode() != MetaDataProtocol.MutationCode.TABLE_ALREADY_EXISTS) {
+                MetaDataEndpointImpl
+                        .dropChildViews(env, taskRecord.getTenantIdBytes(), taskRecord.getSchemaNameBytes(), taskRecord.getTableNameBytes());
+                return new TaskRegionObserver.TaskResult(TaskRegionObserver.TaskResultCode.SUCCESS, "");
+            } else if (System.currentTimeMillis() < timeMaxInterval + timestamp.getTime()) {
+                // skip this task as it has not been expired and its parent table has not been dropped yet
+                LOG.info("Skipping a child view drop task. The parent table has not been dropped yet : " +
+                        taskRecord.getSchemaName() + "." + taskRecord.getTableName() +
+                        " with tenant id " + (tenantId == null ? " IS NULL" : tenantId) +
+                        " and timestamp " + timestamp.toString());
+                return new TaskRegionObserver.TaskResult(TaskRegionObserver.TaskResultCode.SKIPPED, "");
+            }
+            else {
+                LOG.warn(" A drop child view task has expired and will be marked as failed : " +
+                        taskRecord.getSchemaName() + "." + taskRecord.getTableName() +
+                        " with tenant id " + (tenantId == null ? " IS NULL" : tenantId) +
+                        " and timestamp " + timestamp.toString());
+                return new TaskRegionObserver.TaskResult(TaskRegionObserver.TaskResultCode.FAIL, "Expired");
+            }
+        }
+        catch (Throwable t) {
+            LOG.warn("Exception while dropping a child view task. " +
+                    taskRecord.getSchemaName()  + "." + taskRecord.getTableName() +
+                    " with tenant id " + (taskRecord.getTenantId() == null ? " IS NULL" : taskRecord.getTenantId()) +
+                    " and timestamp " + timestamp.toString(), t);
+            return new TaskRegionObserver.TaskResult(TaskRegionObserver.TaskResultCode.FAIL, t.toString());
+        } finally {
+            if (pconn != null) {
+                try {
+                    pconn.close();
+                } catch (SQLException ignored) {
+                    LOG.debug("DropChildViewsTask can't close connection", ignored);
+                }
+            }
+        }
+    }
+
+    public TaskRegionObserver.TaskResult checkCurrentResult(Task.TaskRecord taskRecord) throws Exception {
+        return null;
+    }
+}
