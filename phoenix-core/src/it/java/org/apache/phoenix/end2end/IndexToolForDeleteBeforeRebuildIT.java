53c580e5a33ea47f2076d35b05e81aa8aa7c23bb
Gokcen Iskender <giskender@salesforce.com>
2019-04-26 15:50:06 -0700

    PHOENIX-4703 Make indextool changes to drop before rebuild

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/IndexToolForDeleteBeforeRebuildIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/IndexToolForDeleteBeforeRebuildIT.java
@@ -20,6 +20,7 @@ package org.apache.phoenix.end2end;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.apache.hadoop.conf.Configuration;
+
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.phoenix.jdbc.PhoenixConnection;
 import org.apache.phoenix.mapreduce.index.IndexTool;
