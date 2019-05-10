11e8b703d9f9c9647e7b7c6c594a7546b980f32d
Monani Mihir <mihir6692@apache.org>
2019-05-10 14:00:04 +0530

    PHOENIX-5266 Client can only write on Index Table and skip data table if failure happens because of region split/move etc

--- phoenix-core/src/main/java/org/apache/phoenix/execute/MutationState.java
+++ phoenix-core/src/main/java/org/apache/phoenix/execute/MutationState.java
@@ -47,8 +47,6 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.Pair;
 import org.apache.htrace.Span;
 import org.apache.htrace.TraceScope;
-import org.apache.phoenix.cache.IndexMetaDataCache;
-import org.apache.phoenix.cache.ServerCacheClient;
 import org.apache.phoenix.cache.ServerCacheClient.ServerCache;
 import org.apache.phoenix.compile.MutationPlan;
 import org.apache.phoenix.coprocessor.BaseScannerRegionObserver;
@@ -935,6 +933,8 @@ public class MutationState implements SQLCloseable {
                 TableInfo tableInfo = pair.getKey();
                 byte[] htableName = tableInfo.getHTableName().getBytes();
                 List<Mutation> mutationList = pair.getValue();
+                List<List<Mutation>> mutationBatchList =
+                        getMutationBatchList(batchSize, batchSizeBytes, mutationList);
 
                 // create a span per target table
                 // TODO maybe we can be smarter about the table name to string here?
@@ -978,9 +978,9 @@ public class MutationState implements SQLCloseable {
 
                         startTime = System.currentTimeMillis();
                         child.addTimelineAnnotation("Attempt " + retryCount);
-                        List<List<Mutation>> mutationBatchList = getMutationBatchList(batchSize, batchSizeBytes,
-                                mutationList);
-                        for (final List<Mutation> mutationBatch : mutationBatchList) {
+                        Iterator<List<Mutation>> itrListMutation = mutationBatchList.iterator();
+                        while (itrListMutation.hasNext()) {
+                            final List<Mutation> mutationBatch = itrListMutation.next();
                             if (shouldRetryIndexedMutation) {
                                 // if there was an index write failure, retry the mutation in a loop
                                 final Table finalHTable = hTable;
@@ -1028,9 +1028,17 @@ public class MutationState implements SQLCloseable {
                                         return ioe;
                                     }
                                 }, iwe, connection, connection.getQueryServices().getProps());
+                                shouldRetryIndexedMutation = false;
                             } else {
                                 hTable.batch(mutationBatch);
                             }
+                            // remove each batch from the list once it gets applied
+                            // so when failures happens for any batch we only start
+                            // from that batch only instead of doing duplicate reply of already
+                            // applied batches from entire list, also we can set
+                            // REPLAY_ONLY_INDEX_WRITES for first batch
+                            // only in case of 1121 SQLException
+                            itrListMutation.remove();
 
                             batchCount++;
                             if (logger.isDebugEnabled())
@@ -1081,7 +1089,8 @@ public class MutationState implements SQLCloseable {
                                 if (iwe != null && !shouldRetryIndexedMutation) {
                                     // For an index write failure, the data table write succeeded,
                                     // so when we retry we need to set REPLAY_WRITES
-                                    for (Mutation m : mutationList) {
+                                    // for first batch in list only.
+                                    for (Mutation m : mutationBatchList.get(0)) {
                                         if (!PhoenixIndexMetaData.
                                                 isIndexRebuild(m.getAttributesMap())) {
                                             m.setAttribute(BaseScannerRegionObserver.REPLAY_WRITES,
