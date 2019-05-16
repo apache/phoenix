edaf0822ce675c45871c62f7e9d318c3ca691877
William Shen <willshen@users.noreply.github.com>
2019-05-16 11:18:35 -0700

    PHOENIX-5275: Remove accidental imports from curator-client-2.12.0

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/UpgradeIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/UpgradeIT.java
@@ -41,7 +41,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.google.common.collect.Lists;
-import org.apache.curator.shaded.com.google.common.collect.Sets;
+import com.google.common.collect.Sets;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.snapshot.SnapshotCreationException;
