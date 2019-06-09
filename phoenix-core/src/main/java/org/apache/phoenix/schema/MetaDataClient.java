2d322a11a7fa8f36aa7bdd4cb3d3c5e2da7af332
Toshihiro Suzuki <brfrn169@gmail.com>
2019-06-09 22:25:18 +0900

    PHOENIX-5209 Cannot add non-PK column to table when the last PK column is of type VARBINARY or ARRAY

--- phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
@@ -3593,10 +3593,11 @@ public class MetaDataClient {
 
                 int position = table.getColumns().size();
 
-                List<PColumn> currentPKs = table.getPKColumns();
-                if (numCols > 0) {
+                boolean addPKColumns = columnDefs.stream().anyMatch(ColumnDef::isPK);
+                if (addPKColumns) {
+                    List<PColumn> currentPKs = table.getPKColumns();
                     PColumn lastPK = currentPKs.get(currentPKs.size()-1);
-                    // Disallow adding columns if the last column is VARBIANRY.
+                    // Disallow adding columns if the last column is VARBIANRY or ARRAY.
                     if (lastPK.getDataType() == PVarbinary.INSTANCE || lastPK.getDataType().isArrayType()) {
                         throw new SQLExceptionInfo.Builder(SQLExceptionCode.VARBINARY_LAST_PK)
                         .setColumnName(lastPK.getName().getString()).build().buildException();
