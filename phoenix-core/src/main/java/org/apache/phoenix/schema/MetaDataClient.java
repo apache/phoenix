34d9675a8e63ee0e1a2c0dbf9c001d61d59f7564
Viraj Jasani <virajjasani007@gmail.com>
2019-07-14 18:24:18 +0530

    PHOENIX-5391 : MetadataClient - TenantId Map is not correctly updated with list of Table Refs
    Signed-off-by: Chinmay Kulkarni <chinmayskulkarni@gmail.com>

--- phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
@@ -4209,14 +4209,16 @@ public class MetaDataClient {
                                         .build();
                                 TableRef indexTableRef = new TableRef(viewIndexTable);
                                 PName indexTableTenantId = sharedTableState.getTenantId();
-                                if (indexTableTenantId==null) {
+                                if (indexTableTenantId == null) {
                                     tableRefsToDrop.add(indexTableRef);
-                                }
-                                else {
-                                    if (!tenantIdTableRefMap.containsKey(indexTableTenantId)) {
-                                        tenantIdTableRefMap.put(indexTableTenantId.getString(), Lists.<TableRef>newArrayList());
+                                } else {
+                                    if (!tenantIdTableRefMap.containsKey(
+                                            indexTableTenantId.getString())) {
+                                        tenantIdTableRefMap.put(indexTableTenantId.getString(),
+                                            Lists.<TableRef>newArrayList());
                                     }
-                                    tenantIdTableRefMap.get(indexTableTenantId.getString()).add(indexTableRef);
+                                    tenantIdTableRefMap.get(indexTableTenantId.getString())
+                                            .add(indexTableRef);
                                 }
 
                             }
