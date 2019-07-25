77cf9eeb5a9760d362087f2879514f1516f900ac
Gokcen Iskender <giskender@salesforce.com>
2019-07-25 11:04:18 -0700

    PHOENIX-5348: Fix flaky test: testIndexRebuildTask
    Signed-off-by: Chinmay Kulkarni <chinmayskulkarni@apache.org>

--- phoenix-core/src/main/java/org/apache/phoenix/mapreduce/index/PhoenixIndexImportDirectReducer.java
+++ phoenix-core/src/main/java/org/apache/phoenix/mapreduce/index/PhoenixIndexImportDirectReducer.java
@@ -20,11 +20,9 @@ package org.apache.phoenix.mapreduce.index;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
@@ -36,7 +34,6 @@ import org.apache.phoenix.mapreduce.util.PhoenixConfigurationUtil;
 import org.apache.phoenix.schema.PIndexState;
 import org.apache.phoenix.schema.PTable;
 import org.apache.phoenix.schema.task.Task;
-import org.apache.phoenix.util.EnvironmentEdgeManager;
 import org.apache.phoenix.util.SchemaUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -75,12 +72,12 @@ public class PhoenixIndexImportDirectReducer extends
             String schemaName = SchemaUtil.getSchemaNameFromFullName(fullTableName);
             String tableName = SchemaUtil.getTableNameFromFullName(fullTableName);
             String indexName = PhoenixConfigurationUtil.getDisableIndexes(context.getConfiguration());
-            List<Task.TaskRecord> taskRecords = Task.queryTaskTable(connection, schemaName, tableName,
+            List<Task.TaskRecord> taskRecords = Task.queryTaskTable(connection, null, schemaName, tableName,
                     PTable.TaskType.INDEX_REBUILD, tenantId, indexName);
             if (taskRecords != null && taskRecords.size() > 0) {
                 for (Task.TaskRecord taskRecord : taskRecords) {
                     TaskRegionObserver.SelfHealingTask.setEndTaskStatus(
-                            connection.unwrap(PhoenixConnection.class), taskRecords.get(0),
+                            connection.unwrap(PhoenixConnection.class), taskRecord,
                             PTable.TaskStatus.COMPLETED.toString());
                 }
             }
