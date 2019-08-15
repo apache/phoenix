d8a0a8a540d71889d487e9ba31a8c2115d8a6aba
Toshihiro Suzuki <brfrn169@gmail.com>
2019-08-15 13:50:32 +0900

    PHOENIX-5210 NullPointerException when alter options of a table that is appendOnlySchema
    Signed-off-by: Xinyi Yan <yanxinyi@apache.org>

--- phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
@@ -3683,7 +3683,7 @@ public class MetaDataClient {
             connection.setAutoCommit(false);
 
             List<ColumnDef> columnDefs;
-            if (table.isAppendOnlySchema() || ifNotExists) {
+            if ((table.isAppendOnlySchema() || ifNotExists) && origColumnDefs != null) {
                 // only make the rpc if we are adding new columns
                 columnDefs = Lists.newArrayList();
                 for (ColumnDef columnDef : origColumnDefs) {
