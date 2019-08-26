f4579a52a950a880368a2ec9f8cecbd3c67cbae9
Rajeshbabu Chintaguntla <Rajeshbabu Chintaguntla>
2019-08-26 21:53:06 +0530

    PHOENIX-5136 Rows with null values inserted by UPSERT .. ON DUPLICATE KEY UPDATE are included in query results when they shouldn't be(Miles Spielberg)

--- phoenix-core/src/main/java/org/apache/phoenix/expression/AndOrExpression.java
+++ phoenix-core/src/main/java/org/apache/phoenix/expression/AndOrExpression.java
@@ -24,7 +24,7 @@ import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.phoenix.schema.types.PBoolean;
 import org.apache.phoenix.schema.types.PDataType;
 import org.apache.phoenix.schema.tuple.Tuple;
-
+import org.apache.phoenix.util.ByteUtil;
 
 /**
  * 
@@ -36,6 +36,8 @@ import org.apache.phoenix.schema.tuple.Tuple;
 public abstract class AndOrExpression extends BaseCompoundExpression {
     // Remember evaluation of child expression for partial evaluation
     private BitSet partialEvalState;
+    // true if we have seen NULL as the value of some child expression
+    private boolean seenNull = false;
    
     public AndOrExpression() {
     }
@@ -56,33 +58,44 @@ public abstract class AndOrExpression extends BaseCompoundExpression {
         } else {
             partialEvalState.clear();
         }
+        seenNull = false;
         super.reset();
     }
     
     @Override
     public boolean evaluate(Tuple tuple, ImmutableBytesWritable ptr) {
-        boolean isNull = false;
+        boolean childFailed = false;
         for (int i = 0; i < children.size(); i++) {
-            Expression child = children.get(i);
             // If partial state is available, then use that to know we've already evaluated this
             // child expression and do not need to do so again.
             if (partialEvalState == null || !partialEvalState.get(i)) {
+                Expression child = children.get(i);
                 // Call through to child evaluate method matching parent call to allow child to optimize
                 // evaluate versus getValue code path.
                 if (child.evaluate(tuple, ptr)) {
                     // Short circuit if we see our stop value
                     if (isStopValue((Boolean) PBoolean.INSTANCE.toObject(ptr, child.getDataType()))) {
                         return true;
-                    } else if (partialEvalState != null) {
+                    }
+                    if (ptr.getLength() == 0) {
+                        seenNull = true;
+                    }
+                    if (partialEvalState != null) {
                         partialEvalState.set(i);
                     }
                 } else {
-                    isNull = true;
+                    childFailed = true;
                 }
             }
         }
-        if (isNull) {
+        if (childFailed) {
             return false;
+        }
+
+        if (seenNull) {
+            // Some child evaluated to NULL and we never saw a stop value.
+            // The expression evaluates as NULL even if the last child evaluated was non-NULL.
+            ptr.set(ByteUtil.EMPTY_BYTE_ARRAY);
         }
         return true;
     }
