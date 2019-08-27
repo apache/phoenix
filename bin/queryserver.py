3d57e1d522f5189d8e5b1cc595799832bc996627
Istvan Toth <stoty@stoty.hu>
2019-08-27 16:03:04 +0200

    PHOENIX-5454 scripts start foreground java processes as child processes
    use os.exec*() in python scripts to start foreground java processes

--- bin/queryserver.py
+++ bin/queryserver.py
@@ -210,6 +210,5 @@ elif command == 'stop':
 else:
     # run in the foreground using defaults from log4j.properties
     cmd = java_cmd % {'java': java, 'root_logger': 'INFO,console', 'log_dir': '.', 'log_file': 'psql.log'}
-    # Because shell=True is not set, we don't have to alter the environment
-    child = subprocess.Popen(cmd.split())
-    sys.exit(child.wait())
+    splitcmd = cmd.split()
+    os.execvp(splitcmd[0], splitcmd)
