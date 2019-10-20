7e98eb21cd24edaf5667d3f95571a211bb068c9a
Kadir <kozdemir@salesforce.com>
2019-10-20 01:02:22 -0700

    PHOENIX-5535 Index rebuilds via UngroupedAggregateRegionObserver should replay delete markers

--- phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/MetaDataClient.java
@@ -1315,8 +1315,10 @@ public class MetaDataClient {
             PostIndexDDLCompiler compiler = new PostIndexDDLCompiler(connection, dataTableRef);
             return compiler.compile(index);
         } else {
-            ServerBuildIndexCompiler compiler = new ServerBuildIndexCompiler(connection, getFullTableName(dataTableRef));
-            return compiler.compile(index);
+            ServerBuildIndexCompiler compiler = new ServerBuildIndexCompiler(connection,
+                    SchemaUtil.getTableName(dataTableRef.getTable().getSchemaName().getString(), dataTableRef.getTable().getTableName().getString()),
+                    SchemaUtil.getTableName(index.getSchemaName().getString(), index.getTableName().getString()));
+            return compiler.compile();
         }
     }
 
@@ -1686,9 +1688,6 @@ public class MetaDataClient {
             return buildIndexAtTimeStamp(table, statement.getTable());
         }
 
-        String dataTableFullName = SchemaUtil.getTableName(
-                tableRef.getTable().getSchemaName().getString(),
-                tableRef.getTable().getTableName().getString());
         return buildIndex(table, tableRef);
     }
 
