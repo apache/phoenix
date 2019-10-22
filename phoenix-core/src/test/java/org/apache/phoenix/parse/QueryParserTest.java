152c157e1bce85dccface13f95586f7e4f51eb6f
Bharath Vissapragada <bharathv@apache.org>
2019-10-22 14:14:10 -0700

    PHOENIX-5543: Implement SHOW TABLES/SCHEMAS sql commands
    This patch adds new SQL grammar like following
    
    - SHOW SCHEMAS [like '<pattern>']
    - SHOW TABLES [IN <schema>] [like '<pattern']
    
    Example invocations:
    
    - show schemas
    - show scemas like 'SYS%'
    - show tables
    - show tables in SYSTEM
    - show tables in SYSTEM like 'CAT%'
    
    The current way of fetching this information is by using
    !tables and !schemas via sqlline JDBC support but that is
    not flexible enough for the end users to add more fitlers.
    This approach is more inline with what other databases do.
    
    Added test coverage in parser tests and core e2e tests.
    
    Change-Id: Iafc5cc8122e88501f0a5595ee51ba2dc7556ec4a

--- phoenix-core/src/test/java/org/apache/phoenix/parse/QueryParserTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/parse/QueryParserTest.java
@@ -26,8 +26,6 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.apache.hadoop.hbase.util.Pair;
@@ -893,7 +891,25 @@ public class QueryParserTest {
 
     @Test
     public void testLimitRVCOffsetQuery() throws Exception {
-        String sql = "SELECT * FROM T LIMIT 10 OFFSET (A,B,C)=('a','b','c')";
-        parseQuery(sql);
+      String sql = "SELECT * FROM T LIMIT 10 OFFSET (A,B,C)=('a','b','c')";
+      parseQuery(sql);
+    }
+
+    @Test
+    public void testShowStmt() throws Exception {
+        // Happy paths
+        parseQuery("show schemas");
+        parseQuery("show schemas like 'foo%'");
+        parseQuery("show tables");
+        parseQuery("show tables in foo");
+        parseQuery("show tables in foo like 'bar%'");
+        parseQuery("show tables like 'bar%'");
+
+        // Expected failures.
+        parseQueryThatShouldFail("show schemas like foo");
+        parseQueryThatShouldFail("show schemas in foo");
+        parseQueryThatShouldFail("show tables 'foo'");
+        parseQueryThatShouldFail("show tables in 'foo'");
+        parseQueryThatShouldFail("show tables like foo");
     }
 }
