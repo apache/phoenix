1a497ed84b5b81687eeaae642137665c721cccc5
Kadir <kozdemir@salesforce.com>
2019-10-30 21:32:22 -0700

    PHOENIX-5478 IndexTool mapper task should not timeout (addendum for start/close region operation)

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/UngroupedAggregateRegionObserver.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/UngroupedAggregateRegionObserver.java
@@ -1147,6 +1147,7 @@ public class UngroupedAggregateRegionObserver extends BaseScannerRegionObserver 
         @Override
         public boolean next(List<Cell> results) throws IOException {
             int rowCount = 0;
+            region.startRegionOperation();
             try {
                 byte[] uuidValue = ServerCacheClient.generateId();
                 synchronized (innerScanner) {
@@ -1196,9 +1197,7 @@ public class UngroupedAggregateRegionObserver extends BaseScannerRegionObserver 
                 LOGGER.error("IOException during rebuilding: " + Throwables.getStackTraceAsString(e));
                 throw e;
             } finally {
-                if (!hasMore) {
-                    region.closeRegionOperation();
-                }
+                region.closeRegionOperation();
             }
             byte[] rowCountBytes = PLong.INSTANCE.toBytes(Long.valueOf(rowCount));
             final KeyValue aggKeyValue = KeyValueUtil.newKeyValue(UNGROUPED_AGG_ROW_KEY, SINGLE_COLUMN_FAMILY,
@@ -1216,7 +1215,6 @@ public class UngroupedAggregateRegionObserver extends BaseScannerRegionObserver 
     private RegionScanner rebuildIndices(final RegionScanner innerScanner, final Region region, final Scan scan,
                                          final Configuration config) throws IOException {
 
-        region.startRegionOperation();
         RegionScanner scanner = new IndexRebuildRegionScanner(innerScanner, region, scan, config);
         return scanner;
     }
