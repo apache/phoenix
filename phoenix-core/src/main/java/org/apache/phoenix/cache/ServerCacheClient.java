04cd0af9ad5a1b61e88f25fa43d409dfa69220d2
Viraj Jasani <vjasani@apache.org>
2020-01-04 12:14:32 +0530

    PHOENIX-5655 : ServerCache client should remove cache map entry correctly
    Signed-off-by: Chinmay Kulkarni <chinmayskulkarni@apache.org>

--- phoenix-core/src/main/java/org/apache/phoenix/cache/ServerCacheClient.java
+++ phoenix-core/src/main/java/org/apache/phoenix/cache/ServerCacheClient.java
@@ -443,7 +443,7 @@ public class ServerCacheClient {
                         lastThrowable);
             }
         } finally {
-            cacheUsingTableMap.remove(cacheId);
+            cacheUsingTableMap.remove(Bytes.mapKey(cacheId));
             Closeables.closeQuietly(iterateOverTable);
         }
     }
