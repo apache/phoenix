d9592edb1802a2ef76d87febd62a51b889065cf6
Xinyi Yan <xyan@salesforce.com>
2020-03-16 16:59:25 -0700

    PHOENIX-5698 Phoenix Query with RVC IN list expression generates wrong scan with non-pk ordered pks
    Signed-off-by: Xinyi Yan <yanxinyi@apache.org>

--- phoenix-core/src/test/java/org/apache/phoenix/expression/InListExpressionTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/expression/InListExpressionTest.java
@@ -19,13 +19,22 @@ package org.apache.phoenix.expression;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
+
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+
+
+import org.apache.phoenix.schema.types.PInteger;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.phoenix.hbase.index.util.ImmutableBytesPtr;
-import org.junit.Test;
+
 
 public class InListExpressionTest {
 
@@ -61,5 +70,201 @@ public class InListExpressionTest {
             assertEquals("hash code not equal, firstHashCode: " + firstHashCode + ", restHashCode: "
                     + hashCode, firstHashCode, hashCode);
         }
+    }
+
+    @Test
+    public void testGetSortedInListColumnKeyValuePairWithNoPkOrder() {
+        testGetSortedInListColumnKeyValuePair(false);
+    }
+
+    @Test
+    public void testGetSortedInListColumnKeyValuePairWithPkOrder() {
+        testGetSortedInListColumnKeyValuePair(true);
+    }
+
+    private void testGetSortedInListColumnKeyValuePair(boolean isPkOrder) {
+        // mock literal
+        List<Expression> expressionList = new ArrayList<>();
+        LiteralExpression literalChild1 = Mockito.mock(LiteralExpression.class);
+        List<Expression> literalExpressions = new ArrayList<>();
+        when(literalChild1.getDataType()).thenReturn(PInteger.INSTANCE);
+        when(literalChild1.getBytes()).thenReturn(null);
+        when(literalChild1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        literalExpressions.add(literalChild1);
+        literalExpressions.add(literalChild1);
+
+        // mock row key column
+        List<Expression> expressionChildren = new ArrayList<>();
+        RowKeyColumnExpression rowKeyColumnExpressionMock1 = Mockito.mock(RowKeyColumnExpression.class);
+        RowKeyColumnExpression rowKeyColumnExpressionMock2 = Mockito.mock(RowKeyColumnExpression.class);
+
+        when(rowKeyColumnExpressionMock1.getPosition()).thenReturn(1);
+        when(rowKeyColumnExpressionMock1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock2.getPosition()).thenReturn(2);
+        when(rowKeyColumnExpressionMock2.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock1.getChildren()).thenReturn(expressionChildren);
+        when(rowKeyColumnExpressionMock2.getChildren()).thenReturn(literalExpressions);
+
+        // mock row key column PK order position
+        if (isPkOrder) {
+            expressionChildren.add(rowKeyColumnExpressionMock1);
+            expressionChildren.add(rowKeyColumnExpressionMock2);
+
+        } else {
+            expressionChildren.add(rowKeyColumnExpressionMock2);
+            expressionChildren.add(rowKeyColumnExpressionMock1);
+
+        }
+
+        RowValueConstructorExpression rvc1 = new RowValueConstructorExpression(expressionChildren, true);
+        RowValueConstructorExpression rvc2 = new RowValueConstructorExpression(literalExpressions, true);
+        expressionList.add(rvc1);
+        expressionList.add(rvc2);
+
+        if (isPkOrder) {
+            assertEquals(1, ((RowKeyColumnExpression)expressionList.get(0).getChildren().get(0)).getPosition());
+            assertEquals(2, ((RowKeyColumnExpression)expressionList.get(0).getChildren().get(1)).getPosition());
+        } else {
+            assertEquals(2, ((RowKeyColumnExpression)expressionList.get(0).getChildren().get(0)).getPosition());
+            assertEquals(1, ((RowKeyColumnExpression)expressionList.get(0).getChildren().get(1)).getPosition());
+        }
+
+        List<InListExpression.InListColumnKeyValuePair> inListColumnKeyValuePairList =
+                InListExpression.getSortedInListColumnKeyValuePair(expressionList);
+
+        assertEquals(1, inListColumnKeyValuePairList.get(0).getRowKeyColumnExpression().getPosition());
+        assertEquals(2, inListColumnKeyValuePairList.get(1).getRowKeyColumnExpression().getPosition());
+    }
+
+    @Test
+    public void testGetSortedInListColumnKeyValuePairWithLessValueThanPkColumns() {
+        List<Expression> expressionList = new ArrayList<>();
+        LiteralExpression literalChild1 = Mockito.mock(LiteralExpression.class);
+        List<Expression> literalExpressions = new ArrayList<>();
+        when(literalChild1.getDataType()).thenReturn(PInteger.INSTANCE);
+        when(literalChild1.getBytes()).thenReturn(null);
+        when(literalChild1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        literalExpressions.add(literalChild1);
+        literalExpressions.add(literalChild1);
+
+        // mock row key column
+        List<Expression> expressionChildren = new ArrayList<>();
+        RowKeyColumnExpression rowKeyColumnExpressionMock1 = Mockito.mock(RowKeyColumnExpression.class);
+
+        when(rowKeyColumnExpressionMock1.getPosition()).thenReturn(1);
+        when(rowKeyColumnExpressionMock1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock1.getChildren()).thenReturn(expressionChildren);
+
+        expressionChildren.add(rowKeyColumnExpressionMock1);
+
+        RowValueConstructorExpression rvc1 = new RowValueConstructorExpression(expressionChildren, true);
+        RowValueConstructorExpression rvc2 = new RowValueConstructorExpression(literalExpressions, true);
+        expressionList.add(rvc1);
+        expressionList.add(rvc2);
+
+        List<InListExpression.InListColumnKeyValuePair> inListColumnKeyValuePairList =
+                InListExpression.getSortedInListColumnKeyValuePair(expressionList);
+
+        assertEquals(null, inListColumnKeyValuePairList);
+    }
+
+    @Test
+    public void testGetSortedInListColumnKeyValuePairWithMoreValueThanPkColumn() {
+        List<Expression> expressionList = new ArrayList<>();
+        LiteralExpression literalChild1 = Mockito.mock(LiteralExpression.class);
+        List<Expression> literalExpressions = new ArrayList<>();
+        when(literalChild1.getDataType()).thenReturn(PInteger.INSTANCE);
+        when(literalChild1.getBytes()).thenReturn(null);
+        when(literalChild1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        literalExpressions.add(literalChild1);
+
+        // mock row key column
+        List<Expression> expressionChildren = new ArrayList<>();
+        RowKeyColumnExpression rowKeyColumnExpressionMock1 = Mockito.mock(RowKeyColumnExpression.class);
+        when(rowKeyColumnExpressionMock1.getPosition()).thenReturn(1);
+        when(rowKeyColumnExpressionMock1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock1.getChildren()).thenReturn(expressionChildren);
+
+        expressionChildren.add(rowKeyColumnExpressionMock1);
+        expressionChildren.add(rowKeyColumnExpressionMock1);
+
+        RowValueConstructorExpression rvc1 = new RowValueConstructorExpression(expressionChildren, true);
+        RowValueConstructorExpression rvc2 = new RowValueConstructorExpression(literalExpressions, true);
+        expressionList.add(rvc1);
+        expressionList.add(rvc2);
+
+        List<InListExpression.InListColumnKeyValuePair> inListColumnKeyValuePairList =
+                InListExpression.getSortedInListColumnKeyValuePair(expressionList);
+
+        assertEquals(null, inListColumnKeyValuePairList);
+    }
+
+    @Test
+    public void testInListColumnKeyValuePairClass() {
+        RowKeyColumnExpression rowKeyColumnExpression = Mockito.mock(RowKeyColumnExpression.class);
+        LiteralExpression literalChild = Mockito.mock(LiteralExpression.class);
+
+        InListExpression.InListColumnKeyValuePair inListColumnKeyValuePair =
+                new InListExpression.InListColumnKeyValuePair(rowKeyColumnExpression);
+        inListColumnKeyValuePair.addToLiteralExpressionList(literalChild);
+
+        assertEquals(rowKeyColumnExpression, inListColumnKeyValuePair.getRowKeyColumnExpression());
+        assertEquals(literalChild, inListColumnKeyValuePair.getLiteralExpressionList().get(0));
+    }
+
+    @Test
+    public void testGetSortedRowValueConstructorExpressionList() {
+        byte[] bytesValueOne = ByteBuffer.allocate(4).putInt(1).array();
+        byte[] bytesValueTwo = ByteBuffer.allocate(4).putInt(1).array();
+        // mock literal
+        List<Expression> literalExpressions = new ArrayList<>();
+        LiteralExpression literalChild1 = Mockito.mock(LiteralExpression.class);
+        when(literalChild1.getDataType()).thenReturn(PInteger.INSTANCE);
+        when(literalChild1.getBytes()).thenReturn(bytesValueOne);
+        when(literalChild1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        literalExpressions.add(literalChild1);
+
+        LiteralExpression literalChild2 = Mockito.mock(LiteralExpression.class);
+        when(literalChild2.getDataType()).thenReturn(PInteger.INSTANCE);
+        when(literalChild2.getBytes()).thenReturn(bytesValueTwo);
+        when(literalChild2.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        literalExpressions.add(literalChild2);
+
+        List<Expression> expressionChildren = new ArrayList<>();
+        RowKeyColumnExpression rowKeyColumnExpressionMock1 = Mockito.mock(RowKeyColumnExpression.class);
+        RowKeyColumnExpression rowKeyColumnExpressionMock2 = Mockito.mock(RowKeyColumnExpression.class);
+        expressionChildren.add(rowKeyColumnExpressionMock1);
+        expressionChildren.add(rowKeyColumnExpressionMock2);
+
+        when(rowKeyColumnExpressionMock1.getPosition()).thenReturn(1);
+        when(rowKeyColumnExpressionMock1.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock2.getPosition()).thenReturn(2);
+        when(rowKeyColumnExpressionMock2.getDeterminism()).thenReturn(Determinism.ALWAYS);
+        when(rowKeyColumnExpressionMock1.getChildren()).thenReturn(expressionChildren);
+        when(rowKeyColumnExpressionMock2.getChildren()).thenReturn(literalExpressions);
+
+        //construct sorted InListColumnKeyValuePair list
+        List<InListExpression.InListColumnKeyValuePair> children = new ArrayList<>();
+        InListExpression.InListColumnKeyValuePair rvc1 =
+                new InListExpression.InListColumnKeyValuePair(rowKeyColumnExpressionMock1);
+        rvc1.addToLiteralExpressionList(literalChild1);
+        children.add(rvc1);
+        InListExpression.InListColumnKeyValuePair rvc2 =
+                new InListExpression.InListColumnKeyValuePair(rowKeyColumnExpressionMock2);
+        rvc2.addToLiteralExpressionList(literalChild2);
+        children.add(rvc2);
+
+        List<Expression> result = InListExpression.getSortedRowValueConstructorExpressionList(
+                children,true, 1);
+
+        assertTrue(result.get(0).getChildren().get(0) instanceof RowKeyColumnExpression);
+        assertTrue(result.get(0).getChildren().get(1) instanceof RowKeyColumnExpression);
+        assertEquals(1, ((RowKeyColumnExpression)result.get(0).getChildren().get(0)).getPosition());
+        assertEquals(2, ((RowKeyColumnExpression)result.get(0).getChildren().get(1)).getPosition());
+
+        assertTrue(result.get(1).getChildren().get(0) instanceof LiteralExpression);
+        assertTrue(result.get(1).getChildren().get(1) instanceof LiteralExpression);
+        assertEquals(bytesValueOne, ((LiteralExpression)result.get(1).getChildren().get(0)).getBytes());
+        assertEquals(bytesValueTwo, ((LiteralExpression)result.get(1).getChildren().get(1)).getBytes());
     }
 }
