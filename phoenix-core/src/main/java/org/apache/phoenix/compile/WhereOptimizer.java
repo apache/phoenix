e4a8d7e4b28fb8a13bf0958573becad1d35abfa2
Daniel Wong <daniel.wong@salesforce.com>
2020-04-21 16:58:13 -0700

    PHOENIX-5833 Fix Incorrect results with RVCs and AND operator

--- phoenix-core/src/main/java/org/apache/phoenix/compile/WhereOptimizer.java
+++ phoenix-core/src/main/java/org/apache/phoenix/compile/WhereOptimizer.java
@@ -1392,6 +1392,7 @@ public class WhereOptimizer {
                     return KeyRange.EMPTY_RANGE;
                 }
                 // If we're not dealing with single keys, then we can use our normal intersection
+                //   however, if we truncate a span then we need to change exclusive to inclusive
                 if (otherRange.intersect(KeyRange.getKeyRange(trailingBytes)) == KeyRange.EMPTY_RANGE) {
                     // Exit early since the upper range is the same as the lower range
                     if (result.isSingleKey()) {
@@ -1399,6 +1400,9 @@ public class WhereOptimizer {
                     }
                     ptr.set(result.getLowerRange(), 0, lowerOffset - separatorLength);
                     lowerRange = ptr.copyBytes();
+                    if(pkPos < otherPKPos && !lowerInclusive) {
+                        lowerInclusive = true;
+                    }
                 }
             }
             boolean upperInclusive = result.isUpperInclusive();
