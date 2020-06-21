658464545971c6c7d051c1f8046928725e1aed12
chenglei <chenglei@apache.org>
2020-06-21 23:52:43 +0800

    PHOENIX-5970 ViewUtil.dropChildViews may cause HConnection leak

--- phoenix-core/src/main/java/org/apache/phoenix/util/ViewUtil.java
+++ phoenix-core/src/main/java/org/apache/phoenix/util/ViewUtil.java
@@ -189,16 +189,34 @@ public class ViewUtil {
         try {
             hTable = ServerUtil.getHTableForCoprocessorScan(env, SchemaUtil.getPhysicalTableName(
                             sysCatOrSysChildLink, env.getConfiguration()));
-        }
-        catch (Exception e){
+        } catch (Exception e){
+            logger.error("ServerUtil.getHTableForCoprocessorScan error!", e);
         }
         // if the SYSTEM.CATALOG or SYSTEM.CHILD_LINK doesn't exist just return
         if (hTable==null) {
             return;
         }
 
-        TableViewFinderResult childViewsResult = findRelatedViews(hTable, tenantIdBytes, schemaName, tableName,
-                LinkType.CHILD_TABLE, HConstants.LATEST_TIMESTAMP);
+        TableViewFinderResult childViewsResult = null;
+        try {
+            childViewsResult = findRelatedViews(
+                    hTable,
+                    tenantIdBytes,
+                    schemaName,
+                    tableName,
+                    LinkType.CHILD_TABLE,
+                    HConstants.LATEST_TIMESTAMP);
+        } finally {
+            hTable.close();
+        }
+
+        if(childViewsResult == null) {
+           logger.info("tenantIdBytes:" + Bytes.toStringBinary(tenantIdBytes) +
+                       ", schemaName:" + Bytes.toStringBinary(schemaName) +
+                       ", tableName:" + Bytes.toStringBinary(tableName) +
+                       ", ViewUtil.findRelatedViews return null.");
+           return;
+        }
 
         for (TableInfo viewInfo : childViewsResult.getLinks()) {
             byte[] viewTenantId = viewInfo.getTenantId();
