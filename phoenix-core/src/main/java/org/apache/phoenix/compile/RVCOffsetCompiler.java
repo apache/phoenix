c70a6bc616ee94510fffd70f8e90a58569c42032
Istvan Toth <stoty@apache.org>
2020-07-23 18:54:30 +0200

    fix rebase problem

--- phoenix-core/src/main/java/org/apache/phoenix/compile/RVCOffsetCompiler.java
+++ phoenix-core/src/main/java/org/apache/phoenix/compile/RVCOffsetCompiler.java
@@ -17,9 +17,6 @@
  */
 package org.apache.phoenix.compile;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Optional;
-import com.google.common.collect.Lists;
 import org.apache.phoenix.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.phoenix.thirdparty.com.google.common.base.Optional;
 import org.apache.phoenix.thirdparty.com.google.common.collect.Lists;
