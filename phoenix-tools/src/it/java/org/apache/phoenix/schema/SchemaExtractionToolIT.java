63bf4a9dd5390ec001e27c42a0077c1b6d6795e4
Swaroopa Kadam <swaroopa.kadam07@gmail.com>
2020-07-27 15:58:55 -0700

    PHOENIX-5946: Implement SchemaExtractionTool utility to get effective… (#806)
    * PHOENIX-5946: Implement SchemaExtractionTool utility to get effective DDL from the cluster (Co-authored by Tanuj Khurana)
    
    Co-authored-by: s.kadam <s.kadam@apache.org>

---
+++ phoenix-tools/src/it/java/org/apache/phoenix/schema/SchemaExtractionToolIT.java
@@ -0,0 +1,213 @@
+package org.apache.phoenix.schema;
+
+import org.apache.phoenix.jdbc.PhoenixConnection;
+import org.apache.phoenix.query.BaseTest;
+import org.apache.phoenix.util.PhoenixRuntime;
+import org.apache.phoenix.util.PropertiesUtil;
+import org.apache.phoenix.util.ReadOnlyProps;
+import org.apache.phoenix.util.SchemaUtil;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collections;
+
+import java.util.Map;
+import java.util.Properties;
+
+import static org.apache.phoenix.util.TestUtil.TEST_PROPERTIES;
+
+public class SchemaExtractionToolIT extends BaseTest {
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        Map<String, String> props = Collections.emptyMap();
+        setUpTestDriver(new ReadOnlyProps(props.entrySet().iterator()));
+    }
+
+    @Test
+    public void testCreateTableStatement() throws Exception {
+        String tableName = generateUniqueName();
+        String schemaName = generateUniqueName();
+        Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+        String properties = "TTL=2592000,IMMUTABLE_ROWS=true,DISABLE_WAL=true";
+
+        try (Connection conn = DriverManager.getConnection(getUrl(), props)) {
+
+            String pTableFullName = SchemaUtil.getQualifiedTableName(schemaName, tableName);
+            conn.createStatement().execute("CREATE TABLE "+ pTableFullName + "(k VARCHAR NOT NULL PRIMARY KEY, "
+                    + "v1 VARCHAR, v2 VARCHAR)"
+                    + properties);
+            conn.commit();
+            String [] args = {"-tb", tableName, "-s", schemaName};
+
+            SchemaExtractionTool set = new SchemaExtractionTool();
+            set.setConf(conn.unwrap(PhoenixConnection.class).getQueryServices().getConfiguration());
+            set.run(args);
+            String actualProperties = set.getOutput().substring(set.getOutput().lastIndexOf(")")+1).replace(" ","");
+            Assert.assertEquals(3, actualProperties.split(",").length);
+        }
+    }
+
+    @Test
+    public void testCreateTableStatement_tenant() throws Exception {
+        String tableName = generateUniqueName();
+        String viewName = generateUniqueName();
+        String schemaName = generateUniqueName();
+        String tenantId = "abc";
+        Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+        String pTableFullName = SchemaUtil.getQualifiedTableName(schemaName, tableName);
+        String properties = "TTL=2592000,IMMUTABLE_ROWS=true,DISABLE_WAL=true";
+        SchemaExtractionTool set;
+        String viewFullName = SchemaUtil.getQualifiedTableName(schemaName, viewName);
+        String createView = "CREATE VIEW "+viewFullName + "(id1 BIGINT, id2 BIGINT NOT NULL, "
+                + "id3 VARCHAR NOT NULL CONSTRAINT PKVIEW PRIMARY KEY (id2, id3 DESC)) "
+                + "AS SELECT * FROM "+pTableFullName;
+
+        try (Connection conn = DriverManager.getConnection(getUrl(), props)) {
+
+            conn.createStatement().execute("CREATE TABLE "+pTableFullName + "(k BIGINT NOT NULL PRIMARY KEY, "
+                    + "v1 VARCHAR, v2 VARCHAR)"
+                    + properties);
+            set = new SchemaExtractionTool();
+            set.setConf(conn.unwrap(PhoenixConnection.class).getQueryServices().getConfiguration());
+            conn.commit();
+        }
+        try (Connection conn = getTenantConnection(getUrl(), tenantId)) {
+            conn.createStatement().execute(createView);
+            conn.commit();
+        }
+        String [] args = {"-tb", viewName, "-s", schemaName, "-t", tenantId};
+        set.run(args);
+        Assert.assertEquals(createView.toUpperCase(), set.getOutput().toUpperCase());
+    }
+
+    private Connection getTenantConnection(String url, String tenantId) throws SQLException {
+        Properties props = new Properties();
+        props.setProperty(PhoenixRuntime.TENANT_ID_ATTRIB, tenantId);
+        return DriverManager.getConnection(url, props);
+    }
+
+    @Test
+    public void testCreateIndexStatement() throws Exception {
+        String tableName = generateUniqueName();
+        String schemaName = generateUniqueName();
+        String indexName = generateUniqueName();
+        String indexName1 = generateUniqueName();
+        String indexName2 = generateUniqueName();
+        Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+        String properties = "TTL=2592000,IMMUTABLE_ROWS=true,DISABLE_WAL=true";
+
+        try (Connection conn = DriverManager.getConnection(getUrl(), props)) {
+
+            String pTableFullName = SchemaUtil.getQualifiedTableName(schemaName, tableName);
+            conn.createStatement().execute("CREATE TABLE "+pTableFullName + "(k VARCHAR NOT NULL PRIMARY KEY, v1 VARCHAR, v2 VARCHAR)"
+                    + properties);
+
+            String createIndexStatement = "CREATE INDEX "+indexName + " ON "+pTableFullName+"(v1 DESC) INCLUDE (v2)";
+
+            String createIndexStatement1 = "CREATE INDEX "+indexName1 + " ON "+pTableFullName+"(v2 DESC) INCLUDE (v1)";
+
+            String createIndexStatement2 = "CREATE INDEX "+indexName2 + " ON "+pTableFullName+"(k)";
+
+            conn.createStatement().execute(createIndexStatement);
+            conn.createStatement().execute(createIndexStatement1);
+            conn.createStatement().execute(createIndexStatement2);
+            conn.commit();
+            SchemaExtractionTool set = new SchemaExtractionTool();
+            set.setConf(conn.unwrap(PhoenixConnection.class).getQueryServices().getConfiguration());
+
+            String [] args = {"-tb", indexName, "-s", schemaName};
+            set.run(args);
+            Assert.assertEquals(createIndexStatement.toUpperCase(), set.getOutput().toUpperCase());
+
+            String [] args1 = {"-tb", indexName1, "-s", schemaName};
+            set.run(args1);
+            Assert.assertEquals(createIndexStatement1.toUpperCase(), set.getOutput().toUpperCase());
+
+            String [] args2 = {"-tb", indexName2, "-s", schemaName};
+            set.run(args2);
+            Assert.assertEquals(createIndexStatement2.toUpperCase(), set.getOutput().toUpperCase());
+        }
+    }
+
+    @Test
+    public void testCreateViewStatement() throws Exception {
+        String tableName = generateUniqueName();
+        String schemaName = generateUniqueName();
+        String viewName = generateUniqueName();
+        Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+        String properties = "TTL=2592000,IMMUTABLE_ROWS=true,DISABLE_WAL=true";
+
+        try (Connection conn = DriverManager.getConnection(getUrl(), props)) {
+
+            String pTableFullName = SchemaUtil.getQualifiedTableName(schemaName, tableName);
+            conn.createStatement().execute("CREATE TABLE "+pTableFullName + "(k BIGINT NOT NULL PRIMARY KEY, "
+                    + "v1 VARCHAR, v2 VARCHAR)"
+                    + properties);
+            String viewFullName = SchemaUtil.getQualifiedTableName(schemaName, viewName);
+            String viewFullName1 = SchemaUtil.getQualifiedTableName(schemaName, viewName+"1");
+
+
+            String createView = "CREATE VIEW "+viewFullName + "(id1 BIGINT, id2 BIGINT NOT NULL, "
+                    + "id3 VARCHAR NOT NULL CONSTRAINT PKVIEW PRIMARY KEY (id2, id3 DESC)) "
+                    + "AS SELECT * FROM "+pTableFullName;
+            String createView1 = "CREATE VIEW "+viewFullName1 + "(id1 BIGINT, id2 BIGINT NOT NULL, "
+                    + "id3 VARCHAR NOT NULL CONSTRAINT PKVIEW PRIMARY KEY (id2, id3 DESC)) "
+                    + "AS SELECT * FROM "+pTableFullName;
+
+            conn.createStatement().execute(createView);
+            conn.createStatement().execute(createView1);
+            conn.commit();
+            String [] args = {"-tb", viewName, "-s", schemaName};
+
+            SchemaExtractionTool set = new SchemaExtractionTool();
+            set.setConf(conn.unwrap(PhoenixConnection.class).getQueryServices().getConfiguration());
+            set.run(args);
+            Assert.assertEquals(createView.toUpperCase(), set.getOutput().toUpperCase());
+        }
+    }
+
+    @Test
+    public void testCreateViewIndexStatement() throws Exception {
+        String tableName = generateUniqueName();
+        String schemaName = generateUniqueName();
+        String viewName = generateUniqueName();
+        String childView = generateUniqueName();
+        String indexName = generateUniqueName();
+        Properties props = PropertiesUtil.deepCopy(TEST_PROPERTIES);
+        String properties = "TTL=2592000,IMMUTABLE_ROWS=true,DISABLE_WAL=true";
+
+        try (Connection conn = DriverManager.getConnection(getUrl(), props)) {
+
+            String pTableFullName = SchemaUtil.getQualifiedTableName(schemaName, tableName);
+            conn.createStatement().execute("CREATE TABLE "+pTableFullName + "(k BIGINT NOT NULL PRIMARY KEY, "
+                    + "v1 VARCHAR, v2 VARCHAR)"
+                    + properties);
+            String viewFullName = SchemaUtil.getQualifiedTableName(schemaName, viewName);
+            String childviewName = SchemaUtil.getQualifiedTableName(schemaName, childView);
+
+            String createView = "CREATE VIEW "+viewFullName + "(id1 BIGINT, id2 BIGINT NOT NULL, "
+                    + "id3 VARCHAR NOT NULL CONSTRAINT PKVIEW PRIMARY KEY (id2, id3 DESC)) "
+                    + "AS SELECT * FROM "+pTableFullName;
+
+            String createView1 = "CREATE VIEW "+childviewName + " AS SELECT * FROM "+viewFullName;
+
+            String createIndexStatement = "CREATE INDEX "+indexName + " ON "+childviewName+"(id1) INCLUDE (v1)";
+
+            conn.createStatement().execute(createView);
+            conn.createStatement().execute(createView1);
+            conn.createStatement().execute(createIndexStatement);
+            conn.commit();
+            String [] args = {"-tb", indexName, "-s", schemaName};
+
+            SchemaExtractionTool set = new SchemaExtractionTool();
+            set.setConf(conn.unwrap(PhoenixConnection.class).getQueryServices().getConfiguration());
+            set.run(args);
+            Assert.assertEquals(createIndexStatement.toUpperCase(), set.getOutput().toUpperCase());
+        }
+    }
+}
---
+++ phoenix-tools/src/main/java/org/apache/phoenix/schema/SchemaExtractionTool.java
@@ -0,0 +1,109 @@
+package org.apache.phoenix.schema;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import java.util.logging.Logger;
+
+public class SchemaExtractionTool extends Configured implements Tool {
+
+    private static final Logger LOGGER = Logger.getLogger(SchemaExtractionTool.class.getName());
+    private static final Option HELP_OPTION = new Option("h", "help",
+            false, "Help");
+    private static final Option TABLE_OPTION = new Option("tb", "table", true,
+            "[Required] Table name ex. table1");
+    private static final Option SCHEMA_OPTION = new Option("s", "schema", true,
+            "[Optional] Schema name ex. schema");
+    private static final Option TENANT_OPTION = new Option("t", "tenant", true,
+            "[Optional] Tenant Id ex. abc");
+
+    private String pTableName;
+    private String pSchemaName;
+    private String tenantId;
+
+    public static Configuration conf;
+    private String output;
+
+    @Override
+    public int run(String[] args) throws Exception {
+        populateToolAttributes(args);
+        conf = HBaseConfiguration.addHbaseResources(getConf());
+        SchemaExtractionProcessor processor = new SchemaExtractionProcessor(tenantId,
+                conf, pSchemaName, pTableName);
+        output = processor.process();
+        return 0;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    private void populateToolAttributes(String[] args) {
+        try {
+            CommandLine cmdLine = parseOptions(args);
+            pTableName = cmdLine.getOptionValue(TABLE_OPTION.getOpt());
+            pSchemaName = cmdLine.getOptionValue(SCHEMA_OPTION.getOpt());
+            tenantId = cmdLine.getOptionValue(TENANT_OPTION.getOpt());
+            LOGGER.info("Schema Extraction Tool initiated: " + StringUtils.join( args, ","));
+        } catch (IllegalStateException e) {
+            printHelpAndExit(e.getMessage(), getOptions());
+        }
+    }
+
+    private CommandLine parseOptions(String[] args) {
+        final Options options = getOptions();
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmdLine = null;
+        try {
+            cmdLine = parser.parse(options, args);
+        } catch (ParseException e) {
+            printHelpAndExit("severe parsing command line options: " + e.getMessage(),
+                    options);
+        }
+        if (cmdLine.hasOption(HELP_OPTION.getOpt())) {
+            printHelpAndExit(options, 0);
+        }
+        if (!(cmdLine.hasOption(TABLE_OPTION.getOpt()))) {
+            throw new IllegalStateException("Table name should be passed "
+                    +TABLE_OPTION.getLongOpt());
+        }
+        return cmdLine;
+    }
+
+    private Options getOptions() {
+        final Options options = new Options();
+        options.addOption(TABLE_OPTION);
+        SCHEMA_OPTION.setOptionalArg(true);
+        options.addOption(SCHEMA_OPTION);
+        TENANT_OPTION.setOptionalArg(true);
+        options.addOption(TENANT_OPTION);
+        return options;
+    }
+
+    private void printHelpAndExit(String severeMessage, Options options) {
+        System.err.println(severeMessage);
+        printHelpAndExit(options, 1);
+    }
+
+    private void printHelpAndExit(Options options, int exitCode) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("help", options);
+        System.exit(exitCode);
+    }
+
+    public static void main (String[] args) throws Exception {
+        int result = ToolRunner.run(new SchemaExtractionTool(), args);
+        System.exit(result);
+    }
+}
