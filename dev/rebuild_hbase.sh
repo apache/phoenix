aac2633a964ab47905b9b03f4b7df3a4fdf6056f
Istvan Toth <stoty@apache.org>
2020-08-14 11:09:16 +0200

    PHOENIX-6077 PHOENIX-5946 breaks mvn verify
    make sure IT is run by maven
    replace commomns-lang with commons-lang3 like in the rest of Phoenix
    POM cleanups
    (unrelated) add ASL2 header to to rebuild_hbase.sh

--- dev/rebuild_hbase.sh
+++ dev/rebuild_hbase.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# Rebuilds HBase with -Dhadoop.profile=3.0 locally, to work around PHOENIX-5993
+# Intended mainly for CI jobs, but can simplify manual rebuilds as well.
+
 # The name of the Apache Hbase source file
 HBASE_SOURCE_NAME="hbase-$1-src.tar.gz"
 # The relative path on the ASF mirrors for the Hbase source file
