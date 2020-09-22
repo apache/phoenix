9cc1996a6439a9f278e2a0aef3705a008bb2680b
Jacob Isaac <jisaac@salesforce.com>
2020-09-22 12:26:19 -0700

    Added some more defaults and configs

--- phoenix-pherf/src/main/java/org/apache/phoenix/pherf/configuration/LoadProfile.java
+++ phoenix-pherf/src/main/java/org/apache/phoenix/pherf/configuration/LoadProfile.java
@@ -23,16 +23,60 @@ import java.util.List;
 
 @XmlType
 public class LoadProfile {
-    public static int MIN_BATCH_SIZE = 1;
+    private static final int MIN_BATCH_SIZE = 1;
+    private static final String DEFAULT_TENANT_ID_FMT = "00D%s%07d";
+    private static final int DEFAULT_GROUP_ID_LEN = 5;
+    private static final int DEFAULT_TENANT_ID_LEN = 15;
 
+    // Holds the batch size to be used in upserts.
     private int batchSize;
+    // Holds the number of operations to be generated.
     private long numOperations;
+    /**
+     * Holds the format to be used when generating tenantIds.
+     * TenantId format should typically have 2 parts -
+     * 1. string fmt - that hold the tenant group id.
+     * 2. int fmt - that holds a random number between 1 and max tenants
+     * for e.g DEFAULT_TENANT_ID_FMT = "00D%s%07d";
+     */
+    private String tenantIdFormat;
+    private int groupIdLength;
+    private int tenantIdLength;
+    // Holds the desired tenant distribution for this load.
     List<TenantGroup> tenantDistribution;
+    // Holds the desired operation distribution for this load.
     List<OperationGroup> opDistribution;
 
     public LoadProfile() {
         this.batchSize = MIN_BATCH_SIZE;
         this.numOperations = Long.MAX_VALUE;
+        this.tenantIdFormat = DEFAULT_TENANT_ID_FMT;
+        this.tenantIdLength = DEFAULT_TENANT_ID_LEN;
+        this.groupIdLength = DEFAULT_GROUP_ID_LEN;
+    }
+
+    public String getTenantIdFormat() {
+        return tenantIdFormat;
+    }
+
+    public void setTenantIdFormat(String tenantIdFormat) {
+        this.tenantIdFormat = tenantIdFormat;
+    }
+
+    public int getTenantIdLength() {
+        return tenantIdLength;
+    }
+
+    public void setTenantIdLength(int tenantIdLength) {
+        this.tenantIdLength = tenantIdLength;
+    }
+
+    public int getGroupIdLength() {
+        return groupIdLength;
+    }
+
+    public void setGroupIdLength(int groupIdLength) {
+        this.groupIdLength = groupIdLength;
     }
 
     public int getBatchSize() {
