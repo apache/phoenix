185654888a2e596b1f807bee73ed3aef42110448
Viraj Jasani <vjasani@apache.org>
2020-10-15 20:34:17 +0530

    fixing checkstyle

--- phoenix-core/src/main/java/org/apache/phoenix/exception/SQLExceptionCode.java
+++ phoenix-core/src/main/java/org/apache/phoenix/exception/SQLExceptionCode.java
@@ -453,10 +453,13 @@ public enum SQLExceptionCode {
     OPERATION_TIMED_OUT(6000, "TIM01", "Operation timed out.", new Factory() {
         @Override
         public SQLException newException(SQLExceptionInfo info) {
-            final String reason = (info.getMessage() != null ? info.getMessage() : "")
-                + (info.getRootCause() != null ? " , rootCause: " + info.getRootCause() : "");
+            final String reason =
+                (info.getMessage() != null ? info.getMessage() : "")
+                    + (info.getRootCause() != null ? " , rootCause: "
+                    + info.getRootCause() : "");
             return new SQLTimeoutException(reason,
-                OPERATION_TIMED_OUT.getSQLState(), OPERATION_TIMED_OUT.getErrorCode());
+                OPERATION_TIMED_OUT.getSQLState(),
+                OPERATION_TIMED_OUT.getErrorCode());
         }
     }),
     FUNCTION_UNDEFINED(6001, "42F01", "Function undefined.", new Factory() {
