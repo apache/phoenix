ba09e361795fa2be821ccbdb401899e7dd304fa6
Jacob Isaac <jisaac@salesforce.com>
2020-10-25 12:46:49 -0700

    PHOENIX-6171 Child views should not be allowed to override the parent view PHOENIX_TTL attribute.

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/MetaDataEndpointImpl.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/MetaDataEndpointImpl.java
@@ -60,6 +60,7 @@ import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SORT_ORDER_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STORAGE_SCHEME_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STORE_NULLS_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CHILD_LINK_NAME_BYTES;
+import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CATALOG_NAME_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.TABLE_FAMILY_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.TABLE_SEQ_NUM_BYTES;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.TABLE_TYPE_BYTES;
@@ -117,11 +118,13 @@ import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorException;
 import org.apache.hadoop.hbase.coprocessor.CoprocessorService;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
 import org.apache.hadoop.hbase.filter.FirstKeyOnlyFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.ipc.RpcServer.Call;
@@ -132,6 +135,7 @@ import org.apache.hadoop.hbase.regionserver.RegionScanner;
 import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.hbase.util.Pair;
+import org.apache.hadoop.hbase.util.SimplePositionedByteRange;
 import org.apache.hadoop.hbase.util.VersionInfo;
 import org.apache.phoenix.cache.GlobalCache;
 import org.apache.phoenix.cache.GlobalCache.FunctionBytesPtr;
@@ -2605,11 +2609,14 @@ public class MetaDataEndpointImpl extends MetaDataProtocol implements Coprocesso
      *     long, boolean)}
      */
     private MetaDataMutationResult validateIfMutationAllowedOnParent(
+            final PTable parentTable,
+            final List<Mutation> tableMetadata,
             final PTableType expectedType, final long clientTimeStamp,
             final byte[] tenantId, final byte[] schemaName,
             final byte[] tableOrViewName, final List<PTable> childViews,
             final int clientVersion) throws IOException, SQLException {
         boolean isMutationAllowed = true;
+        boolean isSchemaMutationAllowed = true;
         if (expectedType == PTableType.TABLE || expectedType == PTableType.VIEW) {
             try (Table hTable = env.getTable(getSystemTableForChildLinks(clientVersion,
                     env.getConfiguration()))) {
@@ -2638,12 +2645,33 @@ public class MetaDataEndpointImpl extends MetaDataProtocol implements Coprocesso
                         QueryServices.ALLOW_SPLITTABLE_SYSTEM_CATALOG_ROLLBACK);
                 }
             }
+            if (settingNewPhoenixTTLAttribute(tableMetadata, PHOENIX_TTL_BYTES)) {
+                // Disallow if the parent has PHOENIX_TTL set.
+                if (parentTable != null &&  parentTable.getPhoenixTTL() != PHOENIX_TTL_NOT_DEFINED) {
+                    isSchemaMutationAllowed = false;
+                }
+
+                // Since we do not allow propagation of PHOENIX_TTL values during ALTER for now.
+                // If a child view exists and the parent previously had a PHOENIX_TTL value set then that
+                // implies that the child view too has a valid PHOENIX_TTL (non zero).
+                // In this case we do not allow for ALTER of the parent view PHOENIX_TTL value.
+                if (!childViews.isEmpty()) {
+                    isSchemaMutationAllowed = false;
+                }
+            }
         }
         if (!isMutationAllowed) {
             MetaDataMutationResult metaDataMutationResult =
                 new MetaDataMutationResult(
                     MetaDataProtocol.MutationCode.UNALLOWED_TABLE_MUTATION,
                     EnvironmentEdgeManager.currentTimeMillis(), null);
+            return metaDataMutationResult;
+        }
+        if (!isSchemaMutationAllowed) {
+            MetaDataMutationResult metaDataMutationResult =
+                    new MetaDataMutationResult(
+                            MetaDataProtocol.MutationCode.UNALLOWED_SCHEMA_MUTATION,
+                            EnvironmentEdgeManager.currentTimeMillis(), null);
             return metaDataMutationResult;
         }
         return null;
@@ -2677,6 +2705,7 @@ public class MetaDataEndpointImpl extends MetaDataProtocol implements Coprocesso
             try {
                 List<PTable> childViews = Lists.newArrayList();
                 MetaDataMutationResult mutationResult = validateIfMutationAllowedOnParent(
+                        parentTable, tableMetadata,
                         expectedType, clientTimeStamp, tenantId, schemaName, tableOrViewName,
                         childViews, clientVersion);
                 // only if mutation is allowed, we should get Optional.empty() here
@@ -2922,6 +2951,23 @@ public class MetaDataEndpointImpl extends MetaDataProtocol implements Coprocesso
             queryServices.clearTableFromCache(ByteUtil.EMPTY_BYTE_ARRAY, schemaName, tableName,
                     clientTimeStamp);
         }
+    }
+
+    // Checks whether a non-zero PHOENIX_TTL value is being set.
+    private boolean settingNewPhoenixTTLAttribute(List<Mutation> tableMetadata, byte[] phoenixTtlBytes) {
+        for (Mutation m : tableMetadata) {
+            if (m instanceof Put) {
+                Put p = (Put)m;
+                List<Cell> cells = p.get(TABLE_FAMILY_BYTES, phoenixTtlBytes);
+                if (cells != null && cells.size() > 0) {
+                    Cell cell = cells.get(0);
+                    long newPhoenixTTL = (long) PLong.INSTANCE.toObject(cell.getValueArray(),
+                            cell.getValueOffset(), cell.getValueLength());
+                    return newPhoenixTTL != PHOENIX_TTL_NOT_DEFINED ;
+                }
+            }
+        }
+        return false;
     }
 
     public static class ColumnFinder extends StatelessTraverseAllExpressionVisitor<Void> {
