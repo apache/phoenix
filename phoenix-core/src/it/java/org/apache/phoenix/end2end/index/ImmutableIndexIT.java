a511e38519d7de9658dd7d6bdd4f061f45f70ed2
Tanuj Khurana <khurana.tanuj@gmail.com>
2020-11-12 16:41:00 -0800

    Refactor tests to reduce duplicate code. Also, added similar tests for mutable indexes.

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/index/ImmutableIndexIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/index/ImmutableIndexIT.java
@@ -632,6 +632,36 @@ public class ImmutableIndexIT extends BaseUniqueNamesOwnClusterIT {
         }
     }
 
+    private void setupForDeleteCount(Connection conn, String schemaName, String dataTableName,
+            String indexTableName1, String indexTableName2) throws SQLException {
+
+        String dataTableFullName = SchemaUtil.getTableName(schemaName, dataTableName);
+
+        conn.createStatement().execute("CREATE TABLE " + dataTableFullName
+            + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
+            + this.tableDDLOptions);
+
+        if (indexTableName1 != null) {
+            conn.createStatement().execute(String.format(
+                "CREATE INDEX %s ON %s (VAL1) INCLUDE (VAL2)", indexTableName1, dataTableFullName));
+        }
+
+        if (indexTableName2 != null) {
+            conn.createStatement().execute(String.format(
+                "CREATE INDEX %s ON %s (VAL2) INCLUDE (VAL1)", indexTableName2, dataTableFullName));
+        }
+
+        PreparedStatement dataPreparedStatement =
+            conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
+        for (int i = 1; i <= 10; i++) {
+            dataPreparedStatement.setInt(1, i);
+            dataPreparedStatement.setInt(2, i + 1);
+            dataPreparedStatement.setInt(3, i * 2);
+            dataPreparedStatement.execute();
+        }
+        conn.commit();
+    }
+
     @Test
     public void testDeleteCount_PK() throws Exception {
         String schemaName = generateUniqueName();
@@ -640,23 +670,7 @@ public class ImmutableIndexIT extends BaseUniqueNamesOwnClusterIT {
         String indexTableName = "IND_" + generateUniqueName();
 
         try (Connection conn = DriverManager.getConnection(getUrl())) {
-
-            conn.createStatement().execute("CREATE TABLE " + dataTableFullName
-                + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
-                + this.tableDDLOptions);
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL1) INCLUDE (VAL2)", indexTableName, dataTableFullName));
-
-            PreparedStatement dataPreparedStatement =
-                conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
-            for (int i = 1; i <= 10; i++) {
-                dataPreparedStatement.setInt(1, i);
-                dataPreparedStatement.setInt(2, i + 1);
-                dataPreparedStatement.setInt(3, i * 2);
-                dataPreparedStatement.execute();
-            }
-            conn.commit();
+            setupForDeleteCount(conn, schemaName, dataTableName, indexTableName, null);
 
             PreparedStatement deleteStmt =
                 conn.prepareStatement("DELETE FROM " + dataTableFullName + " WHERE ID > 5");
@@ -670,30 +684,11 @@ public class ImmutableIndexIT extends BaseUniqueNamesOwnClusterIT {
         String schemaName = generateUniqueName();
         String dataTableName = "TBL_" + generateUniqueName();
         String dataTableFullName = SchemaUtil.getTableName(schemaName, dataTableName);
-        String indexTableName = "IND_" + generateUniqueName();
+        String indexTableName1 = "IND_" + generateUniqueName();
         String indexTableName2 = "IND_" + generateUniqueName();
 
         try (Connection conn = DriverManager.getConnection(getUrl())) {
-
-            conn.createStatement().execute("CREATE TABLE " + dataTableFullName
-                + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
-                + this.tableDDLOptions);
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL1) INCLUDE (VAL2)", indexTableName, dataTableFullName));
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL2) INCLUDE (VAL1)", indexTableName2, dataTableFullName));
-
-            PreparedStatement dataPreparedStatement =
-                conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
-            for (int i = 1; i <= 10; i++) {
-                dataPreparedStatement.setInt(1, i);
-                dataPreparedStatement.setInt(2, i + 1);
-                dataPreparedStatement.setInt(3, i * 2);
-                dataPreparedStatement.execute();
-            }
-            conn.commit();
+            setupForDeleteCount(conn, schemaName, dataTableName, indexTableName1, indexTableName2);
 
             PreparedStatement deleteStmt =
                 conn.prepareStatement("DELETE FROM " + dataTableFullName + " WHERE VAL1 > 6");
@@ -707,67 +702,11 @@ public class ImmutableIndexIT extends BaseUniqueNamesOwnClusterIT {
         String schemaName = generateUniqueName();
         String dataTableName = "TBL_" + generateUniqueName();
         String dataTableFullName = SchemaUtil.getTableName(schemaName, dataTableName);
-        String indexTableName = "IND_" + generateUniqueName();
+        String indexTableName1 = "IND_" + generateUniqueName();
         String indexTableName2 = "IND_" + generateUniqueName();
 
         try (Connection conn = DriverManager.getConnection(getUrl())) {
-
-            conn.createStatement().execute("CREATE TABLE " + dataTableFullName
-                + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
-                + this.tableDDLOptions);
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL1) INCLUDE (VAL2)", indexTableName, dataTableFullName));
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL2) INCLUDE (VAL1)", indexTableName2, dataTableFullName));
-
-            PreparedStatement dataPreparedStatement =
-                conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
-            for (int i = 1; i <= 10; i++) {
-                dataPreparedStatement.setInt(1, i);
-                dataPreparedStatement.setInt(2, i + 1);
-                dataPreparedStatement.setInt(3, i * 2);
-                dataPreparedStatement.execute();
-            }
-            conn.commit();
-
-            PreparedStatement deleteStmt =
-                conn.prepareStatement("DELETE FROM " + dataTableFullName + " WHERE VAL1 > 6 LIMIT 3");
-            assertEquals(3, deleteStmt.executeUpdate());
-            conn.commit();
-        }
-    }
-
-    @Test
-    public void testDeleteCount_noCoveredColumn() throws Exception {
-        String schemaName = generateUniqueName();
-        String dataTableName = "TBL_" + generateUniqueName();
-        String dataTableFullName = SchemaUtil.getTableName(schemaName, dataTableName);
-        String indexTableName = "IND_" + generateUniqueName();
-        String indexTableName2 = "IND_" + generateUniqueName();
-
-        try (Connection conn = DriverManager.getConnection(getUrl())) {
-
-            conn.createStatement().execute("CREATE TABLE " + dataTableFullName
-                + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
-                + this.tableDDLOptions);
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL1)", indexTableName, dataTableFullName));
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL2)", indexTableName2, dataTableFullName));
-
-            PreparedStatement dataPreparedStatement =
-                conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
-            for (int i = 1; i <= 10; i++) {
-                dataPreparedStatement.setInt(1, i);
-                dataPreparedStatement.setInt(2, i + 1);
-                dataPreparedStatement.setInt(3, i * 2);
-                dataPreparedStatement.execute();
-            }
-            conn.commit();
+            setupForDeleteCount(conn, schemaName, dataTableName, indexTableName1, indexTableName2);
 
             PreparedStatement deleteStmt =
                 conn.prepareStatement("DELETE FROM " + dataTableFullName + " WHERE VAL1 > 6 LIMIT 3");
@@ -780,28 +719,11 @@ public class ImmutableIndexIT extends BaseUniqueNamesOwnClusterIT {
     public void testDeleteCount_index() throws Exception {
         String schemaName = generateUniqueName();
         String dataTableName = "TBL_" + generateUniqueName();
-        String dataTableFullName = SchemaUtil.getTableName(schemaName, dataTableName);
         String indexTableName = "IND_" + generateUniqueName();
         String indexTableFullName = SchemaUtil.getTableName(schemaName, indexTableName);
 
         try (Connection conn = DriverManager.getConnection(getUrl())) {
-
-            conn.createStatement().execute("CREATE TABLE " + dataTableFullName
-                + " (ID INTEGER NOT NULL PRIMARY KEY, VAL1 INTEGER, VAL2 INTEGER) "
-                + this.tableDDLOptions);
-
-            conn.createStatement().execute(String.format(
-                "CREATE INDEX %s ON %s (VAL1)", indexTableName, dataTableFullName));
-
-            PreparedStatement dataPreparedStatement =
-                conn.prepareStatement("UPSERT INTO " + dataTableFullName + " VALUES(?,?,?)");
-            for (int i = 1; i <= 10; i++) {
-                dataPreparedStatement.setInt(1, i);
-                dataPreparedStatement.setInt(2, i + 1);
-                dataPreparedStatement.setInt(3, i * 2);
-                dataPreparedStatement.execute();
-            }
-            conn.commit();
+            setupForDeleteCount(conn, schemaName, dataTableName, indexTableName, null);
 
             PreparedStatement deleteStmt =
                 conn.prepareStatement("DELETE FROM " + indexTableFullName + " WHERE \"0:VAL1\" > 6");
