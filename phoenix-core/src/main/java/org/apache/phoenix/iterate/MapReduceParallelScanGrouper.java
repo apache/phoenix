28825235519c3d5e4d3ea22d72dd93be0601bfde
sakshamgangwar <sakshamgangwar19@gmail.com>
2021-01-15 18:31:11 -0800

    removing cleanup

--- phoenix-core/src/main/java/org/apache/phoenix/iterate/MapReduceParallelScanGrouper.java
+++ phoenix-core/src/main/java/org/apache/phoenix/iterate/MapReduceParallelScanGrouper.java
@@ -17,11 +17,9 @@
  */
 package org.apache.phoenix.iterate;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.apache.hadoop.hbase.snapshot.RestoreSnapshotHelper;
 import org.apache.phoenix.thirdparty.com.google.common.annotations.VisibleForTesting;
 import org.apache.phoenix.thirdparty.com.google.common.base.Preconditions;
 import org.apache.phoenix.thirdparty.com.google.common.collect.Lists;
@@ -41,8 +39,6 @@ import org.apache.hadoop.hbase.snapshot.SnapshotManifest;
 import org.apache.phoenix.compile.QueryPlan;
 import org.apache.phoenix.compile.StatementContext;
 import org.apache.phoenix.mapreduce.util.PhoenixConfigurationUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Scan grouper that creates a scan group if a plan is row key ordered or if a
@@ -51,7 +47,6 @@ import org.slf4j.LoggerFactory;
 public class MapReduceParallelScanGrouper implements ParallelScanGrouper {
 
 	private static final MapReduceParallelScanGrouper INSTANCE = new MapReduceParallelScanGrouper();
-	private static final Logger LOGGER = LoggerFactory.getLogger(MapReduceParallelScanGrouper.class);
 
     public static MapReduceParallelScanGrouper getInstance() {
 		return INSTANCE;
@@ -76,11 +71,6 @@ public class MapReduceParallelScanGrouper implements ParallelScanGrouper {
 				FileSystem fs = rootDir.getFileSystem(conf);
 				Path snapshotDir = SnapshotDescriptionUtils.getCompletedSnapshotDir(snapshotName, rootDir);
 				SnapshotDescription snapshotDescription = SnapshotDescriptionUtils.readSnapshotInfo(fs, snapshotDir);
-				//Perform a restore directory cleanup
-				cleanup(fs, new Path(conf.get(PhoenixConfigurationUtil.RESTORE_DIR_KEY)));
-				//Performing snapshot restore which will be used during scans
-				Path restoreDir = new Path(conf.get(PhoenixConfigurationUtil.RESTORE_DIR_KEY));
-				RestoreSnapshotHelper.copySnapshotForScanner(conf, fs, rootDir, restoreDir, snapshotName);
 				SnapshotManifest manifest = SnapshotManifest.open(conf, fs, snapshotDir, snapshotDescription);
 				return getRegionLocationsFromManifest(manifest);
 			}
@@ -90,27 +80,6 @@ public class MapReduceParallelScanGrouper implements ParallelScanGrouper {
 		}
 		else {
 			return context.getConnection().getQueryServices().getAllTableRegions(tableName);
-		}
-	}
-
-	/**
-	 * Perform cleanup for snapshot restore directory
-	 * @param fs
-	 * @param restoreDir
-	 */
-	private void cleanup(FileSystem fs, Path restoreDir) {
-		try {
-			if (fs.exists(restoreDir)) {
-				if (fs.delete(restoreDir, true)) {
-					LOGGER.info("Cleanup performed on snapshot restoreDir: " + restoreDir);
-				} else {
-					LOGGER.warn("Delete restore directory for the snapshot failed. restoreDir: "
-							+ restoreDir);
-				}
-			}
-		} catch (IOException ex) {
-			LOGGER.warn("Could not delete restore directory for the snapshot. restoreDir: "
-					+ restoreDir, ex);
 		}
 	}
 
