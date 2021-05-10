c754dd4545d8d0fb9dbb8b89c363cf52943a8f0e
vmeka2020 <vmeka@salesforce.com>
2021-05-10 16:13:04 -0700

    removing curator dependencies

--- phoenix-core/src/test/java/org/apache/phoenix/monitoring/TableMetricsManagerTest.java
+++ phoenix-core/src/test/java/org/apache/phoenix/monitoring/TableMetricsManagerTest.java
@@ -18,8 +18,8 @@
 
 package org.apache.phoenix.monitoring;
 
-import org.apache.curator.shaded.com.google.common.collect.Lists;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hbase.thirdparty.com.google.common.collect.Lists;
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.query.QueryServicesOptions;
 
