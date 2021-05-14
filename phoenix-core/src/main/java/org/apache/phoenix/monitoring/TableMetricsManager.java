4d1c807b382a3d6571ed9828875112fcc4aaa460
vmeka2020 <vmeka@salesforce.com>
2021-05-14 10:04:45 -0700

    fixing testcases

--- phoenix-core/src/main/java/org/apache/phoenix/monitoring/TableMetricsManager.java
+++ phoenix-core/src/main/java/org/apache/phoenix/monitoring/TableMetricsManager.java
@@ -230,7 +230,7 @@ public class TableMetricsManager {
      * @param tableName
      * @return TableClientMetrics object
      */
-    private TableClientMetrics getTableClientMetrics(String tableName) {
+    public TableClientMetrics getTableClientMetrics(String tableName) {
 
         if (Strings.isNullOrEmpty(tableName)) {
             LOGGER.debug("Phoenix Table metrics TableName cannot be null or empty");
