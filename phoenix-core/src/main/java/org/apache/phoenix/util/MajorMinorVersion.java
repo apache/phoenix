e516b19e8a6c0f5d2eea4bc7cd6ecbe611bf300e
Viraj Jasani <vjasani@apache.org>
2021-06-30 15:02:47 +0530

    PHOENIX-6500 Allow 4.16 client to connect to 5.1 server (#1257)
    Signed-off-by: Andrew Purtell <apurtell@apache.org>

---
+++ phoenix-core/src/main/java/org/apache/phoenix/util/MajorMinorVersion.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.phoenix.util;
+
+/**
+ * A class representing major and minor Phoenix versions.
+ */
+class MajorMinorVersion {
+
+    private final int majorVersion;
+    private final int minorVersion;
+
+    public MajorMinorVersion(int majorVersion, int minorVersion) {
+        this.majorVersion = majorVersion;
+        this.minorVersion = minorVersion;
+    }
+
+    public int getMajorVersion() {
+        return majorVersion;
+    }
+
+    public int getMinorVersion() {
+        return minorVersion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MajorMinorVersion version = (MajorMinorVersion) o;
+        if (majorVersion != version.majorVersion) {
+            return false;
+        }
+        return minorVersion == version.minorVersion;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = majorVersion;
+        result = 31 * result + minorVersion;
+        return result;
+    }
+}
