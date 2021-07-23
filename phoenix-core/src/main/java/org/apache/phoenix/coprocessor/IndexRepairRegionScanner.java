14b34a81cc75ed1cc0ca388e395133caf0725ab5
tkhurana <khurana.tanuj@gmail.com>
2021-07-23 08:00:01 -0700

    PHOENIX-6476 (ADDENDUM) Index tool when verifying from index to data doesn't correctly split page into tasks (#1274)

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/IndexRepairRegionScanner.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/IndexRepairRegionScanner.java
@@ -63,6 +63,7 @@ import org.apache.phoenix.util.ServerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 /**
