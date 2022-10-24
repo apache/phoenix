ff754bc7026027ee67ee86a56552c45245dd9660
Kiran Maturi <maturi.kiran@gmail.com>
2022-10-24 15:25:38 +0530

    removed tephra reference

--- phoenix-core/src/main/java/org/apache/phoenix/schema/stats/UpdateStatisticsTool.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/stats/UpdateStatisticsTool.java
@@ -214,15 +214,6 @@ public class UpdateStatisticsTool extends Configured implements Tool {
         TableMapReduceUtil.addDependencyJars(job);
         TableMapReduceUtil.addDependencyJarsForClasses(job.getConfiguration(),
                 PhoenixConnection.class, Chronology.class, CharStream.class, Gauge.class, MetricRegistriesImpl.class);
-        try {
-            TableMapReduceUtil.addDependencyJarsForClasses(job.getConfiguration(),
-                Class.forName("org.apache.tephra.TransactionNotInProgressException"),
-                Class.forName("org.apache.tephra.TransactionSystemClient"),
-                Class.forName("org.apache.tephra.hbase.coprocessor.TransactionProcessor"),
-                Class.forName("org.apache.thrift.transport.TTransportException"));
-        } catch (Throwable t) {
-            //Tephra is excluded
-        }
         LOGGER.info("UpdateStatisticsTool running for: " + tableName
                 + " on snapshot: " + snapshotName + " with restore dir: " + restoreDir);
     }
