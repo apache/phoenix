ff80f5beb32f7ec87d39b56165bdd3ae5fa3a3e0
Aman Poonia <aman.poonia.29@gmail.com>
2023-02-13 23:32:43 +0530

    PHOENIX-6871 Fix write threads deadlock (#1559)
    * PHOENIX-6871 Fix write threads deadlock
    
    * Add  comment about Treeset importance

--- phoenix-core/src/main/java/org/apache/phoenix/hbase/index/IndexRegionObserver.java
+++ phoenix-core/src/main/java/org/apache/phoenix/hbase/index/IndexRegionObserver.java
@@ -28,6 +28,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.phoenix.thirdparty.com.google.common.collect.ArrayListMultimap;
@@ -200,7 +201,8 @@ public class IndexRegionObserver extends CompatIndexRegionObserver implements Re
       // The collection of candidate index mutations that will be applied after the data table mutations
       private ListMultimap<HTableInterfaceReference, Pair<Mutation, byte[]>> indexUpdates;
       private List<RowLock> rowLocks = Lists.newArrayListWithExpectedSize(QueryServicesOptions.DEFAULT_MUTATE_BATCH_SIZE);
-      private HashSet<ImmutableBytesPtr> rowsToLock = new HashSet<>();
+      //  TreeSet to improve locking efficiency and avoid deadlock (PHOENIX-6871 and HBASE-17924) 
+      private Set<ImmutableBytesPtr> rowsToLock = new TreeSet<>();
       // The current and next states of the data rows corresponding to the pending mutations
       private HashMap<ImmutableBytesPtr, Pair<Put, Put>> dataRowStates;
       // The previous concurrent batch contexts
@@ -467,7 +469,8 @@ public class IndexRegionObserver extends CompatIndexRegionObserver implements Re
       }
   }
 
-  private void populateRowsToLock(MiniBatchOperationInProgress<Mutation> miniBatchOp, BatchMutateContext context) {
+  private void populateRowsToLock(MiniBatchOperationInProgress<Mutation> miniBatchOp,
+          BatchMutateContext context) {
       for (int i = 0; i < miniBatchOp.size(); i++) {
           if (miniBatchOp.getOperationStatus(i) == IGNORE) {
               continue;
@@ -475,9 +478,8 @@ public class IndexRegionObserver extends CompatIndexRegionObserver implements Re
           Mutation m = miniBatchOp.getOperation(i);
           if (this.builder.isEnabled(m)) {
               ImmutableBytesPtr row = new ImmutableBytesPtr(m.getRow());
-              if (!context.rowsToLock.contains(row)) {
-                  context.rowsToLock.add(row);
-              }
+              context.rowsToLock.add(row);
+
           }
       }
   }
