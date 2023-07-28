4883080c7ba72351158db0bae8634cb65dd750f3
Jing Yu <yu.j@salesforce.com>
2023-07-28 00:41:04 -0400

    PHOENIX-6989 Expose server side metrics for DDL operations (#1638)

--- phoenix-core/src/main/java/org/apache/phoenix/schema/metrics/MetricsMetadataSourceImpl.java
+++ phoenix-core/src/main/java/org/apache/phoenix/schema/metrics/MetricsMetadataSourceImpl.java
@@ -35,6 +35,21 @@ public class MetricsMetadataSourceImpl extends BaseSourceImpl implements Metrics
     private final MutableFastCounter alterExportFailureCount;
     private final MetricHistogram alterExportFailureTimeHisto;
 
+    private final MutableFastCounter createTableCount;
+    private final MutableFastCounter createViewCount;
+    private final MutableFastCounter createIndexCount;
+    private final MutableFastCounter createSchemaCount;
+    private final MutableFastCounter createFunctionCount;
+
+    private final MutableFastCounter alterAddColumnCount;
+    private final MutableFastCounter alterDropColumnCount;
+
+    private final MutableFastCounter dropTableCount;
+    private final MutableFastCounter dropViewCount;
+    private final MutableFastCounter dropIndexCount;
+    private final MutableFastCounter dropSchemaCount;
+    private final MutableFastCounter dropFunctionCount;
+
     public MetricsMetadataSourceImpl() {
         this(METRICS_NAME, METRICS_DESCRIPTION, METRICS_CONTEXT, METRICS_JMX_CONTEXT);
     }
@@ -61,6 +76,32 @@ public class MetricsMetadataSourceImpl extends BaseSourceImpl implements Metrics
         alterExportFailureTimeHisto = getMetricsRegistry().newHistogram(ALTER_EXPORT_FAILURE_TIME,
             ALTER_EXPORT_FAILURE_TIME_DESC);
 
+        createTableCount = getMetricsRegistry().newCounter(CREATE_TABLE_COUNT,
+                CREATE_TABLE_COUNT_DESC, 0L);
+        createViewCount = getMetricsRegistry().newCounter(CREATE_VIEW_COUNT,
+                CREATE_VIEW_COUNT_DESC, 0L);
+        createIndexCount = getMetricsRegistry().newCounter(CREATE_INDEX_COUNT,
+                CREATE_INDEX_COUNT_DESC, 0L);
+        createFunctionCount = getMetricsRegistry().newCounter(CREATE_FUNCTION_COUNT,
+                CREATE_FUNCTION_COUNT_DESC, 0L);
+        createSchemaCount = getMetricsRegistry().newCounter(CREATE_SCHEMA_COUNT,
+                CREATE_SCHEMA_COUNT_DESC, 0L);
+
+        alterAddColumnCount = getMetricsRegistry().newCounter(ALTER_ADD_COLUMN_COUNT,
+                ALTER_ADD_COLUMN_COUNT_DESC, 0L);
+        alterDropColumnCount = getMetricsRegistry().newCounter(ALTER_DROP_COLUMN_COUNT,
+                ALTER_DROP_COLUMN_COUNT_DESC, 0L);
+
+        dropTableCount = getMetricsRegistry().newCounter(DROP_TABLE_COUNT,
+                DROP_TABLE_COUNT_DESC, 0L);
+        dropViewCount = getMetricsRegistry().newCounter(DROP_VIEW_COUNT,
+                DROP_VIEW_COUNT_DESC, 0L);
+        dropIndexCount = getMetricsRegistry().newCounter(DROP_INDEX_COUNT,
+                DROP_INDEX_COUNT_DESC, 0L);
+        dropSchemaCount = getMetricsRegistry().newCounter(DROP_SCHEMA_COUNT,
+                DROP_SCHEMA_COUNT_DESC, 0L);
+        dropFunctionCount = getMetricsRegistry().newCounter(DROP_FUNCTION_COUNT,
+                DROP_FUNCTION_COUNT_DESC, 0L);
     }
 
     @Override public void incrementCreateExportCount() {
@@ -93,5 +134,53 @@ public class MetricsMetadataSourceImpl extends BaseSourceImpl implements Metrics
 
     @Override public void updateAlterExportFailureTime(long t) {
         alterExportFailureTimeHisto.add(t);
+    }
+
+    @Override public void incrementCreateTableCount() {
+        createTableCount.incr();
+    }
+
+    @Override public void incrementCreateViewCount() {
+        createViewCount.incr();
+    }
+
+    @Override public void incrementCreateIndexCount() {
+        createIndexCount.incr();
+    }
+
+    @Override public void incrementCreateSchemaCount() {
+        createSchemaCount.incr();
+    }
+
+    @Override public void incrementCreateFunctionCount() {
+        createFunctionCount.incr();
+    }
+
+    @Override public void incrementAlterAddColumnCount() {
+        alterAddColumnCount.incr();
+    }
+
+    @Override public void incrementAlterDropColumnCount() {
+        alterDropColumnCount.incr();
+    }
+
+    @Override public void incrementDropTableCount() {
+        dropTableCount.incr();
+    }
+
+    @Override public void incrementDropViewCount() {
+        dropViewCount.incr();
+    }
+
+    @Override public void incrementDropIndexCount() {
+        dropIndexCount.incr();
+    }
+
+    @Override public void incrementDropSchemaCount() {
+        dropSchemaCount.incr();
+    }
+
+    @Override public void incrementDropFunctionCount() {
+        dropFunctionCount.incr();
     }
 }
