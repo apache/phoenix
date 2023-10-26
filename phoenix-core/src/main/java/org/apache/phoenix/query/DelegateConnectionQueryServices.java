233fa5c30919a8404762abe2ad8e3957f33a2c7a
Mihir Monani <mmonani@mmonani-ltmbd9x.internal.salesforce.com>
2023-10-26 16:52:05 -0700

    build fix

--- phoenix-core/src/main/java/org/apache/phoenix/query/DelegateConnectionQueryServices.java
+++ phoenix-core/src/main/java/org/apache/phoenix/query/DelegateConnectionQueryServices.java
@@ -415,6 +415,7 @@ public class DelegateConnectionQueryServices extends DelegateQueryServices imple
 
     public ConnectionLimiter getConnectionLimiter() {
         return getDelegate().getConnectionLimiter();
+    }
 
     @Override
     public int getConnectionCount(boolean isInternal) {
