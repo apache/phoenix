0d24ed821db83c0bc72553f0b7ccdf6bc5574883
Palash Chauhan <p.chauhan@pchauha-ltm8owy.internal.salesforce.com>
2024-01-12 10:38:08 -0800

    fix test

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/AppendOnlySchemaIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/AppendOnlySchemaIT.java
@@ -306,7 +306,7 @@ public class AppendOnlySchemaIT extends ParallelStatsDisabledIT {
                     "create table IF NOT EXISTS " + tableName + " ( id char(1) NOT NULL,"
                             + " col1 integer NOT NULL,"
                             + " CONSTRAINT NAME_PK PRIMARY KEY (id, col1))"
-                            + " APPEND_ONLY_SCHEMA = true");
+                            + " APPEND_ONLY_SCHEMA = true, UPDATE_CACHE_FREQUENCY=always");
                 fail("UPDATE_CACHE_FREQUENCY attribute must not be set to ALWAYS if APPEND_ONLY_SCHEMA is true");
             } catch (SQLException e) {
                 assertEquals(SQLExceptionCode.UPDATE_CACHE_FREQUENCY_INVALID.getErrorCode(),
