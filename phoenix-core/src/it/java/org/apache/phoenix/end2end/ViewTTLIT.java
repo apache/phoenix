cbbeaf66f728c50a474510ce6168395362213068
Jacob Isaac <jacobpisaac18@gmail.com>
2024-01-18 13:26:06 -0800

    Added Local Index tests and test cleanup

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/ViewTTLIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/ViewTTLIT.java
@@ -2325,8 +2325,8 @@ public class ViewTTLIT extends ParallelStatsDisabledIT {
         // TODO handle Local Index cases
         // Test cases :
         // Local vs Global indexes, various column family options.
-        for (boolean isIndex1Local : Lists.newArrayList(/*true,*/ false)) {
-            for (boolean isIndex2Local : Lists.newArrayList(/*true, */false)) {
+        for (boolean isIndex1Local : Lists.newArrayList(true, false)) {
+            for (boolean isIndex2Local : Lists.newArrayList(true, false)) {
                 for (OtherOptions options : getTableAndGlobalAndTenantColumnFamilyOptions()) {
 
                     resetEnvironmentEdgeManager();
@@ -2474,8 +2474,8 @@ public class ViewTTLIT extends ParallelStatsDisabledIT {
         // TODO handle Local Index cases
         // Test cases :
         // Local vs Global indexes, various column family options.
-        for (boolean isIndex1Local : Lists.newArrayList(/*true, */ false)) {
-            for (boolean isIndex2Local : Lists.newArrayList(/* true, */ false)) {
+        for (boolean isIndex1Local : Lists.newArrayList(true, false)) {
+            for (boolean isIndex2Local : Lists.newArrayList(true, false)) {
                 for (OtherOptions options : getTableAndGlobalAndTenantColumnFamilyOptions()) {
 
                     resetEnvironmentEdgeManager();
