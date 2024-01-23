d00e29d962a064b2c2f9cc984c0370dc31decfb4
Hari Dara <haridara@gmail.com>
2024-01-23 15:05:33 +0530

    Comment out debug props

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/CDCMiscIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/CDCMiscIT.java
@@ -286,11 +286,12 @@ public class CDCMiscIT extends ParallelStatsDisabledIT {
 
     private Connection newConnection() throws SQLException {
         Properties props = new Properties();
-        props.put(QueryServices.TASK_HANDLING_INTERVAL_MS_ATTRIB, Long.toString(Long.MAX_VALUE));
-        props.put("hbase.client.scanner.timeout.period", "6000000");
-        props.put("phoenix.query.timeoutMs", "6000000");
-        props.put("zookeeper.session.timeout", "6000000");
-        props.put("hbase.rpc.timeout", "6000000");
+        // Use these only for debugging.
+        //props.put(QueryServices.TASK_HANDLING_INTERVAL_MS_ATTRIB, Long.toString(Long.MAX_VALUE));
+        //props.put("hbase.client.scanner.timeout.period", "6000000");
+        //props.put("phoenix.query.timeoutMs", "6000000");
+        //props.put("zookeeper.session.timeout", "6000000");
+        //props.put("hbase.rpc.timeout", "6000000");
         return DriverManager.getConnection(getUrl(), props);
     }
 
