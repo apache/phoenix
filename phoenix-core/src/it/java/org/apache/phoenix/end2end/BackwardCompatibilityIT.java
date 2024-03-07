e8b84349c8e286f50b0e7d516db21270cae5f742
palash <palashc983@gmail.com>
2024-03-07 12:35:41 -0800

    Update BackwardCompatibilityIT.java

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/BackwardCompatibilityIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/BackwardCompatibilityIT.java
@@ -59,11 +59,7 @@ import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.TableDescriptor;
-<<<<<<< HEAD
 import org.apache.phoenix.cache.ServerMetadataCacheImpl;
-=======
-import org.apache.phoenix.cache.ServerMetadataCache;
->>>>>>> PHOENIX-6883-feature
 import org.apache.phoenix.coprocessor.SystemCatalogRegionObserver;
 import org.apache.phoenix.coprocessor.TaskMetaDataEndpoint;
 import org.apache.phoenix.end2end.BackwardCompatibilityTestUtil.MavenCoordinates;
