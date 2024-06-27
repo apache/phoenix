c74706dc6794dc3c6defae36da8928ca5bbde210
Jacob Isaac <jacobpisaac18@gmail.com>
2024-06-26 20:06:10 -0700

    PHOENIX-7108 Provide support for pruning expired rows of views using Phoenix level compactions (#1908)

--- phoenix-core-server/src/main/java/org/apache/phoenix/coprocessor/TTLRegionScanner.java
+++ phoenix-core-server/src/main/java/org/apache/phoenix/coprocessor/TTLRegionScanner.java
@@ -79,14 +79,13 @@ public class TTLRegionScanner extends BaseRegionScanner {
         } else {
             ttl = env.getRegion().getTableDescriptor().getColumnFamilies()[0].getTimeToLive();
         }
-        ttlWindowStart = ttl == HConstants.FOREVER ? 1 : currentTime - ttl * 1000;
-        ttl *= 1000;
         // Regardless if the Phoenix Table TTL feature is disabled cluster wide or the client is
         // an older client and does not supply the empty column parameters, the masking should not
         // be done here. We also disable masking when TTL is HConstants.FOREVER.
         isMaskingEnabled = emptyCF != null && emptyCQ != null && ttl != HConstants.FOREVER
-                && env.getConfiguration().getBoolean(QueryServices.PHOENIX_TABLE_TTL_ENABLED,
-                        QueryServicesOptions.DEFAULT_PHOENIX_TABLE_TTL_ENABLED);
+                && (isPhoenixTableTTLEnabled(env.getConfiguration()) && (isSystemTable == null
+                || !Bytes.toBoolean(isSystemTable)));
+
         ttlWindowStart = ttl == HConstants.FOREVER ? 1 : currentTime - ttl * 1000;
         ttl *= 1000;
     }
