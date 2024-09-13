68b447a719eba9c9e31db56158dfff2f06b34e23
Palash Chauhan <p.chauhan@pchauha-ltm8owy.internal.salesforce.com>
2024-09-13 16:05:48 -0700

    add more cases in test

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/index/PartialIndexIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/index/PartialIndexIT.java
@@ -28,9 +28,7 @@ import org.apache.phoenix.mapreduce.index.IndexTool;
 import org.apache.phoenix.query.BaseTest;
 import org.apache.phoenix.query.QueryServices;
 import org.apache.phoenix.schema.ColumnNotFoundException;
-import org.apache.phoenix.schema.PColumn;
 import org.apache.phoenix.schema.PTable;
-import org.apache.phoenix.schema.PTableKey;
 import org.apache.phoenix.thirdparty.com.google.common.collect.Maps;
 import org.apache.phoenix.util.EnvironmentEdgeManager;
 import org.apache.phoenix.util.PhoenixRuntime;
@@ -56,7 +54,6 @@ import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -1062,27 +1059,28 @@ public class PartialIndexIT extends BaseTest {
         try(Connection conn = DriverManager.getConnection(getUrl());
             Statement stmt = conn.createStatement()) {
             String dataTableName = generateUniqueName();
-            String indexName = generateUniqueName();
+            String indexName1 = generateUniqueName();
+            String indexName2 = generateUniqueName();
+            String indexName3 = generateUniqueName();
 
             stmt.execute("CREATE TABLE " + dataTableName
-                    + " (\"hashKeY\" VARCHAR NOT NULL PRIMARY KEY, v1 VARCHAR, \"CoL\" VARCHAR, v3 VARCHAR)");
+                    + " (\"hashKeY\" VARCHAR NOT NULL PRIMARY KEY, v1 VARCHAR, \"CoL\" VARCHAR, \"coLUmn3\" VARCHAR)");
 
-            stmt.execute("CREATE INDEX " + indexName
-                    + " ON " + dataTableName + "(v1) INCLUDE (\"CoL\") WHERE v3 IS NOT NULL");
+            stmt.execute("CREATE INDEX " + indexName1
+                    + " ON " + dataTableName + "(v1) INCLUDE (\"CoL\") WHERE \"coLUmn3\" IS NOT NULL");
+            stmt.execute("CREATE INDEX " + indexName2
+                    + " ON " + dataTableName + "(\"CoL\") INCLUDE (v1) WHERE \"coLUmn3\" IS NOT NULL");
+            stmt.execute("CREATE INDEX " + indexName3
+                    + " ON " + dataTableName + "(\"coLUmn3\") INCLUDE (\"CoL\") WHERE v1 IS NOT NULL");
 
             stmt.execute("UPSERT INTO " + dataTableName + " VALUES ('a', 'b', 'c', 'd')");
             conn.commit();
 
-            List<PColumn> cols = conn.unwrap(PhoenixConnection.class).getTable(new PTableKey(null, dataTableName)).getColumns();
-            for (PColumn col: cols) {
-                System.out.println(col.getName().getString());
-            }
-            cols = conn.unwrap(PhoenixConnection.class).getTable(new PTableKey(null, indexName)).getColumns();
-            for (PColumn col: cols) {
-                System.out.println(col.getName().getString());
-            }
-
-            ResultSet rs = stmt.executeQuery("SELECT \"CoL\" FROM " + dataTableName + " WHERE v1='b'");
+            ResultSet rs = stmt.executeQuery("SELECT \"CoL\", \"coLUmn3\" FROM " + dataTableName + " WHERE v1='b'");
+            Assert.assertTrue(rs.next());
+            rs = stmt.executeQuery("SELECT v1, \"coLUmn3\" FROM " + dataTableName + " WHERE \"CoL\"='c'");
+            Assert.assertTrue(rs.next());
+            rs = stmt.executeQuery("SELECT \"CoL\", v1 FROM " + dataTableName + " WHERE \"coLUmn3\"='d'");
             Assert.assertTrue(rs.next());
         }
     }
