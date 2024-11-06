48364cf14b9bac28df91673dc8eb6f8445728322
Nihal Jain <nihaljain@apache.org>
2024-11-06 19:46:07 +0530

    PHOENIX-7441 Integrate the Spotless plugin and update the code template  - Update dev/PhoenixCodeTemplate.xml to use latest format as in hbase.  - Copied license-header file from hbase, the one which phoenix was using was a little different  - Fix file having misplaced package block as we do not want any manual code change in the commit where we run spotless

--- phoenix-core/src/main/java/org/apache/phoenix/coprocessor/DelegateRegionScanner.java
+++ phoenix-core/src/main/java/org/apache/phoenix/coprocessor/DelegateRegionScanner.java
@@ -14,7 +14,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.apache.phoenix.coprocessor;
+ */
+package org.apache.phoenix.coprocessor;
 
 import java.io.IOException;
 import java.util.List;
