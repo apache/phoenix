433b960726a3106fdf03b039fab4ffbe428ced62
Palash Chauhan <p.chauhan@pchauha-ltm8owy.internal.salesforce.com>
2025-02-03 15:26:09 -0800

    add stream type column

--- phoenix-core-client/src/main/java/org/apache/phoenix/query/QueryConstants.java
+++ phoenix-core-client/src/main/java/org/apache/phoenix/query/QueryConstants.java
@@ -142,6 +142,7 @@ import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STORE_NULLS;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STREAMING_TOPIC_NAME;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STREAM_NAME;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STREAM_STATUS;
+import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.STREAM_TYPE;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CATALOG_SCHEMA;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CATALOG_TABLE;
 import static org.apache.phoenix.jdbc.PhoenixDatabaseMetaData.SYSTEM_CDC_STREAM_STATUS_TABLE;
@@ -658,6 +659,7 @@ public interface QueryConstants {
             STREAM_NAME + " VARCHAR NOT NULL," +
             // Non-PK columns
             STREAM_STATUS + " VARCHAR,\n" +
+            STREAM_TYPE + " VARCHAR,\n" +
             "CONSTRAINT " + SYSTEM_TABLE_PK_NAME + " PRIMARY KEY (" +
             TABLE_NAME + "," + STREAM_NAME + "))\n" +
             HConstants.VERSIONS + "=%s,\n" +
