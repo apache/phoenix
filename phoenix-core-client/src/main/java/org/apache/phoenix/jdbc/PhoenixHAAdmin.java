83f794056e880a648bba0312e310a17eb2ab77f9
Ritesh Garg <ritesh.garg@riteshg-ltmd34g.internal.salesforce.com>
2025-03-17 19:28:43 -0700

    PHOENIX-7493. Graceful Failover with Phoenix HA

--- phoenix-core-client/src/main/java/org/apache/phoenix/jdbc/PhoenixHAAdmin.java
+++ phoenix-core-client/src/main/java/org/apache/phoenix/jdbc/PhoenixHAAdmin.java
@@ -36,7 +36,12 @@ import org.slf4j.LoggerFactory;
 import java.io.Closeable;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Properties;
 
 /**
  * Helper class to update cluster role record for a ZK cluster.
