d3e36f35966a01883bee004575b0c1b0134f025b
ritegarg <58840065+ritegarg@users.noreply.github.com>
2025-03-22 09:10:37 -0700

    Update HAGroupStoreClientIT.java

--- phoenix-core/src/it/java/org/apache/phoenix/jdbc/HAGroupStoreClientIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/jdbc/HAGroupStoreClientIT.java
@@ -364,7 +364,7 @@ public class HAGroupStoreClientIT extends BaseTest {
     @Test
     public void testHAGroupStoreClientWithMultiThreadedUpdates() throws Exception {
         // Number of threads to execute
-        int threadCount = 50;
+        int threadCount = 10;
 
         // Capture versions of crr in a list(crrEventVersions)  in order they are received.
         List<Integer> crrEventVersions = new ArrayList<>();
