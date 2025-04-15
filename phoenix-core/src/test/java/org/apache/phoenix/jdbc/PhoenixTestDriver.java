62e2598d1467acb47905cc0d2e1dcb2f3214e12b
Jacob Isaac <jacobpisaac18@gmail.com>
2025-04-14 18:28:14 -0700

    Refactored synchronization in PhoenixTestDriver

--- phoenix-core/src/test/java/org/apache/phoenix/jdbc/PhoenixTestDriver.java
+++ phoenix-core/src/test/java/org/apache/phoenix/jdbc/PhoenixTestDriver.java
@@ -23,11 +23,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 
 import org.apache.phoenix.end2end.ConnectionQueryServicesTestImpl;
+import org.apache.phoenix.exception.SQLExceptionCode;
+import org.apache.phoenix.exception.SQLExceptionInfo;
 import org.apache.phoenix.query.ConnectionQueryServices;
 import org.apache.phoenix.query.ConnectionlessQueryServicesImpl;
 import org.apache.phoenix.query.QueryServices;
@@ -35,7 +39,7 @@ import org.apache.phoenix.query.QueryServicesTestImpl;
 import org.apache.phoenix.util.PropertiesUtil;
 import org.apache.phoenix.util.ReadOnlyProps;
 
-
+import static org.apache.phoenix.thirdparty.com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * 
@@ -50,15 +54,14 @@ public class PhoenixTestDriver extends PhoenixEmbeddedDriver {
 
     private final ReadOnlyProps overrideProps;
     
-    @GuardedBy("this")
     private final QueryServices queryServices;
     
-    @GuardedBy("this")
-    private boolean closed = false;
-
-    @GuardedBy("this")
     private final Map<ConnectionInfo, ConnectionQueryServices>
             connectionQueryServicesMap = new HashMap<>();
+
+    @GuardedBy("closeLock")
+    private volatile boolean closed = false;
+    private final ReadWriteLock closeLock = new ReentrantReadWriteLock();
 
     public PhoenixTestDriver() {
         this(ReadOnlyProps.EMPTY_PROPS);
@@ -71,9 +74,14 @@ public class PhoenixTestDriver extends PhoenixEmbeddedDriver {
     }
 
     @Override
-    public synchronized QueryServices getQueryServices() {
-        checkClosed();
-        return queryServices;
+    public QueryServices getQueryServices() throws SQLException {
+        lockInterruptibly(PhoenixTestDriver.LockMode.READ);
+        try {
+            checkClosed();
+            return queryServices;
+        } finally {
+            unlock(PhoenixTestDriver.LockMode.READ);
+        }
     }
 
     @Override
@@ -83,43 +91,60 @@ public class PhoenixTestDriver extends PhoenixEmbeddedDriver {
     }
     
     @Override
-    public  Connection connect(String url, Properties info) throws SQLException {
-        checkClosed();
-        return super.connect(url, info);
+    public Connection connect(String url, Properties info) throws SQLException {
+        lockInterruptibly(PhoenixTestDriver.LockMode.READ);
+        try {
+            checkClosed();
+            return super.connect(url, info);
+        } finally {
+            unlock(PhoenixTestDriver.LockMode.READ);
+        }
     }
     
     @Override // public for testing
-    public  ConnectionQueryServices getConnectionQueryServices(String url, Properties infoIn) throws SQLException {
-        checkClosed();
-        final Properties info = PropertiesUtil.deepCopy(infoIn);
-        ConnectionInfo connInfo = ConnectionInfo.create(url, null, info);
-        ConnectionQueryServices connectionQueryServices = connectionQueryServicesMap.get(connInfo);
-        if (connectionQueryServices != null) {
+    public ConnectionQueryServices getConnectionQueryServices(String url, Properties infoIn) throws SQLException {
+        lockInterruptibly(PhoenixTestDriver.LockMode.READ);
+        try {
+            checkClosed();
+            final Properties info = PropertiesUtil.deepCopy(infoIn);
+            ConnectionInfo connInfo = ConnectionInfo.create(url, null, info);
+            ConnectionQueryServices connectionQueryServices = connectionQueryServicesMap.get(connInfo);
+            if (connectionQueryServices != null) {
+                return connectionQueryServices;
+            }
+            info.putAll(connInfo.asProps().asMap());
+            if (connInfo.isConnectionless()) {
+                connectionQueryServices = new ConnectionlessQueryServicesImpl(queryServices, connInfo, info);
+            } else {
+                connectionQueryServices = new ConnectionQueryServicesTestImpl(queryServices, connInfo, info);
+            }
+            connectionQueryServices.init(url, info);
+            connectionQueryServicesMap.put(connInfo, connectionQueryServices);
             return connectionQueryServices;
+        } finally {
+            unlock(PhoenixTestDriver.LockMode.READ);
         }
-        info.putAll(connInfo.asProps().asMap());
-        if (connInfo.isConnectionless()) {
-            connectionQueryServices = new ConnectionlessQueryServicesImpl(queryServices, connInfo, info);
-        } else {
-            connectionQueryServices = new ConnectionQueryServicesTestImpl(queryServices, connInfo, info);
-        }
-        connectionQueryServices.init(url, info);
-        connectionQueryServicesMap.put(connInfo, connectionQueryServices);
-        return connectionQueryServices;
     }
-    
-    private synchronized void checkClosed() {
+
+    @GuardedBy("closeLock")
+    private void checkClosed() {
         if (closed) {
             throw new IllegalStateException("The Phoenix jdbc test driver has been closed.");
         }
     }
     
     @Override
-    public synchronized void close() throws SQLException {
-        if (closed) {
-            return;
+    public void close() throws SQLException {
+        lockInterruptibly(PhoenixTestDriver.LockMode.WRITE);
+
+        try {
+            if (closed) {
+                return;
+            }
+            closed = true;
+        } finally {
+            unlock(PhoenixTestDriver.LockMode.WRITE);
         }
-        closed = true;
         try {
             for (ConnectionQueryServices cqs : connectionQueryServicesMap.values()) {
                 cqs.close();
@@ -132,6 +157,43 @@ public class PhoenixTestDriver extends PhoenixEmbeddedDriver {
                 if (executor != null) executor.shutdownNow();
                 connectionQueryServicesMap.clear();;
             }
+        }
+    }
+    private enum LockMode {
+        READ, WRITE
+    };
+
+    private void lockInterruptibly(PhoenixTestDriver.LockMode mode) throws SQLException {
+        checkNotNull(mode);
+        switch (mode) {
+        case READ:
+            try {
+                closeLock.readLock().lockInterruptibly();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new SQLExceptionInfo.Builder(SQLExceptionCode.INTERRUPTED_EXCEPTION)
+                        .setRootCause(e).build().buildException();
+            }
+            break;
+        case WRITE:
+            try {
+                closeLock.writeLock().lockInterruptibly();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new SQLExceptionInfo.Builder(SQLExceptionCode.INTERRUPTED_EXCEPTION)
+                        .setRootCause(e).build().buildException();
+            }
+        }
+    }
+
+    private void unlock(PhoenixTestDriver.LockMode mode) {
+        checkNotNull(mode);
+        switch (mode) {
+        case READ:
+            closeLock.readLock().unlock();
+            break;
+        case WRITE:
+            closeLock.writeLock().unlock();
         }
     }
 }
