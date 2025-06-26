1e8d8b7c78991c830cb1e8f18116b4bc37a57304
ritegarg <58840065+ritegarg@users.noreply.github.com>
2025-06-26 10:21:41 -0700

    Update IndexRegionObserver.java

--- phoenix-core-server/src/main/java/org/apache/phoenix/hbase/index/IndexRegionObserver.java
+++ phoenix-core-server/src/main/java/org/apache/phoenix/hbase/index/IndexRegionObserver.java
@@ -545,9 +545,9 @@ public class IndexRegionObserver implements RegionCoprocessor, RegionObserver {
           final Configuration conf = c.getEnvironment().getConfiguration();
           final HAGroupStoreManager haGroupStoreManager = HAGroupStoreManager.getInstance(conf);
           if (haGroupStoreManager.isMutationBlocked()) {
-               throw new MutationBlockedIOException("Blocking Mutation as some CRRs "
-                    + "are in ACTIVE_TO_STANDBY state and "
-                    + "CLUSTER_ROLE_BASED_MUTATION_BLOCK_ENABLED is true");
+              throw new MutationBlockedIOException("Blocking Mutation as some CRRs "
+                      + "are in ACTIVE_TO_STANDBY state and "
+                      + "CLUSTER_ROLE_BASED_MUTATION_BLOCK_ENABLED is true");
           }
           preBatchMutateWithExceptions(c, miniBatchOp);
           return;
