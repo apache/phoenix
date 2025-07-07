051a5b56569586379543fbc9f2372e8cb624cb3b
Rahul Kumar <rahul.kumar@rahulku-ltmyptp.internal.salesforce.com>
2025-07-07 21:42:10 +0530

    @PHOENIX-7654: Restrict BSON datatype in composite PK as last part of the pk column

--- phoenix-core-client/src/main/java/org/apache/phoenix/exception/SQLExceptionCode.java
+++ phoenix-core-client/src/main/java/org/apache/phoenix/exception/SQLExceptionCode.java
@@ -238,7 +238,7 @@ public enum SQLExceptionCode {
     VARBINARY_IN_ROW_KEY(1005, "42J03",
         "The VARBINARY/ARRAY type can only be used as the last part of a multi-part row key. "
             + "For Binary types, you can use VARBINARY_ENCODED for early part of multi-part row key."),
-    BSON_IN_ROW_KEY(11157, "42J03",
+    BSON_IN_ROW_KEY(1157, "42J03",
         "The BSON type can only be used as the last part of a multi-part row key."),
     NOT_NULLABLE_COLUMN_IN_ROW_KEY(1006, "42J04", "Only nullable columns may be added to primary key."),
     VARBINARY_LAST_PK(1015, "42J04", "Cannot add column to table when the last PK column is of type VARBINARY or ARRAY."),
