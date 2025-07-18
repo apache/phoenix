343677b190f6c672e3dc56547df580df11564fb7
Rahul Kumar <rahul.kumar@rahulku-ltmyptp.internal.salesforce.com>
2025-07-18 10:29:29 +0530

    fix checks

--- phoenix-core-client/src/main/java/org/apache/phoenix/expression/function/BsonValueTypeFunction.java
+++ phoenix-core-client/src/main/java/org/apache/phoenix/expression/function/BsonValueTypeFunction.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,8 +25,8 @@ import org.apache.phoenix.expression.util.bson.CommonComparisonExpressionUtils;
 import org.apache.phoenix.parse.BsonValueTypeParseNode;
 import org.apache.phoenix.parse.FunctionParseNode;
 import org.apache.phoenix.schema.tuple.Tuple;
-import org.apache.phoenix.schema.types.PBson;
 import org.apache.phoenix.schema.types.PBoolean;
+import org.apache.phoenix.schema.types.PBson;
 import org.apache.phoenix.schema.types.PDataType;
 import org.apache.phoenix.schema.types.PDate;
 import org.apache.phoenix.schema.types.PDouble;
@@ -51,18 +51,24 @@ import org.bson.BsonValue;
 import org.bson.RawBsonDocument;
 
 /**
- * BSON_VALUE_TYPE function to retrieve the SQL data type name of any field in BSON. This can be used for any
- * top-level or nested Bson fields.
+ * BSON_VALUE_TYPE function to retrieve the SQL data type name of any field in BSON.
+ * This can be used for any top-level or nested Bson fields.
  * 1. The first argument represents BSON Object on which the function performs scan.
  * 2. The second argument represents the field key. The field key can represent any top level or
  * nested fields within the document. The caller should use "." notation for accessing nested
  * document elements and "[n]" notation for accessing nested array elements.
  * Top level fields do not require any additional character.
  */
-@FunctionParseNode.BuiltInFunction(name = BsonValueTypeFunction.NAME,
-    nodeClass = BsonValueTypeParseNode.class, args = {
-    @FunctionParseNode.Argument(allowedTypes = { PJson.class, PBson.class, PVarbinary.class }),
-    @FunctionParseNode.Argument(allowedTypes = { PVarchar.class }, isConstant = true), })
+@FunctionParseNode.BuiltInFunction(
+        name = BsonValueTypeFunction.NAME,
+        nodeClass = BsonValueTypeParseNode.class,
+        args = {
+                @FunctionParseNode.Argument(
+                    allowedTypes = { PJson.class, PBson.class, PVarbinary.class }),
+                @FunctionParseNode.Argument(
+                    allowedTypes = { PVarchar.class }, isConstant = true),
+        }
+)
 public class BsonValueTypeFunction extends ScalarFunction {
 
     public static final String NAME = "BSON_VALUE_TYPE";
