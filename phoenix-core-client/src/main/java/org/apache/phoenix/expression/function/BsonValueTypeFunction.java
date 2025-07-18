32a83a2117629794520af360b7762c3ef6d245dd
Rahul Kumar <rahul.kumar@rahulku-ltmyptp.internal.salesforce.com>
2025-07-18 16:16:08 +0530

    fix license

--- phoenix-core-client/src/main/java/org/apache/phoenix/expression/function/BsonValueTypeFunction.java
+++ phoenix-core-client/src/main/java/org/apache/phoenix/expression/function/BsonValueTypeFunction.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -59,16 +59,15 @@ import org.bson.RawBsonDocument;
  * document elements and "[n]" notation for accessing nested array elements.
  * Top level fields do not require any additional character.
  */
-@FunctionParseNode.BuiltInFunction(
-        name = BsonValueTypeFunction.NAME,
-        nodeClass = BsonValueTypeParseNode.class,
-        args = {
-                @FunctionParseNode.Argument(
+@FunctionParseNode.BuiltInFunction( name = BsonValueTypeFunction.NAME,
+    nodeClass = BsonValueTypeParseNode.class,
+    args =
+        {
+            @FunctionParseNode.Argument(
                     allowedTypes = { PJson.class, PBson.class, PVarbinary.class }),
-                @FunctionParseNode.Argument(
+            @FunctionParseNode.Argument(
                     allowedTypes = { PVarchar.class }, isConstant = true),
-        }
-)
+        })
 public class BsonValueTypeFunction extends ScalarFunction {
 
     public static final String NAME = "BSON_VALUE_TYPE";
