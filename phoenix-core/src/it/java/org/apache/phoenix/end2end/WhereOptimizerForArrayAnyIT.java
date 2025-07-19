e1cabb1010cb6bb450d1f761de14ccb9cef82235
Sanjeet Malhotra <sanjeetmalhotra@sanjeet-ltmkky6.internal.salesforce.com>
2025-07-19 22:45:44 +0530

    Adding test coverage for different data types

--- phoenix-core/src/it/java/org/apache/phoenix/end2end/WhereOptimizerForArrayAnyIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/end2end/WhereOptimizerForArrayAnyIT.java
@@ -25,17 +25,23 @@ import static org.junit.Assert.assertTrue;
 import java.math.BigDecimal;
 import java.sql.Array;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.HashMap;
 
+import org.apache.hadoop.hbase.TableName;
 import org.apache.phoenix.query.BaseTest;
+import org.apache.phoenix.util.DateUtil;
 import org.apache.phoenix.util.QueryUtil;
 import org.apache.phoenix.util.ReadOnlyProps;
+import org.apache.phoenix.util.TestUtil;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -133,18 +139,17 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     @Test
     public void testArrayAnyComparisonWithDoubleToFloatConversion() throws Exception {
         String tableName = generateUniqueName();
-        String ddl =
-            "CREATE TABLE " + tableName + " (pk1 FLOAT NOT NULL, " + "pk2 VARCHAR(3) NOT NULL, "
-                + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1, pk2))";
+        String ddl ="CREATE TABLE " + tableName + " (" 
+            + "pk1 FLOAT NOT NULL, " 
+            + "pk2 VARCHAR(3) NOT NULL, "
+            + "col1 VARCHAR, " 
+            + "CONSTRAINT pk PRIMARY KEY (pk1, pk2)"
+        + ")";
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             try (Statement stmt = conn.createStatement()) {
                 stmt.execute(ddl);
                 conn.commit();
-                stmt.execute("UPSERT INTO " + tableName + " VALUES (1.1, 'x', 'a')");
-                conn.commit();
                 stmt.execute("UPSERT INTO " + tableName + " VALUES (2.2, 'y', 'b')");
-                conn.commit();
-                stmt.execute("UPSERT INTO " + tableName + " VALUES (3.3, 'z', 'c')");
                 conn.commit();
             }
         }
@@ -168,9 +173,7 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     public void testArrayAnyComparisonWithLongToIntegerConversion() throws Exception {
         String tableName = generateUniqueName();
         createTableASCPkColumns(tableName);
-        insertData(tableName, 1, "x", "a");
         insertData(tableName, 2, "y", "b");
-        insertData(tableName, 3, "z", "c");
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             String selectSql = "SELECT * FROM " + tableName + " WHERE pk1 = ANY(?) AND pk2 = 'y'";
             Array arr = conn.createArrayOf("BIGINT", new Long[] { 4L, 2L, 0L });
@@ -193,7 +196,6 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
         createTableASCPkColumns(tableName);
         insertData(tableName, 1, null, "a");
         insertData(tableName, 2, "y", "b");
-        insertData(tableName, 3, "z", "c");
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             String selectSql = "SELECT * FROM " + tableName + " WHERE pk1 = 1 AND pk2 = ANY(?)";
             Array arr = conn.createArrayOf("VARCHAR", new String[] { "y", "z", null });
@@ -222,8 +224,12 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     @Test
     public void testArrayAnyComparisonWithDescPKAndNullInArray() throws Exception {
         String tableName = generateUniqueName();
-        String ddl = "CREATE TABLE " + tableName + " (pk1 INTEGER NOT NULL, " + "pk2 VARCHAR(3), "
-            + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1, pk2 DESC))";
+        String ddl = "CREATE TABLE " + tableName + " (" 
+            + "pk1 INTEGER NOT NULL, " 
+            + "pk2 VARCHAR(3), " 
+            + "col1 VARCHAR, " 
+            + "CONSTRAINT pk PRIMARY KEY (pk1, pk2 DESC)"
+        + ")";
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             try (Statement stmt = conn.createStatement()) {
                 stmt.execute(ddl);
@@ -261,8 +267,12 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     @Test
     public void testArrayAnyComparisonForDescCharPKWithPadding() throws Exception {
         String tableName = generateUniqueName();
-        String ddl = "CREATE TABLE " + tableName + " (pk1 CHAR(3) NOT NULL, " + "pk2 VARCHAR(3), "
-            + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1 DESC, pk2))";
+        String ddl = "CREATE TABLE " + tableName + " (" 
+            + "pk1 CHAR(3) NOT NULL, "
+            + "pk2 VARCHAR(3), "
+            + "col1 VARCHAR, "
+            + "CONSTRAINT pk PRIMARY KEY (pk1 DESC, pk2)"
+        + ")";
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             try (Statement stmt = conn.createStatement()) {
                 stmt.execute(ddl);
@@ -298,8 +308,12 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     @Test
     public void testArrayAnyComparisonWithDecimalArray() throws Exception {
         String tableName = generateUniqueName();
-        String ddl = "CREATE TABLE " + tableName + " (pk1 DECIMAL(10, 2) NOT NULL, "
-            + "pk2 VARCHAR(3), " + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1, pk2))";
+        String ddl = "CREATE TABLE " + tableName + " (" 
+            + "pk1 DECIMAL(10, 2) NOT NULL, "
+            + "pk2 VARCHAR(3), "
+            + "col1 VARCHAR, "
+            + "CONSTRAINT pk PRIMARY KEY (pk1, pk2)"
+        + ")";
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             try (Statement stmt = conn.createStatement()) {
                 stmt.execute(ddl);
@@ -318,8 +332,10 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
         }
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             String selectSql = "SELECT * FROM " + tableName + " WHERE pk1 = ANY(?) AND pk2 = 'x'";
-            Array arr = conn.createArrayOf("DECIMAL", new BigDecimal[] { new BigDecimal("1.230"),
-                new BigDecimal("2.340"), new BigDecimal("3.450") });
+            Array arr = conn.createArrayOf("DECIMAL", new BigDecimal[] {
+                new BigDecimal("1.230"),
+                new BigDecimal("2.340"), 
+                new BigDecimal("3.450") });
             try (PreparedStatement stmt = conn.prepareStatement(selectSql)) {
                 stmt.setArray(1, arr);
                 try (ResultSet rs = stmt.executeQuery()) {
@@ -337,8 +353,12 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
     public void testArrayAnyComparisonWithDataTypeAndSortOrderCoercionForDecimalColumn()
         throws Exception {
         String tableName = generateUniqueName();
-        String ddl = "CREATE TABLE " + tableName + " (pk1 DECIMAL(10, 2) NOT NULL, "
-            + "pk2 VARCHAR(3), " + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1 DESC, pk2))";
+        String ddl = "CREATE TABLE " + tableName + " ("
+            + "pk1 DECIMAL(10, 2) NOT NULL, "
+            + "pk2 VARCHAR(3), "
+            + "col1 VARCHAR, "
+            + "CONSTRAINT pk PRIMARY KEY (pk1 DESC, pk2)"
+        + ")";
         try (Connection conn = DriverManager.getConnection(getUrl())) {
             try (Statement stmt = conn.createStatement()) {
                 stmt.execute(ddl);
@@ -368,6 +388,200 @@ public class WhereOptimizerForArrayAnyIT extends BaseTest {
                 }
             }
             assertPointLookupsAreGenerated(selectSql, conn, arr);
+        }
+    }
+
+    @Test
+    public void testArrayAnyComparisonForDateTimeColumn() throws Exception {
+        String tableName = generateUniqueName();
+        String ddl = "CREATE TABLE " + tableName + " ("
+            + "pk1 TIMESTAMP NOT NULL, "
+            + "pk2 TIME NOT NULL, "
+            + "pk3 DATE NOT NULL, "
+            + "col1 VARCHAR, "
+            + "CONSTRAINT pk PRIMARY KEY (pk1, pk2, pk3)" 
+        + ")";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(ddl);
+                conn.commit();
+            }
+        }
+        String pk1Value = "2025-07-18 10:00:00";
+        String pk2Value = "2025-07-18 11:00:00";
+        String pk3Value = "2025-07-18 12:00:00";
+        String upsertStmt = "UPSERT INTO " + tableName + " VALUES (?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (PreparedStatement stmt = conn.prepareStatement(upsertStmt)) {
+                stmt.setTimestamp(1, DateUtil.parseTimestamp(pk1Value));
+                stmt.setTime(2, DateUtil.parseTime(pk2Value));
+                stmt.setDate(3, DateUtil.parseDate(pk3Value));
+                stmt.setString(4, "a");
+                stmt.executeUpdate();
+                conn.commit();
+            }
+        }
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            
+            // Use arrays as bind variables to test the point lookup optimization.
+            String selectSql = "SELECT * FROM " + tableName
+                + " WHERE pk1 = ANY(?) " 
+                + "AND pk2 = ANY(?) " 
+                + "AND pk3 = ANY(?)";
+            Array timestampArr = conn.createArrayOf("TIMESTAMP", new Timestamp[] {
+                DateUtil.parseTimestamp(pk1Value),
+                DateUtil.parseTimestamp("2025-07-19 10:00:00"),
+                DateUtil.parseTimestamp("2025-07-17 10:00:00"),
+            });
+            Array timeArr = conn.createArrayOf("TIME", new Time[] {
+                DateUtil.parseTime(pk2Value),
+                DateUtil.parseTime("2025-07-19 11:00:00"),
+                DateUtil.parseTime("2025-07-17 11:00:00"),
+            });
+            Array dateArr = conn.createArrayOf("DATE", new Date[] {
+                DateUtil.parseDate(pk3Value),
+                DateUtil.parseDate("2025-07-19 12:00:00"),
+                DateUtil.parseDate("2025-07-17 12:00:00"),
+            });
+            try (PreparedStatement stmt = conn.prepareStatement(selectSql)) {
+                stmt.setArray(1, timestampArr);
+                stmt.setArray(2, timeArr);
+                stmt.setArray(3, dateArr);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    assertTrue(rs.next());
+                    assertEquals(DateUtil.parseTimestamp(pk1Value), rs.getTimestamp(1));
+                    assertEquals(DateUtil.parseTime(pk2Value), rs.getTime(2));
+                    assertEquals(DateUtil.parseDate(pk3Value), rs.getDate(3));
+                }
+            }
+            try (PreparedStatement stmt = conn.prepareStatement("EXPLAIN " + selectSql)) {
+                stmt.setArray(1, timestampArr);
+                stmt.setArray(2, timeArr);
+                stmt.setArray(3, dateArr);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    String explainPlan = QueryUtil.getExplainPlan(rs);
+                    assertTrue(explainPlan.contains("POINT LOOKUP ON"));
+                }
+            }
+
+            // Use literal arrays to test the point lookup optimization.
+            String timestampLiteralArr = "ARRAY["
+                + "TO_TIMESTAMP('" + pk1Value + "'), "
+                + "TO_TIMESTAMP('" + pk2Value + "'), "
+                + "TO_TIMESTAMP('" + pk3Value + "')]";
+            String timeLiteralArr = "ARRAY["
+                + "TO_TIME('" + pk2Value + "'), "
+                + "TO_TIME('" + pk2Value + "'), "
+                + "TO_TIME('" + pk2Value + "')]";
+            String dateLiteralArr = "ARRAY["
+                + "TO_DATE('" + pk3Value + "'), "
+                + "TO_DATE('" + pk3Value + "'), "
+                + "TO_DATE('" + pk3Value + "')]";
+            selectSql = "SELECT * FROM " + tableName
+                + " WHERE pk1 = ANY(" + timestampLiteralArr + ") " 
+                + "AND pk2 = ANY(" + timeLiteralArr + ")" 
+                + "AND pk3 = ANY(" + dateLiteralArr + ")";
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery(selectSql)) {
+                    assertTrue(rs.next());
+                    assertEquals(DateUtil.parseTimestamp(pk1Value), rs.getTimestamp(1));
+                    assertEquals(DateUtil.parseTime(pk2Value), rs.getTime(2));
+                    assertEquals(DateUtil.parseDate(pk3Value), rs.getDate(3));
+                }
+            }
+            assertPointLookupsAreGenerated(selectSql, conn, null);
+        }
+    }
+    
+    @Test
+    public void testArrayAnyComparisonWithTimestampToDatTimeCoercion() throws Exception {
+        String tableName = generateUniqueName();
+        String ddl =
+            "CREATE TABLE " + tableName + " ("
+                + "pk1 TIMESTAMP NOT NULL, "
+                + "pk2 DATE NOT NULL, "
+                + "col1 VARCHAR, " 
+                + "CONSTRAINT pk PRIMARY KEY (pk1, pk2 DESC)" 
+            + ")";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(ddl);
+                conn.commit();
+            }
+        }
+        String pk1Value = "2025-07-18 10:00:00";
+        String pk2Value = "2025-07-18 11:00:00";
+        String pk3Value = "2025-07-18 12:00:00";
+        String upsertStmt = "UPSERT INTO " + tableName + " VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (PreparedStatement stmt = conn.prepareStatement(upsertStmt)) {
+                stmt.setTimestamp(1, DateUtil.parseTimestamp(pk1Value));
+                stmt.setDate(2, DateUtil.parseDate(pk2Value));
+                stmt.setString(3, "a");
+                stmt.executeUpdate();
+                conn.commit();
+            }
+        }
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            TestUtil.dumpTable(conn, TableName.valueOf(tableName));
+            // Use literal arrays to test the point lookup optimization.
+            String timestampLiteralArr = "ARRAY["
+                + "TO_TIMESTAMP('" + pk1Value + "'), "
+                + "TO_TIMESTAMP('" + pk2Value + "'), "
+                + "TO_TIMESTAMP('" + pk3Value + "')]";
+            String selectSql = "SELECT * FROM " + tableName
+                + " WHERE pk1 = ANY(" + timestampLiteralArr + ") "
+                + "AND pk2 = ANY(" + timestampLiteralArr + ")";
+            try (Statement stmt = conn.createStatement()) {
+                try (ResultSet rs = stmt.executeQuery(selectSql)) {
+                    assertTrue(rs.next());
+                    assertEquals(DateUtil.parseTimestamp(pk1Value), rs.getTimestamp(1));
+                    assertEquals(DateUtil.parseDate(pk2Value), rs.getDate(2));
+                    assertEquals("a", rs.getString(3));
+                }
+            }
+            assertPointLookupsAreGenerated(selectSql, conn, null);
+        }
+    }
+    
+    @Test
+    public void testArrayAnyComparisonForBinaryColumn() throws Exception {
+        String tableName = generateUniqueName();
+        String ddl = "CREATE TABLE " + tableName + " (pk1 BINARY NOT NULL, " + "pk2 VARCHAR(3), "
+            + "col1 VARCHAR, " + "CONSTRAINT pk PRIMARY KEY (pk1, pk2))";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(ddl);
+                conn.commit();
+            }
+        }
+        String pk1Value = "1234567890";
+        String pk2Value = "x";
+        String upsertStmt = "UPSERT INTO " + tableName + " VALUES (?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            try (PreparedStatement stmt = conn.prepareStatement(upsertStmt)) {
+                stmt.setBytes(1, pk1Value.getBytes());
+                stmt.setString(2, pk2Value);
+                stmt.setString(3, "a");
+                stmt.executeUpdate();
+                conn.commit();
+            }
+        }
+        try (Connection conn = DriverManager.getConnection(getUrl())) {
+            TestUtil.dumpTable(conn, TableName.valueOf(tableName));
+            String selectSql = "SELECT * FROM " + tableName + " WHERE pk1 = ANY(?) AND pk2 = ANY(?)";
+            Array binaryArr = conn.createArrayOf("BINARY", new byte[][] { pk1Value.getBytes(), pk1Value.getBytes(), pk1Value.getBytes() });
+            try (PreparedStatement stmt = conn.prepareStatement(selectSql)) {
+                stmt.setArray(1, binaryArr);
+                stmt.setString(2, pk2Value);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    assertTrue(rs.next());
+                    assertEquals(pk1Value, new String(rs.getBytes(1)));
+                    assertEquals(pk2Value, rs.getString(2));
+                    assertEquals("a", rs.getString(3));
+                }
+            }
+            assertPointLookupsAreGenerated(selectSql, conn, binaryArr);
         }
     }
 
