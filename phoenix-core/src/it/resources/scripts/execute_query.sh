e88d0fc75e6dc48de1fa2d24de48753da36d119f
Viraj Jasani <vjasani@apache.org>
2025-09-18 10:40:09 -0700

    PHOENIX-7702 Backward compatibility test framework support for phoenix 5.2 and hbase 2.6 (#2287)

--- phoenix-core/src/it/resources/scripts/execute_query.sh
+++ phoenix-core/src/it/resources/scripts/execute_query.sh
@@ -20,13 +20,27 @@
 # This script is intended to run the sql queries in a file with the given client version
 
 zk_url=$1
-client_group_id=$2
-client_artifact_id=$3
-client_version=$4
-sqlfile=$5
-resultfile=$6
-tmp_dir=$7
-maven_home=$8
+artifact_count=$2
+
+artifacts=()
+param_index=3
+for ((i=0; i<artifact_count; i++)); do
+  group_id=${!param_index}
+  ((param_index++))
+  artifact_id=${!param_index}
+  ((param_index++))
+  version=${!param_index}
+  ((param_index++))
+  artifacts+=("$group_id:$artifact_id:$version")
+done
+
+sqlfile=${!param_index}
+((param_index++))
+resultfile=${!param_index}
+((param_index++))
+tmp_dir=${!param_index}
+((param_index++))
+maven_home=${!param_index}
 
 if [ -n $maven_home ]; then
    export PATH=$maven_home/bin:$PATH
@@ -37,12 +51,41 @@ if [ -z ${error_code+x} ]; then
   security_manager='-Djava.security.manager=allow'
 fi
 
-mvn -B dependency:get -Dartifact=${client_group_id}:${client_artifact_id}:${client_version}
-mvn -B dependency:copy -Dartifact=${client_group_id}:${client_artifact_id}:${client_version} \
--DoutputDirectory=$tmp_dir
+echo "Downloading ${#artifacts[@]} artifacts..."
+for artifact in "${artifacts[@]}"; do
+  echo "Downloading $artifact"
+  mvn -B dependency:get -Dartifact=$artifact
+  if [ $? -ne 0 ]; then
+    echo "Failed to download artifact $artifact"
+    exit 1
+  fi
 
-phoenix_client_jar=$tmp_dir/${client_artifact_id}-${client_version}.jar
-java -cp ".:$phoenix_client_jar" $security_manager sqlline.SqlLine \
+  mvn -B dependency:copy -Dartifact=$artifact -DoutputDirectory=$tmp_dir
+  if [ $? -ne 0 ]; then
+    echo "Failed to copy artifact $artifact"
+    exit 1
+  fi
+done
+
+classpath="."
+for jar in $tmp_dir/*.jar; do
+  classpath="$classpath:$jar"
+done
+
+echo "Using classpath: $classpath"
+
+phoenix_props="-Dphoenix.query.timeoutMs=60000 -Dphoenix.query.keepAliveMs=60000"
+
+phoenix_props="$phoenix_props -Dlog4j.logger.org.apache.phoenix=DEBUG"
+phoenix_props="$phoenix_props -Dlog4j.logger.org.apache.hadoop.hbase=DEBUG"
+phoenix_props="$phoenix_props -Dlog4j.logger.sqlline=DEBUG"
+phoenix_props="$phoenix_props -Dlog4j.logger.org.apache.zookeeper=INFO"
+
+echo "Attempting to connect to Phoenix with URL: jdbc:phoenix:$zk_url"
+echo "Using classpath: $classpath"
+echo "Phoenix properties: $phoenix_props"
+
+java -cp "$classpath" $security_manager $phoenix_props sqlline.SqlLine \
 -d org.apache.phoenix.jdbc.PhoenixDriver -u jdbc:phoenix:$zk_url -n none -p none \
 --color=false --fastConnect=true --outputformat=csv \
 --silent=true --verbose=false --isolation=TRANSACTION_READ_COMMITTED --run=$sqlfile &> $resultfile
